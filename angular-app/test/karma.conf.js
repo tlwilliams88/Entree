f672afaf3c7757fb879298b2322ef7fb0363416b
Maria Knabe <mknabe@credera.com>
2014-07-09 10:27:07 -0500

    use PhantomJS for testing

--- angular-app/test/karma.conf.js
+++ angular-app/test/karma.conf.js
@@ -48,7 +48,7 @@ module.exports = function(config) {
     // - Safari (only Mac)
     // - PhantomJS
     // - IE (only Windows)
-    browsers: ['IE'],
+    browsers: ['PhantomJS'],
 
 
     // Continuous Integration mode
