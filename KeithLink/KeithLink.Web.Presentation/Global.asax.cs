530e4ca982476a6953b64d18af9337efaf8ef028
gsalazar <gsalazar@credera.com>
2014-07-11 11:20:58 -0500

    clean out MS MVC and implement angular build step

--- KeithLink/KeithLink.Web.Presentation/Global.asax.cs
+++ KeithLink/KeithLink.Web.Presentation/Global.asax.cs
@@ -17,12 +17,6 @@ namespace KeithLink.Web.Presentation
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
-
-            WebApiConfig.Register(GlobalConfiguration.Configuration);
-            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
-            RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
-            AuthConfig.RegisterAuth();
         }
     }
 }
