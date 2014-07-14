eb7e9965c9abb4c35da8b8bfadb318c9bac409f6
gsalazar <gsalazar@credera.com>
2014-07-14 09:02:51 -0500

    integrate commerce server and autofac to build, and stub implementation of catalog repository

---
+++ KeithLink/packages/CommerceServerContrib.CodeGeneration.1.0.48.0/Content/RequestTemplates/ControllerFactory.cs.pp
@@ -0,0 +1,35 @@
+﻿//------------------------------------------------------------------------------
+// <copyright file="ControllerFactory.cs" company="CommerceServer.net, Inc">
+//    (c) 2012 CommerceServer.net, Inc. and its affiliates. All rights reserved.
+// </copyright>
+// <summary></summary>
+//------------------------------------------------------------------------------ 
+namespace $rootnamespace$.RequestTemplates
+{
+	using CommerceServerContrib.Web.Controllers;
+	using CommerceServerContrib.CrossTier.Interfaces.Controllers;
+
+	/// <summary>
+    /// A simple factory for returning the Commerce Server Request Template Factory based on Area.
+    /// </summary>
+	public static class ControllerFactory
+	{
+		/// <summary>
+        /// Method which returns the right controller for the area. 
+        /// </summary>
+        /// <param name="area">Area (Catalog, Profiles, etc.) required.</param>
+        /// <returns>Commerce Server Contrib Controller instance.</returns>
+		public static BaseController<IRequestTemplateController> GetController(string area)
+        {
+            switch (area)
+            {
+                case "Orders":
+                    return new OrdersController();
+                case "Marketing":
+                    return new MarketingController();
+                default:
+                    return new CatalogController();
+            }
+        }
+	}
+}
