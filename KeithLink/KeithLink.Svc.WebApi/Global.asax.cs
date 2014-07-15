cf84de5e6da534a7b11972a3df4e024bed33ea8f
gsalazar <gsalazar@credera.com>
2014-07-15 15:01:55 -0500

    add web api v2

--- KeithLink/KeithLink.Svc.WebApi/Global.asax.cs
+++ KeithLink/KeithLink.Svc.WebApi/Global.asax.cs
@@ -3,8 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Http;
-using System.Web.Mvc;
-using System.Web.Optimization;
 using System.Web.Routing;
 using Autofac;
 using Autofac.Integration.WebApi;
@@ -18,16 +16,13 @@ namespace KeithLink.Svc.WebApi
     {
         protected void Application_Start()
         {
-            AreaRegistration.RegisterAllAreas();
-
-            WebApiConfig.Register(GlobalConfiguration.Configuration);
-            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
-            RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            GlobalConfiguration.Configure(WebApiConfig.Register);
 
             // Configure Web API with the dependency resolver.
             var resolver = DependencyMap.Build();
             GlobalConfiguration.Configuration.DependencyResolver = resolver;
+
+            GlobalConfiguration.Configuration.EnsureInitialized(); 
         }
     }
 }
