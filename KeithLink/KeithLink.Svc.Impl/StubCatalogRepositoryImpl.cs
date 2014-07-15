a82908ae3b1d5d2e678905bfa356a83a1870477a
gsalazar <gsalazar@credera.com>
2014-07-15 14:39:11 -0500

    add web api v2

--- KeithLink/KeithLink.Svc.Impl/StubCatalogRepositoryImpl.cs
+++ KeithLink/KeithLink.Svc.Impl/StubCatalogRepositoryImpl.cs
@@ -18,6 +18,22 @@ namespace KeithLink.Svc.Impl
             new Product { Id = 5, Name = "Hammer Five", Category = "Hardware", Price = 16.95M, Weight = "5lb"  } 
         };
 
+        Category[] categories = new Category[]
+        {
+            new Category() { Name = "Shrimp", Description="Shrimp, Headless, Raw", Id="FS940"
+                , SubCategories = new Category[] {
+                    new Category() { Id = "FS942", Name="Jumbo Shrimp", Description="Shrimp, Headless, Raw 12 Ct"},
+                    new Category() { Id = "FS941", Name="Popcorn Shrimp", Description="Shrimp, Headless, Raw 65 Ct"}}},
+            new Category() { Name = "Cakes", Description="Cakes Silly", Id="CS940"
+                , SubCategories = new Category[] {
+                    new Category() { Id = "CS942", Name="Cakes, Decorated", Description="Decorated Cakes"},
+                    new Category() { Id = "CS941", Name="Cakes, Bulk", Description="Un-decorated cakes"}}},
+            new Category() { Name = "Pork", Description="Soo weee!!!", Id="PS940"
+                , SubCategories = new Category[] {
+                    new Category() { Id = "PS942", Name="Pork Shoulder", Description="Bulk Pork Shoulder"},
+                    new Category() { Id = "PS941", Name="Pork, Chops", Description="Pork Chops"}}},
+        };
+
         public IEnumerable<Product> GetProductsForCategory(string category)
         {
             if (String.IsNullOrEmpty(category))
@@ -29,6 +45,12 @@ namespace KeithLink.Svc.Impl
                 IEnumerable<Product> prods = products.Where(x => x.Category == category);
                 return prods;
             }
+        }
+
+
+        public CategoriesReturn GetCategories()
+        {
+            return new CategoriesReturn() { Categories = categories.ToList() };
         }
     }
 }
