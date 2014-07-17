1fb0c573ba5d0264c595996da5c1066e52e8738b
aallen <aallen@AAllen-e7440.credera.com>
2014-07-17 17:06:24 -0500

    adding mobile styling to home page

--- KeithLink/KeithLink.Web.Presentation/angular-app/app/scripts/controllers/HomeController.js
+++ KeithLink/KeithLink.Web.Presentation/angular-app/app/scripts/controllers/HomeController.js
@@ -8,22 +8,32 @@
  * Controller of the bekApp
  */
 angular.module('bekApp')
-  .controller('HomeController', function ($scope) {
-    
-    $scope.orders = [
-      {
-        orderNum: 212342342,
-        deliveryDate: '12/13/2014',
-        totalCost: '1234.32',
-        status: 'Open',
-        paymentStatus: 'N/A'
-      }
-    ];
+  .controller('HomeController', function($scope) {
+
+    $scope.orders = [{
+      orderNum: 212342342,
+      deliveryDate: '12/13/2014',
+      totalCost: '1234.32',
+      status: 'Open',
+      paymentStatus: 'N/A'
+    }];
 
     $scope.locations = [
       'Jimmy\'s Chicken Shack',
       'Torchy\'s Tacos'
     ];
 
+    $scope.myInterval = -1;
+    var items = $scope.items = [];
+    $scope.addItem = function() {
+      var newWidth = 300 + items.length;
+      items.push({
+        image: 'http://placebear.com/' + newWidth + '/300',
+        description: '50% off of apples!'
+      });
+    };
+    for (var i = 0; i < 4; i++) {
+      $scope.addItem();
+    }
 
   });
