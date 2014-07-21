5a17624515028617617b7f25237fc1dec23eb971
bekco\jwames <jwames@benekeith.com>
2014-07-21 14:44:43 -0500

    add the price class

---
+++ KeithLink/KeithLink.Svc.Core/Price.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace KeithLink.Svc.Core
+{
+    [DataContract(Name="Price")]
+    class Price
+    {
+        [DataMember(Name="BranchId")]
+        public string BranchId { get; set; }
+
+        [DataMember(Name="CustomerNumber")]
+        public string CustomerNumber { get; set; }
+
+        [DataMember(Name="ItemNumber")]
+        public string ItemNumber { get; set; }
+
+        [DataMember(Name="CasePrice")]
+        public double CasePrice { get; set; }
+
+        [DataMember(Name="PackagePrice")]
+        public double PackagePrice { get; set; }
+    }
+
+    [DataContract(Name="PriceReturn")]
+    class PriceReturn
+    {
+        [DataMember(Name="Prices")]
+        public List<Price> Prices { get; set; }
+    }
+}
