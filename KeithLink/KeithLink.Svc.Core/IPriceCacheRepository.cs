233a2adff759872e4bc9012999cc411f7fa20416
bekco\jwames <jwames@benekeith.com>
2014-07-28 11:14:21 -0500

    change the name of the IPriceCache to IPriceCacheRepository

---
+++ KeithLink/KeithLink.Svc.Core/IPriceCacheRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeithLink.Svc.Core
+{
+    public interface IPriceCacheRepository
+    {
+        void AddItem(string branchId, string customerNumber, string itemNumber, double casePrice, double packagePrice);
+
+        void ResetAllItems();
+
+        void ResetItemsByCustomer(string branchId, string customerNumber);
+
+        void RemoveItem(string branchId, string customerNumber, string itemNumber);
+    }
+}
