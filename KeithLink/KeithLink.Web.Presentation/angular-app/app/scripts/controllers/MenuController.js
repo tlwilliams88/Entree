4c8638c5f90016727727ce49f3608a847171143b
Maria Knabe <mknabe@credera.com>
2014-07-29 15:56:08 -0500

    update branch info

--- KeithLink/KeithLink.Web.Presentation/angular-app/app/scripts/controllers/MenuController.js
+++ KeithLink/KeithLink.Web.Presentation/angular-app/app/scripts/controllers/MenuController.js
@@ -23,13 +23,17 @@ angular.module('bekApp')
       'phone': 9348234934,
       'location': 'Dallas',
       'stores': [{
-        'name': 'Jimmy\'s Chicken Shack',
+        'name': 'Dallas Ft Worth',
         'customerNumber': 453234,
-        'branchId': 'fam'
+        'branchId': 'fdf'
       }, {
-        'name': 'Saltgrass',
+        'name': 'San Antonio',
         'customerNumber': 534939,
         'branchId': 'fsa'
+      }, {
+        'name': 'Amarillo',
+        'customerNumber': 534939,
+        'branchId': 'fam'
       }],
       'accountNumber': 9783459,
       'salesRep': {
@@ -39,6 +43,14 @@ angular.module('bekApp')
         'email': 'heather.hill@ben.e.keith.com',
         'imageUrl': '../images/placeholder-dsr.jpg'
       }
+    };
+
+    $scope.test = function(e) {
+      $scope.testMessage = new Date();
+    };
+
+    $scope.mouseupEvent = function(e) {
+      $scope.testMessage = 'move';
     };
 
     $scope.currentUser.currentLocation = $scope.currentUser.stores[0];
