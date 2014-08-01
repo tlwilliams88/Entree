044e5f578c59613d53ee3d631eeeb401481d821b
gsalazar <gsalazar@credera.com>
2014-08-01 01:09:20 -0600

    merge

---
+++ KeithLink/KeithLink.Svc.Core/IListRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeithLink.Svc.Core
+{
+    public interface IListRepository
+    {
+        Guid CreateList(UserList list);
+
+        List<UserList> ReadAllLists();
+        UserList ReadList(Guid listId);
+        List<string> ReadListLabels(Guid listId);
+        List<string> ReadListLabels();
+        
+    }
+}
---
+++ KeithLink/KeithLink.Svc.Core/UserList.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeithLink.Svc.Core
+{
+    [DataContract(Name = "UserList")]
+    public class UserList
+    {
+        [DataMember(Name = "listid")]
+        public Guid ListId { get; set; }
+        [DataMember(Name = "name")]
+        public string Name { get; set; }
+        [DataMember(Name = "items")]
+        public List<ListItem> Items { get; set; }
+    }
+}
