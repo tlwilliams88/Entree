498eb10a3a271fc862a167f38becfd1394ae2649
bekco\jwames <jwames@benekeith.com>
2014-08-01 15:20:12 -0500

    create method for authenticating a

---
+++ KeithLink/KeithLink.Svc.Impl/Profile/AuthenticationManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.DirectoryServices.AccountManagement;
+using System.Text;
+
+namespace KeithLink.Svc.Impl.Profile
+{
+    public static class AuthenticationManager 
+    {
+        public bool AuthenticateUser(string userName, string password)
+        {
+            try
+            {
+                using (PrincipalContext principal = new PrincipalContext(ContextType.ApplicationDirectory, 
+                                                                         Configuration.ActiveDirectoryServerName, 
+                                                                         Configuration.ActiveDirectoryRootNode,
+                                                                         ContextOptions.SecureSocketLayer | ContextOptions.SimpleBind, 
+                                                                         Configuration.ActiveDirectoryUserName, 
+                                                                         Configuration.ActiveDirectoryPassword) )
+                {
+                    return principal.ValidateCredentials(userName, password);                
+                }
+            }
+            catch (Exception ex){
+                new Common.Impl.Logging.EventLogRepositoryImpl(Configuration.ApplicationName).WriteErrorLog("Could not authenticate user", ex);
+
+                return false;
+            }
+        }
+    }
+}
