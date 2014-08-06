bb17a07bbcc20f7f6359d415476459cab529f723
bekco\jwames <jwames@benekeith.com>
2014-08-06 09:31:03 -0500

    create user profile classes

---
+++ KeithLink/KeithLink.Svc.Core/Profile/UserProfileReturn.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeithLink.Svc.Core.Profile
+{
+    [DataContract(Name="UserProfileReturn")]
+    public class UserProfileReturn
+    {
+        #region attributes
+        private List<UserProfile> _profileList;
+        #endregion
+
+        #region ctor
+        public UserProfileReturn()
+        {
+            _profileList = new List<UserProfile>();
+        }
+        #endregion
+
+        #region properties
+        [DataMember(Name= "UserProfiles")]
+        public List<UserProfile> UserProfiles {
+            get { return _profileList; }
+            set { _profileList = value; }
+        }
+        #endregion
+    }
+}
