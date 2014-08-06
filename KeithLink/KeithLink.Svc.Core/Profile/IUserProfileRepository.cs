dc25d9d9cb94bd12f9360ee74775642cc8e45636
bekco\jwames <jwames@benekeith.com>
2014-08-06 09:43:53 -0500

    add method for getting all

--- KeithLink/KeithLink.Svc.Core/Profile/IUserProfileRepository.cs
+++ KeithLink/KeithLink.Svc.Core/Profile/IUserProfileRepository.cs
@@ -10,7 +10,8 @@ namespace KeithLink.Svc.Core.Profile
     {
         public void CreateUserProfile(string userName, string customerName, string emailAddres, string firstName, string lastName, string phoneNumber);
         public void DeleteUserProfile(string userName);
-        public UserProfile GetUserProfile(string userName);
+        public UserProfileReturn GetUserProfile(string userName);
+        public UserProfileReturn GetUserProfilesByCustomerName(string customerName);
         public void UpdateUserProfile(string userName, string customerName, string emailAddres, string firstName, string lastName, string phoneNumber);
     }
 }
