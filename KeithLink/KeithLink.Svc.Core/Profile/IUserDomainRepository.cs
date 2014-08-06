c4a5fb49d4474483cdbe278b88508660e0b2a8d1
bekco\jwames <jwames@benekeith.com>
2014-08-06 09:50:44 -0500

    create interface for IUserDomainRepository

---
+++ KeithLink/KeithLink.Svc.Core/Profile/IUserDomainRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeithLink.Svc.Core.Profile
+{
+    public interface IUserDomainRepository
+    {
+        public bool AuthenticateUser(string userName, string password);
+        public bool IsInGroup(string userName, string groupName);
+    }
+}
