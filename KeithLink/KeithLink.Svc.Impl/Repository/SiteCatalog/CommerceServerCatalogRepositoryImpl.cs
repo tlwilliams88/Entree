f72dbb750ecaf464ffa90dead3ef01be6f19f3e2
gsalazar <gsalazar@credera.com>
2014-08-07 15:54:25 -0600

    fixing the build.  i should have to wear a dunce hat tomorrow

--- KeithLink/KeithLink.Svc.Impl/Repository/SiteCatalog/CommerceServerCatalogRepositoryImpl.cs
+++ KeithLink/KeithLink.Svc.Impl/Repository/SiteCatalog/CommerceServerCatalogRepositoryImpl.cs
@@ -13,23 +13,11 @@ namespace KeithLink.Svc.Impl.Repository.SiteCatalog
     {
         public ProductsReturn GetProductsByCategory(string branch, string category, int from, int size, string facetFilters)
         {
-            var queryUser =
-                new CommerceServer.Foundation.CommerceQuery<CommerceServer.Foundation.CommerceEntity>("UserProfile");
-            queryUser.SearchCriteria.Model.Properties["Email"] = "joesmith@company.com";
-            queryUser.Model.Properties.Add("Id");
-            queryUser.Model.Properties.Add("FirstName");
-            CommerceServer.Foundation.CommerceRequestContext requestContext = new CommerceServer.Foundation.CommerceRequestContext();
-            // indicate the default channel
-            requestContext.Channel = string.Empty;
-            requestContext.RequestId = System.Guid.NewGuid().ToString("B");
-            requestContext.UserLocale = "en-US";
-            requestContext.UserUILocale = "en-US";
-            // Execute the operation and get the results back
-            CommerceServer.Foundation.OperationServiceAgent serviceAgent = new CommerceServer.Foundation.OperationServiceAgent();
-            CommerceServer.Foundation.CommerceResponse response =
-                serviceAgent.ProcessRequest(requestContext, queryUser.ToRequest());
-            CommerceServer.Foundation.CommerceQueryOperationResponse queryResponse = response.OperationResponses[0] as CommerceServer.Foundation.CommerceQueryOperationResponse;
-            CommerceServer.Foundation.CommerceEntity user = queryResponse.CommerceEntities[0];
+            throw new NotImplementedException();
+        }
+
+        public ProductsReturn GetProductsByIds(string branch, List<string> ids)
+        {
             throw new NotImplementedException();
         }
 
