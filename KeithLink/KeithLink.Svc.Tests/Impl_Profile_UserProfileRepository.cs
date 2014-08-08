6216d337a2968dd7230dd99ecc34ea19b42e79cb
bekco\jwames <jwames@benekeith.com>
2014-08-08 09:00:03 -0500

    and there's the test

---
+++ KeithLink/KeithLink.Svc.Tests/Impl_Profile_UserProfileRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace KeithLink.Svc.Test
+{
+    [TestClass]
+    public class Impl_Profile_UserProfileRepository
+    {
+        [TestMethod]
+        public void CreateUser()
+        {
+            KeithLink.Svc.Impl.Profile.UserProfileRepository userProfile = new Impl.Profile.UserProfileRepository();
+
+            userProfile.CreateUserProfile("mytest", "Test Customer", "joesmith@company.com", "Joe", "Smith", "1234567890");
+        }
+
+        [TestMethod]
+        public void GetUserByEmailAddress()
+        {
+            KeithLink.Svc.Impl.Profile.UserProfileRepository userProfile = new Impl.Profile.UserProfileRepository();
+
+            userProfile.GetUserProfile("joesmith@company.com");
+        }
+    }
+}
