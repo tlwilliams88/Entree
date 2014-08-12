56d1dd8b56f35da488824406358d8205e8f68149
Maria Knabe <mknabe@credera.com>
2014-08-12 11:26:10 -0500

    connect login service, display placeholders for nutrition info

--- KeithLink/KeithLink.Web.Presentation/angular-app/app/scripts/services/UserProfileService.js
+++ KeithLink/KeithLink.Web.Presentation/angular-app/app/scripts/services/UserProfileService.js
@@ -8,55 +8,32 @@
  * Service of the bekApp
  */
 angular.module('bekApp')
-  .factory('UserProfileService', function ($http) {
-    
-    var profile = {
-      'name': 'Steven',
-      'customerNumber': 12345,
-      'imageUrl': '../images/placeholder-user.png',
-      'role': 'Owner',
-      'phone': 9348234934,
-      'location': 'Dallas',
-      'stores': [{
-        'name': 'Dallas Ft Worth',
-        'customerNumber': 453234,
-        'branchId': 'fdf'
-      }, {
-        'name': 'San Antonio',
-        'customerNumber': 534939,
-        'branchId': 'fsa'
-      }, {
-        'name': 'Amarillo',
-        'customerNumber': 534939,
-        'branchId': 'fam'
-      }],
-      'accountNumber': 9783459,
-      'salesRep': {
-        'id': 34234,
-        'name': 'Heather Hill',
-        'phone': '(888) 912-2342',
-        'email': 'heather.hill@ben.e.keith.com',
-        'imageUrl': '../images/placeholder-dsr.jpg'
-      },
-      'currentLocation': {
-        'name': 'Dallas Ft Worth',
-        'customerNumber': 453234,
-        'branchId': 'fdf'
-      }
-    };
-
-    var currentLocation = profile.stores[0];
+  .factory('UserProfileService', ['$http', function ($http) {
 
     var Service = {
+      profile: {},
+
       getProfile: function() {
-        return profile;
+        return Service.profile;
+      },
+
+      setProfile: function(newProfile) {
+        angular.copy(newProfile, Service.profile);
       },
 
       getCurrentLocation: function() {
-        return profile.currentLocation;
+        return Service.profile.currentLocation;
+      },
+
+      createUser: function() {
+
+      },
+
+      updateUser: function() {
+
       }
     };
 
     return Service;
 
-  });
+  }]);
