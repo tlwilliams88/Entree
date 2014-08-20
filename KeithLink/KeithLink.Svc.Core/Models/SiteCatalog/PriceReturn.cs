1853b64a861aaec77f466312ed69fa6623d1fb9d
bekco\jwames <jwames@benekeith.com>
2014-08-20 16:49:09 -0500

    separate the price return class from the price class file

---
+++ KeithLink/KeithLink.Svc.Core/Models/SiteCatalog/PriceReturn.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace KeithLink.Svc.Core.Models.SiteCatalog
+{
+    [DataContract(Name = "PriceReturn")]
+    public class PriceReturn
+    {
+
+        #region ctor
+        public PriceReturn()
+        {
+            Prices = new List<Price>();
+        }
+        #endregion
+
+        #region properties
+        [DataMember(Name = "Prices")]
+        public List<Price> Prices { get; set; }
+        #endregion
+    }
+}
