2c54694059bc4e4c5ab399aa3f1349ef5a0c1b64
Maria Knabe <mknabe@credera.com>
2014-08-22 15:51:57 -0500

    use up arrow to navigate to previous par level

--- KeithLink/KeithLink.Web.Presentation/angular-app/app/scripts/directives/GoToNextParlevel.js
+++ KeithLink/KeithLink.Web.Presentation/angular-app/app/scripts/directives/GoToNextParlevel.js
@@ -7,18 +7,33 @@ angular.module('bekApp')
 		link: function (scope, elem, attr, ctrl) {
 			
       elem.bind('keyup', function(event) {
-        if(event.keyCode === 40 || event.keyCode === 13) {
-		      var tabindex = parseInt(angular.element(event.target).attr('tabindex')) + 1;
+        var tabindex = parseInt(angular.element(event.target).attr('tabindex'));
+        var endIndex;
 
-          // get the parlevel input with the new tabindex
-		      var elems = angular.element('[tabindex=' + tabindex + '][name=parlevel]');
-		      if (elems.length === 1) {
-		        elems[0].focus();
-		      } else { // go back to the beginning of the list
-		        angular.element('[name=parlevel]')[0].focus();
-		      }
-	    	}
+        if(event.keyCode === 40 || event.keyCode === 13) {
+          // 13-enter, 40-down arrow goes to next par level
+		      tabindex++;
+          endIndex = 0;
+          focusParlevelInput(tabindex, endIndex);
+        } else if (event.keyCode === 38) {
+          // 38-up arrow, go to previous parlevel
+          tabindex--;
+          endIndex = angular.element('[name=parlevelForm] [name=parlevel]').length - 1;
+          focusParlevelInput(tabindex, endIndex);
+        }
+
 	    });
+
+      function focusParlevelInput(tabindex, endIndex) {
+        // get the parlevel input with the new tabindex
+        // if next tabindex does not exist go to end index
+        var elems = angular.element('[tabindex=' + tabindex + '][name=parlevel]');
+        if (elems.length === 1) {
+          elems[0].focus();
+        } else { // go back to the beginning of the list
+          angular.element('[name=parlevelForm] [name=parlevel]')[endIndex].focus();
+        }
+      }
 		}
 	};
 });
