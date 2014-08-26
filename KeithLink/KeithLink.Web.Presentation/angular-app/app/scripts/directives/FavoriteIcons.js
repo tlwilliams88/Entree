b51d5a7bb8724d4ee4d301f95fb0209537904253
Maria Knabe <mknabe@credera.com>
2014-08-26 13:15:38 -0500

    added some documentation for directives

--- KeithLink/KeithLink.Web.Presentation/angular-app/app/scripts/directives/FavoriteIcons.js
+++ KeithLink/KeithLink.Web.Presentation/angular-app/app/scripts/directives/FavoriteIcons.js
@@ -1,5 +1,13 @@
 'use strict';
 
+/**
+ * @ngdoc function
+ * @name bekApp.directive:favoriteIcons
+ * @description
+ * displays confirmation message and executes the given function if message is approved
+ *
+ * used on search results and lists page
+ */
 angular.module('bekApp')
 .directive('favoriteIcons', ['ListService', function(ListService){
   return {
