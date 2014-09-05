d94745e9c09fe0a0eaed52e2110f11044f6ca60a
bekco\jwames <jwames@benekeith.com>
2014-09-05 16:27:12 -0500

    add ToString method to order detail

--- KeithLink/KeithLink.Svc.Core/Models/Orders/OrderDetail.cs
+++ KeithLink/KeithLink.Svc.Core/Models/Orders/OrderDetail.cs
@@ -7,6 +7,57 @@ namespace KeithLink.Svc.Core.Models.Orders
     [DataContract(Name="OrderDetail")]
     public class OrderDetail
     {
+        #region methods
+        public string ToString()
+        {
+            if (LineNumber == 0) { throw new ArgumentException("LineNumber has not been set", "LineNumber"); }
+            if (ItemNumber.Length == 0) { throw new ArgumentException("ItemNumber has not been set", "ItemNumber"); }
+
+            StringBuilder output = new StringBuilder("D");
+
+            output.Append(ItemNumber);
+            output.Append(OrderedQuantity.ToString().PadLeft(3, '0'));
+
+            switch (UnitOfMeasure)
+            {
+                case Orders.UnitOfMeasure.Case:
+                    output.Append(" ");
+                    break;
+                case Orders.UnitOfMeasure.Package:
+                    output.Append("P");
+                    break;
+                default:
+                    throw new ArgumentException("UnitOfMeasure has an unrecognized value", "UnitOfMeasure");
+            }
+
+            output.Append(Catchweight ? "Y" : " ");
+            output.Append(LineNumber.ToString().PadLeft(5, '0'));
+
+            switch(ItemChange){
+                case LineType.NoChange:
+                    output.Append(" ");
+                    break;
+                case LineType.Change:
+                    output.Append("C");
+                    break;
+                case LineType.Delete:
+                    output.Append("D");
+                    break;
+                case LineType.Add:
+                    output.Append("A");
+                    break;
+                default:
+                    throw new ArgumentException("ItemChange has an unrecognized value", "ItemChange");
+            }
+
+            output.Append(SubOriginalItemNumber.PadRight(6));
+            output.Append(ReplacedOriginalItemNumber.PadRight(6));
+            output.Append(ItemStatus.PadRight(1));
+
+            return output.ToString();
+        }
+        #endregion
+        
         #region properties
         [DataMember(Name="ItemNumber")]
         public string ItemNumber { get; set; }
