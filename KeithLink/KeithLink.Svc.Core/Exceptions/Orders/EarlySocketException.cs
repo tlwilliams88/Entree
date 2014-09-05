704faceef64178b489002a433b0f4f2bfe67b76f
bekco\jwames <jwames@benekeith.com>
2014-09-05 13:19:56 -0500

    add the EarlySocketException

---
+++ KeithLink/KeithLink.Svc.Core/Exceptions/Orders/EarlySocketException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace KeithLink.Svc.Core.Exceptions.Orders
+{
+    public class EarlySocketException : Exception
+    {
+        #region ctor
+        public EarlySocketException() { }
+
+        public EarlySocketException(string Message) : base(Message) { }
+
+        public EarlySocketException(string Message, Exception ex) : base(Message, ex) { }
+        #endregion
+    }
+}
