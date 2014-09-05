6aaddc90e76a95672215ede94b9878974d1ef6c6
bekco\jwames <jwames@benekeith.com>
2014-09-05 16:32:44 -0500

    add a test for sending a stub order to the mainframe

---
+++ KeithLink/KeithLink.Svc.Tests/Impl_Orders_OrderLogicImpl.cs
@@ -0,0 +1,20 @@
+﻿using KeithLink.Svc.Impl.Logic.Orders;
+using KeithLink.Svc.Impl.Repository.Orders;
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace KeithLink.Svc.Test
+{
+    [TestClass]
+    public class Impl_Orders_OrderLogicImpl
+    {
+        [TestMethod]
+        public void SendMockOrder()
+        {
+            StubOrderLogicImpl order = new StubOrderLogicImpl(new OrderSocketConnectionRepositoryImpl());
+
+            order.ParseFile("somefilename");
+            order.SendToHost();
+        }
+    }
+}
