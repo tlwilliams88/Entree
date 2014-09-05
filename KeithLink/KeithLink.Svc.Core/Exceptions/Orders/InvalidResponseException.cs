de62fe201bde5712af0c08dc14c0caedfaf4dfb9
bekco\jwames <jwames@benekeith.com>
2014-09-05 13:21:55 -0500

    add the InvalidResponseExcpetion

---
+++ KeithLink/KeithLink.Svc.Core/Exceptions/Orders/InvalidResponseException.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace KeithLink.Svc.Core.Exceptions.Orders
+{
+    public class InvalidResponseException : Exception
+    {
+        public InvalidResponseException() : base("Invalid response received from host") { }
+    }
+}
