d3cd1e7ac92c3cdef69fb891e573a6223f8aae0f
bekco\jwames <jwames@benekeith.com>
2014-09-05 13:20:08 -0500

    add the FileSendException

---
+++ KeithLink/KeithLink.Svc.Core/Exceptions/Orders/FileSendException.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeithLink.Svc.Core.Exceptions.Orders
+{
+    public class FileSendException : Exception
+    {
+        #region ctor
+        public FileSendException(string Exceptionmessage, string AffectedFile)
+            : base(Exceptionmessage)
+        {
+            this.AffectedFile = AffectedFile;
+        }
+
+        public FileSendException(string ExceptionMessage, string AffectedFile, Exception ex)
+            : base(ExceptionMessage, ex)
+        {
+            this.AffectedFile = AffectedFile;
+        }
+        #endregion
+
+        #region properties
+        public string AffectedFile { get; set; }
+        #endregion
+    }
+}
