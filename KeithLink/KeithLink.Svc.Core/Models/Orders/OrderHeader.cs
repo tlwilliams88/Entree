1cb19ba30850b8b067beb33b3c7459420c2489a1
bekco\jwames <jwames@benekeith.com>
2014-09-05 16:14:41 -0500

    build the output layout in the ToString method for order header

--- KeithLink/KeithLink.Svc.Core/Models/Orders/OrderHeader.cs
+++ KeithLink/KeithLink.Svc.Core/Models/Orders/OrderHeader.cs
@@ -10,7 +10,48 @@ namespace KeithLink.Svc.Core.Models.Orders
         #region methods
         public string ToString()
         {
+            if (Branch.Length != 3) { throw new ArgumentException("Branch is an invalid format", "Branch"); }
+            if (CustomerNumber.Length != 6) { throw new ArgumentException("CustomerNumber is in an invalid format", "CustomerNumber"); }
+
             StringBuilder output = new StringBuilder("H");
+
+            switch (OrderingSystem)
+            {
+                case OrderSource.KeithCom:
+                    output.Append("B");
+                    break;
+                default:
+                    throw new ArgumentException("Unkown OrderingSystem", "OrderingSystem");
+            }
+
+            output.Append(Branch);
+            output.Append(CustomerNumber);
+            output.Append(DeliveryDate.ToString("yyyyMMdd"));
+            output.Append(PONumber.PadRight(20));
+            output.Append(Specialinstructions.PadRight(80));
+            output.Append(ControlNumber.ToString().PadLeft(7, '0'));
+
+            switch (OrderType)
+            {
+                case Orders.OrderType.NormalOrder:
+                    output.Append(" ");
+                    break;
+                case Orders.OrderType.ChangeOrder:
+                    output.Append("C");
+                    break;
+                case Orders.OrderType.DeleteOrder:
+                    output.Append("D");
+                    break;
+                default:
+                    throw new ArgumentException("Unknown OrderType", "OrderType");
+            }
+
+            output.Append(InvoiceNumber.PadLeft(8, '0'));
+            output.Append(OrderCreateDateTime.ToString("yyyyMMddHHmmss"));
+            output.Append(OrderSendDateTime.ToString("yyyyMMddHHmmss"));
+            output.Append(UserId.PadRight(50));
+            output.Append(OrderFilled ? "Y" : " ");
+            output.Append(FutureOrder ? "Y" : " ");
 
             return output.ToString();
         }
@@ -36,7 +77,7 @@ namespace KeithLink.Svc.Core.Models.Orders
         public string Specialinstructions { get; set; }
 
         [DataMember(Name = "ControlNumber")]
-        public string ControlNumber { get; set; }
+        public int ControlNumber { get; set; }
 
         [DataMember(Name = "OrderType")]
         public OrderType OrderType { get; set; }
@@ -49,6 +90,9 @@ namespace KeithLink.Svc.Core.Models.Orders
 
         [DataMember(Name = "OrderSendDateTime")]
         public DateTime OrderSendDateTime { get; set; }
+
+        [DataMember(Name = "UserId")]
+        public string UserId { get; set; }
 
         [DataMember(Name = "OrderFilled")]
         public bool OrderFilled { get; set; }
