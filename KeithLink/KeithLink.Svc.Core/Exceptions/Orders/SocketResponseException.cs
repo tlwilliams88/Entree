aec63c1529c37c43f610842ca609a80a04a6fcf3
bekco\jwames <jwames@benekeith.com>
2014-09-05 13:29:07 -0500

    add SocketResponseException

---
+++ KeithLink/KeithLink.Svc.Core/Exceptions/Orders/SocketResponseException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace KeithLink.Svc.Core.Exceptions.Orders
+{
+    public class SocketResponseException:Exception
+    {
+        #region ctor
+        public SocketResponseException(string ExceptionMessage) : base (ExceptionMessage) { }
+
+        public SocketResponseException(string ExceptionMessage, Exception ex):base(ExceptionMessage, ex){}
+        #endregion
+    }
+}
