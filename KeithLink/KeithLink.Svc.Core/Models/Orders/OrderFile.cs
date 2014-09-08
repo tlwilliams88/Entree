02dedd1912925eec62eb8ef5bfa6fd5e9381b44d
bekco\jwames <jwames@benekeith.com>
2014-09-08 16:50:54 -0500

    add a model for OrderFile that combines a header and a list of details

---
+++ KeithLink/KeithLink.Svc.Core/Models/Orders/OrderFile.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace KeithLink.Svc.Core.Models.Orders
+{
+    public class OrderFile
+    {
+        #region ctor
+        public OrderFile()
+        {
+            Header = new OrderHeader();
+            Details = new List<OrderDetail>();
+        }
+        #endregion
+
+        #region  properties
+        public OrderHeader Header { get; set; }
+
+        public List<OrderDetail> Details { get; set; }
+        #endregion
+    }
+}
