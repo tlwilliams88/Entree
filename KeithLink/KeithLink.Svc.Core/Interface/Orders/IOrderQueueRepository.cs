fddd84b456bee84ce0751911b48421a0362b0605
bekco\jwames <jwames@benekeith.com>
2014-09-08 16:47:07 -0500

    add service and new repository

---
+++ KeithLink/KeithLink.Svc.Core/Interface/Orders/IOrderQueueRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace KeithLink.Svc.Core.Interface.Orders
+{
+    public interface IOrderQueueRepository
+    {
+        void ConsumeOrders();
+
+        void PublishOrder(string rawOrderFile);
+    }
+}
