4918a257fe7de4bbed3ab0b1df93523b44817e3e
Maria Knabe <mknabe@credera.com>
2014-09-23 16:12:45 -0500

    item number notes field

--- KeithLink/KeithLink.Web.Presentation/angular-app/app/scripts/controllers/ItemDetailsController.js
+++ KeithLink/KeithLink.Web.Presentation/angular-app/app/scripts/controllers/ItemDetailsController.js
@@ -10,7 +10,9 @@
 angular.module('bekApp')
   .controller('ItemDetailsController', ['$scope', 'item', 'ProductService', 'ListService', 'CartService', function ($scope, item, ProductService, ListService, CartService) {
     
-    $scope.item = item;
+
+
+    $scope.item = angular.copy(item);
     $scope.item.quantity = 1;
     
     ProductService.getProductDetails(item.itemnumber).then(function(item) {
@@ -30,5 +32,27 @@ angular.module('bekApp')
 
     $scope.canOrderProduct = function(item) {
       return ProductService.canOrderProduct(item);
+    };
+
+    $scope.saveNote = function(itemNumber, note) {
+      ProductService.updateItemNote(itemNumber, note).then(function() {
+        $scope.displayMessage('success', 'Successfully updated note.');
+      }, function() {
+        $scope.displayMessage('error', 'Error updating note.');
+      });
+    };
+
+    $scope.deleteNote = function(itemNumber) {
+      ProductService.deleteItemNote(itemNumber).then(function() {
+        item.notes = null;
+        $scope.item.notes = null;
+        $scope.displayMessage('success', 'Successfully deleted note.');
+      }, function() {
+        $scope.displayMessage('error', 'Error deleting note.');
+      });
+    };
+
+    $scope.cancelChanges = function() {
+      $scope.item = angular.copy(item);
     };
   }]);
