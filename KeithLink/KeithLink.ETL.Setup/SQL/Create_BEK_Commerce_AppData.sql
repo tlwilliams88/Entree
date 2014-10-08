0311c95960c530b3494ffed16fdb8e51288b18a3
bekco\jmmcmillan <jasonmcmillan@benekeith.com>
2014-10-08 15:13:27 -0500

    updated ETL SQL scripts

--- KeithLink/KeithLink.ETL.Setup/SQL/Create_BEK_Commerce_AppData.sql
+++ KeithLink/KeithLink.ETL.Setup/SQL/Create_BEK_Commerce_AppData.sql
@@ -423,6 +423,68 @@ CREATE TABLE ETL.Staging_FSE_ProductAllergens (
 	, LevelOfContainment varchar(20)
 );
 GO
+
+CREATE TABLE [ETL].[Staging_ProprietaryItem](
+	[Action] [varchar](1) NULL,
+	[CompanyNumber] [varchar](3) NULL,
+	[DivisionNumber] [varchar](3) NULL,
+	[DepartmentNumber] [varchar](3) NULL,
+	[ProprietaryNumber] [varchar](10) NULL,
+	[ItemNumber] [varchar](10) NULL
+) ON [PRIMARY]
+
+GO
+
+CREATE TABLE [ETL].[Staging_ProprietaryCustomer](
+	[Action] [varchar](1) NULL,
+	[CompanyNumber] [varchar](3) NULL,
+	[DivisionNumber] [varchar](3) NULL,
+	[DepartmentNumber] [varchar](3) NULL,
+	[ProprietaryNumber] [varchar](10) NULL,
+	[CustomerNumber] [varchar](10) NULL
+) ON [PRIMARY]
+
+GO
+
+CREATE TABLE [ETL].[Staging_BidContractHeader](
+	[Action] [varchar](1) NULL,
+	[CompanyNumber] [varchar](3) NULL,
+	[DivisionNumber] [varchar](3) NULL,
+	[DepartmentNumber] [varchar](3) NULL,
+	[BidNumber] [varchar](10) NULL,
+	[BidDescription] [varchar](40) NULL
+) ON [PRIMARY]
+
+GO
+
+
+CREATE TABLE [ETL].[Staging_BidContractDetail](
+	[Action] [varchar](1) NULL,
+	[CompanyNumber] [varchar](3) NULL,
+	[DivisionNumber] [varchar](3) NULL,
+	[DepartmentNumber] [varchar](3) NULL,
+	[BidNumber] [varchar](10) NULL,
+	[ItemNumber] [varchar](10) NULL,
+	[BidLineNumber] [varchar](5) NULL,
+	[CategoryNumber] [varchar](5) NULL,
+	[CategoryDescription] [varchar](40) NULL,
+	[ForceEachOrCaseOnly] [varchar](1) NULL
+) ON [PRIMARY]
+
+GO
+
+CREATE TABLE [ETL].[Staging_CustomerBid](
+	[Action] [varchar](1) NULL,
+	[CompanyNumber] [varchar](3) NULL,
+	[DivisionNumber] [varchar](3) NULL,
+	[DepartmentNumber] [varchar](3) NULL,
+	[CustomerNumber] [varchar](10) NULL,
+	[PriorityNumber] [varchar](3) NULL,
+	[BidNumber] [varchar](10) NULL
+) ON [PRIMARY]
+
+GO
+
 CREATE PROCEDURE [ETL].[ReadBranches]
 	
 AS
