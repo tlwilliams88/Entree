b7fc14db23f9a0d2463f8fbc037ea4f6b1a42071
Josh Tirey <jtirey@credera.com>
2014-10-14 15:27:27 -0500

    Changed saveasorder return to an object instead of string

---
+++ KeithLink/KeithLink.Svc.Core/Models/Orders/NewOrderReturn.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeithLink.Svc.Core.Models.Orders
+{
+	[DataContract(Name = "NewOrderReturn")]
+	public class NewOrderReturn
+	{
+		[DataMember(Name = "ordernumber")]
+		public string OrderNumber { get; set; }
+	}
+}
