c12d9f69b683cef38c4384b41a0fd646e5a9f2dc
bekco\jwames <jwames@benekeith.com>
2014-10-16 17:00:15 -0500

    build classes for order history

---
+++ KeithLink/KeithLink.Svc.Core/Models/Orders/History/OrderHistoryDetail.cs
@@ -0,0 +1,40 @@
+﻿using KeithLink.Svc.Core.Enumerations.Order;
+using System;
+using System.Runtime.Serialization;
+
+namespace KeithLink.Svc.Core.Models.Orders.History {
+    [DataContract()]
+    public class OrderHistoryDetail {
+        #region properties
+        [DataMember()]
+        public int LineNumber { get; set; }
+
+        [DataMember()]
+        public string ItemNumber { get; set; }
+
+        [DataMember()]
+        public int OrderQuantity { get; set; }
+
+        [DataMember()]
+        public int ShippedQuantity { get; set; }
+
+        [DataMember()]
+        public UnitOfMeasure UnitOfMeasure { get; set; }
+
+        [DataMember()]
+        public bool CatchWeight { get; set; }
+
+        [DataMember()]
+        public bool ItemDeleted { get; set; }
+
+        [DataMember()]
+        public string SubbedOriginalItemNumber { get; set; }
+
+        [DataMember()]
+        public string ReplacedOriginalItemNumber { get; set; }
+
+        [DataMember()]
+        public string ItemStatus { get; set; }
+        #endregion
+    }
+}
---
+++ KeithLink/KeithLink.Svc.Core/Models/Orders/History/OrderHistoryHeader.cs
@@ -0,0 +1,40 @@
+﻿using KeithLink.Svc.Core.Enumerations.Order;
+using System;
+using System.Runtime.Serialization;
+
+namespace KeithLink.Svc.Core.Models.Orders.History {
+    [DataContract()]
+    public class OrderHistoryHeader {
+        #region properties
+        [DataMember()]
+        public OrderSource OrderSystem { get; set; }
+
+        [DataMember()]
+        public string BranchId { get; set; }
+
+        [DataMember()]
+        public string CustomerNumber { get; set; }
+
+        [DataMember()]
+        public string InvoiceNumber { get; set; }
+
+        [DataMember()]
+        public DateTime DeliveryDate { get; set; }
+
+        [DataMember()]
+        public string PONumber { get; set; }
+
+        [DataMember()]
+        public string ControlNumber { get; set; }
+
+        [DataMember()]
+        public string OrderStatus { get; set; }
+
+        [DataMember()]
+        public bool FutureItems { get; set; }
+
+        [DataMember()]
+        public bool ErrorStatus { get; set; }
+        #endregion
+    }
+}
