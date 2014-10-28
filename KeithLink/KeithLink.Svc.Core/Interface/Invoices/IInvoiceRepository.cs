92f130370b44389ef28d59592864fc8977c0557e
Josh Tirey <jtirey@credera.com>
2014-10-28 12:33:49 -0500

    Added Invoice EF entities

---
+++ KeithLink/KeithLink.Svc.Core/Interface/Invoices/IInvoiceRepository.cs
@@ -0,0 +1,15 @@
+﻿using KeithLink.Svc.Core.Models.EF;
+using KeithLink.Svc.Core.Models.Profile;
+using KeithLink.Svc.Core.Models.SiteCatalog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeithLink.Svc.Core.Interface.Invoices
+{
+	public interface IInvoiceRepository : IBaseEFREpository<Invoice>
+	{
+	}
+}
---
+++ KeithLink/KeithLink.Svc.Core/Models/EF/InvoiceItem.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeithLink.Svc.Core.Models.EF
+{
+	public class InvoiceItem: BaseEFModel
+	{
+		[Required]
+		public int LineItem { get; set; }
+		public string InvoiceType { get; set; }
+		public DateTime InvoiceDate { get; set; }
+		public decimal AmountDue { get; set; }
+		public int DeleteFlag { get; set; }
+		public int LineNumber { get; set; }
+		public int QuantityOrdered { get; set; }
+		public int QuantityShipped { get; set; }
+		public string BrokenCaseCode { get; set; }
+		public string CatchWeightCode { get; set; }
+		public decimal ExtCatchWeight { get; set; }
+		public decimal ItemPrice { get; set; }
+		public string PriceBookNumber { get; set; }
+		public decimal ItemPriceSalesRep { get; set; }
+		public decimal ExtSalesRepAmount { get; set; }
+		public decimal ExtSalesGross { get; set; }
+		public decimal ExtSalesNet { get; set; }
+		public int VendorNumber { get; set; }
+		public string CustomerPO { get; set; }
+		public string CombinedStatmentCustomer { get; set; }
+		public string PriceBook { get; set; }
+
+	}
+}
