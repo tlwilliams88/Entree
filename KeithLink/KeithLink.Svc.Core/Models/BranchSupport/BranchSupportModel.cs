ae3522c361348a087a092c055971ecd90f68b910
Josh Tirey <jtirey@credera.com>
2014-10-31 07:55:44 -0500

    added branch support EF

---
+++ KeithLink/KeithLink.Svc.Core/Models/BranchSupport/BranchSupportModel.cs
@@ -0,0 +1,27 @@
+﻿using KeithLink.Svc.Core.Models.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace KeithLink.Svc.Core.Models.BranchSupport
+{
+	[DataContract(Name = "BranchSupport")]
+	public class BranchSupportModel
+	{
+		[DataMember(Name = "branchname")]
+		public string BranchName { get; set; }
+		[DataMember(Name = "branchid")]
+		public string BranchId { get; set; }
+		[DataMember(Name = "supportphonenumber")]
+		public string SupportPhoneNumber { get; set; }
+		[DataMember(Name = "tollfreenumber")]
+		public string TollFreeNumber { get; set; }
+		[DataMember(Name = "email")]
+		public string Email { get; set; }
+		
+	}
+}
--- KeithLink/KeithLink.Svc.Impl/Repository/EF/Operational/BEKDBContext.cs
+++ KeithLink/KeithLink.Svc.Impl/Repository/EF/Operational/BEKDBContext.cs
@@ -22,6 +22,7 @@ namespace KeithLink.Svc.Impl.Repository.EF.Operational
 		public DbSet<OrderHistoryHeader> OrderHistoryHeaders { get; set; }
 		public DbSet<Invoice> Invoices { get; set; }
 		public DbSet<InvoiceItem> InvoiceItems { get; set; }
+		public DbSet<BranchSupport> BranchSupports { get; set; }
 
 
 		protected override void OnModelCreating(DbModelBuilder modelBuilder)
@@ -32,6 +33,7 @@ namespace KeithLink.Svc.Impl.Repository.EF.Operational
 			modelBuilder.Entity<OrderHistoryDetail>().ToTable("OrderHistoryDetail", schemaName: "Orders").Property(o => o.Id).HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
 			modelBuilder.Entity<Invoice>().ToTable("Invoices", schemaName: "Invoice").Property(o => o.Id).HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
 			modelBuilder.Entity<InvoiceItem>().ToTable("InvoiceItems", schemaName: "Invoice").Property(o => o.Id).HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+			modelBuilder.Entity<BranchSupport>().ToTable("BranchSupports", schemaName: "BranchSupport").Property(o => o.Id).HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
 
 		}
 
