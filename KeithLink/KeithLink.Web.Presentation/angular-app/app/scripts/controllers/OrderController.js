413c1975d4b54cc8ca857606475b9669e21b2b94
Maria Knabe <mknabe@credera.com>
2014-11-04 11:08:16 -0600

    notification details modal, order history filtering/sorting and mobile'

--- KeithLink/KeithLink.Web.Presentation/angular-app/app/scripts/controllers/OrderController.js
+++ KeithLink/KeithLink.Web.Presentation/angular-app/app/scripts/controllers/OrderController.js
@@ -4,6 +4,9 @@ angular.module('bekApp')
 .controller('OrderController', ['$scope', '$state', 'orders',
   function ($scope, $state, orders) {
 
+  $scope.sortBy = 'createddate';
+  $scope.sortOrder = true;
+
   $scope.orders = orders;
 
 
