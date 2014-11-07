e2ee0f5dc5f84e5a34c963019047fff1c5e574a5
Maria Knabe <mknabe@credera.com>
2014-11-07 09:04:23 -0600

    hook up messages with dummy data

--- KeithLink/KeithLink.Web.Presentation/angular-app/app/scripts/controllers/NotificationsController.js
+++ KeithLink/KeithLink.Web.Presentation/angular-app/app/scripts/controllers/NotificationsController.js
@@ -1,23 +1,12 @@
 'use strict';
 
 angular.module('bekApp')
-  .controller('NotificationsController', ['$scope', '$modal',
-    function ($scope, $modal) {
+  .controller('NotificationsController', ['$scope', '$modal', 'notifications',
+    function ($scope, $modal, notifications) {
     
-  $scope.notifications = [{
-    'type': 'order',
-    'message': 'Order #12345 has been confirmed.',
-    'datetime': '2014-03-09'
-  }, {
-    'type': 'invoice',
-    'message': 'Invoice #20123 is past due.',
-    'datetime': '2014-03-09'
-  }, {
-    'type': 'marketing',
-    'message': 'Recall on Pilgrim Chicken Breast due to E. Coli!',
-    'datetime': '2014-05-12',
-    'extmessage': 'Recall on Pilgrim Chicken Breast due to E. Coli! Extended message!'
-  }];
+  $scope.notifications = notifications;
+  $scope.sortBy = 'messagecreatedutc';
+  $scope.sortOrder = true;
 
   $scope.showAdditionalInfo = function(notification) {
     var modalInstance = $modal.open({
