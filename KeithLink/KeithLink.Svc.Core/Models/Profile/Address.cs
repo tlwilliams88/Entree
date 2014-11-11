e4da0dd240e350bc71777c958b183a9e0b3ec30f
gsalazar <gsalazar@credera.com>
2014-11-10 23:52:19 -0700

    Add address and point of contact to user profile

---
+++ KeithLink/KeithLink.Svc.Core/Models/Profile/Address.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace KeithLink.Svc.Core.Models.Profile
+{
+    [DataContract(Name="address")]
+    public class Address
+    {
+        [DataMember(Name="street")]
+        public string StreetAddress { get; set; }
+
+        [DataMember(Name="city")]
+        public string City { get; set; }
+
+        [DataMember(Name="regioncode")]
+        public string RegionCode { get; set; }
+
+        [DataMember(Name="postalcode")]
+        public string PostalCode { get; set; }
+    }
+}
