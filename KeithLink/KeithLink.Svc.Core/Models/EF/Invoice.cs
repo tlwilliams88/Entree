4f49b68f237959f3549859444cff687256c0207d
Josh Tirey <jtirey@credera.com>
2014-11-12 13:07:14 -0600

    EnumUtil helper

--- KeithLink/KeithLink.Svc.Core/Models/EF/Invoice.cs
+++ KeithLink/KeithLink.Svc.Core/Models/EF/Invoice.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
@@ -10,8 +11,11 @@ namespace KeithLink.Svc.Core.Models.EF
 {
 	public enum InvoiceType
 	{
+		[Description("Invoice")]
 		Invoice,
+		[Description("Credit Memo")]
 		CreditMemo,
+		[Description("Adjustment")]
 		Adjustment
 	}
 
