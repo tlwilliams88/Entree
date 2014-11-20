38ffcf677daa0a547a293c740880dac4f1bcc528
Robert Hedges <rhedges@credera.com>
2014-11-20 16:56:42 -0600

    Added files to phonegap project, removed UserProfile ajax call in Customer and User account profiles for notification preference settings, fixed problem with filters directory not syncing to phonegap application

--- KeithLink/KeithLink.Web.Presentation/phonegap-app/BEKPhoneGap/www/scripts/configenv.js
+++ KeithLink/KeithLink.Web.Presentation/phonegap-app/BEKPhoneGap/www/scripts/configenv.js
@@ -1,7 +1,7 @@
-"use strict";
+'use strict';
 
  angular.module('configenv', [])
 
-.constant('ENV', {name:'dev',apiKey:'web_test_v1',apiEndpoint:'https://shopqa.benekeith.com/api',loggingEnabled:true,username:'sabroussard@somecompany.com',password:'L1ttleStev1e'})
+.constant('ENV', {name:'dev',apiKey:'web_test_v1',apiEndpoint:'https://shopqa.benekeith.com/api',loggingEnabled:true,username:'qauser@qa.com',password:'Ab21345'})
 
 ;
