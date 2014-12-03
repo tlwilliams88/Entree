2dd31393ba5f9dc9d1c7fd8a4ef896e1bb8bb42e
Matthew Joiner <mdjoiner@benekeith.com>
2014-12-03 15:12:12 -0600

    Merged current import logic to trunk

---
+++ KeithLink/KeithLink.Svc.Core/Models/Orders/OrderImportFileModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeithLink.Svc.Core.Models.Orders {
+    public class OrderImportFileModel {
+
+        public OrderImportFileModel() {
+            Stream = new System.IO.MemoryStream();
+            Options = new OrderImportOptions();
+        }
+        
+        public string FileName { get; set; }
+        public string Contents { get; set; }
+        public System.IO.MemoryStream Stream { get; set; }
+        public OrderImportOptions Options { get; set; }
+
+    }
+}
