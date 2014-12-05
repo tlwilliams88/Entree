31e88b59c4cdaea6c576ef8680946dc62b7a48de
Jeremy Ames <jwames@benekeith.com>
2014-12-05 15:24:47 -0600

    add unit test

---
+++ KeithLink/KeithLink.Svc.Tests/Repositories/Order/PurchaseOrderRepositoryTest.cs
@@ -0,0 +1,25 @@
+﻿using KeithLink.Svc.Impl.Repository.Orders;
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace KeithLink.Svc.Test.Repositories.Order {
+    [TestClass]
+    public class PurchaseOrderRepositoryTest {
+        #region attributes
+        private PurchaseOrderRepositoryImpl _repo;
+        #endregion
+
+        #region ctor
+        public PurchaseOrderRepositoryTest() {
+            _repo = new PurchaseOrderRepositoryImpl();
+        }
+        #endregion
+
+        #region methods
+        [TestMethod]
+        public void SuccessfulGetOrderByInvoiceNumber() {
+            _repo.ReadPurchaseOrderByInvoice("FAM", "01203045");
+        }
+        #endregion
+    }
+}
