d7c1dfc0c91120f6cbb8416f40166c70cc3f843f
Jeremy Ames <jwames@benekeith.com>
2014-12-09 16:00:19 -0600

    add internal order history logic and interface

---
+++ KeithLink/KeithLink.Svc.Core/Interface/Orders/History/IInternalOrderHistoryLogic.cs
@@ -0,0 +1,9 @@
+﻿using KeithLink.Svc.Core.Models.SiteCatalog;
+using KeithLink.Svc.Core.Models.Orders;
+using System.Collections.Generic;
+
+namespace KeithLink.Svc.Core.Interface.Orders.History {
+    public interface IInternalOrderHistoryLogic {
+        List<Order> GetOrders(UserSelectedContext customerInfo);
+    }
+}
