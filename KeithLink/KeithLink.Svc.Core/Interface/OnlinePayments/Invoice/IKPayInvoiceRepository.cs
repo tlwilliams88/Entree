e08e33c20a80390ac5f1ef2332e8c87f8286c1ce
Jeremy Ames <jwames@benekeith.com>
2014-12-23 15:30:08 -0600

    add queue connection exception

--- KeithLink/KeithLink.Svc.Core/Interface/OnlinePayments/Invoice/IKPayInvoiceRepository.cs
+++ KeithLink/KeithLink.Svc.Core/Interface/OnlinePayments/Invoice/IKPayInvoiceRepository.cs
@@ -1,4 +1,4 @@
-﻿using KeithLink.Svc.Core.Models.OnlinePayments.Invoice.EF;
+﻿using EF = KeithLink.Svc.Core.Models.OnlinePayments.Invoice.EF;
 using KeithLink.Svc.Core.Models.OnlinePayments.Payment.EF;
 using KeithLink.Svc.Core.Models.SiteCatalog;
 using System;
@@ -8,11 +8,19 @@ namespace KeithLink.Svc.Core.Interface.OnlinePayments.Invoice {
     public interface IKPayInvoiceRepository {
         void DeleteInvoice(string division, string customerNumber, string invoiceNumber);
 
-        List<Models.OnlinePayments.Invoice.EF.Invoice> GetInvoiceTransactoin(string division, string customerNumber, string invoiceNumber);
+        //List<EF.Invoice> GetAllOpenInvoices(string division, string customerNumber);
 
-        List<Models.OnlinePayments.Invoice.EF.Invoice> GetMainInvoices(string division, string customerNumber);
+        //List<EF.Invoice> GetAllPaidInvoices(string division, string customerNumber);
 
-		Core.Models.OnlinePayments.Invoice.EF.Invoice GetInvoiceHeader(string division, string customerNumber, string invoiceNumber);
+        //List<EF.Invoice> GetAllPastDueInvoices(string division, string customerNumber);
+
+        //List<EF.Invoice> GetAllPayableInvoices(string division, string customerNumber);
+
+        List<EF.Invoice> GetInvoiceTransactoin(string division, string customerNumber, string invoiceNumber);
+
+        List<EF.Invoice> GetMainInvoices(string division, string customerNumber);
+
+		EF.Invoice GetInvoiceHeader(string division, string customerNumber, string invoiceNumber);
 
 		long GetNextConfirmationId();
 
