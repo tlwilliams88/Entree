58e64fd4371990cb36c5505a989616f98e797403
Maria Knabe <mknabe@credera.com>
2015-01-13 09:56:12 -0600

    separate account users and customer users on admin page, change account wording

--- KeithLink/KeithLink.Web.Presentation/angular-app/app/scripts/controllers/MenuController.js
+++ KeithLink/KeithLink.Web.Presentation/angular-app/app/scripts/controllers/MenuController.js
@@ -12,133 +12,153 @@ angular.module('bekApp')
   .controller('MenuController', ['$scope', '$state', '$modal', '$window', 'branches', 'UserProfileService', 'AuthenticationService', 'AccessService', 'LocalStorage', 'CartService', 'NotificationService',
     function ($scope, $state, $modal, $window, branches, UserProfileService, AuthenticationService, AccessService, LocalStorage, CartService, NotificationService) {
 
-    $scope.$state = $state;
-    $scope.userBar = {};
-    $scope.userBar.universalSearchTerm = '';
+  $scope.$state = $state;
 
-    $scope.messageText = 'Hello world!';
-    $scope.displayGlobalMessage = true;
+  // define search term in user bar so it can be cleared in the SearchController after a user searches
+  $scope.userBar = {};
+  $scope.userBar.universalSearchTerm = '';
+  $scope.branches = branches;
 
-    $scope.userProfile = LocalStorage.getProfile();
-    $scope.branches = branches;
-    refreshAccessPermissions();
-    $scope.userBar.userNotificationsCount = NotificationService.userNotificationsCount;
+  // global notification at the top of all pages
+  // TODO: Global messaging backend?
+  $scope.messageText = 'Hello world!';
+  $scope.displayGlobalMessage = true;
 
-    // get default selected user context
-    if ($scope.isOrderEntryCustomer) {
-      $scope.selectedUserContext = LocalStorage.getCurrentCustomer();
-    } else {
-      $scope.selectedUserContext = LocalStorage.getBranchId();
-    }
+  $scope.userProfile = LocalStorage.getProfile();
+  refreshAccessPermissions();
+  $scope.userBar.userNotificationsCount = NotificationService.userNotificationsCount;
+ 
+  /**********
+  SELECTED USER CONTEXT
+  **********/
 
-    $scope.customerInfiniteScroll = {
-      from: 0,
-      size: 15
-    };
+  $scope.setSelectedUserContext = function(selectedUserContext) {
+    $scope.selectedUserContext = selectedUserContext;
+  };
 
-    // populates upper-left customer dropdown infinite scroll
-    $scope.customerSelectOptions = {
-      query: function (query){
-        $scope.customerInfiniteScroll.from = (query.page - 1) * $scope.customerInfiniteScroll.size;
+  // get default selected user context
+  if ($scope.isOrderEntryCustomer) {
+    $scope.setSelectedUserContext(LocalStorage.getCurrentCustomer());
+  } else {
+    $scope.setSelectedUserContext(LocalStorage.getBranchId());
+  }
 
-        UserProfileService.searchUserCustomers(query.term, $scope.customerInfiniteScroll.size, $scope.customerInfiniteScroll.from).then(function(data) {
-          // convert data to match select2 data object
-          var obj = {
-            results: [],
-            more: query.page * 15 < data.totalResults // boolean if there are more results to display using infinite scroll
-          };
+  $scope.customerInfiniteScroll = {
+    from: 0,
+    size: 15
+  };
 
-          data.results.forEach(function(customer) {
-            obj.results.push({
-              id: customer.customerNumber, // value
-              text: customer.displayname,  // display text
-              customer: customer
-            });
+  // populates upper-left customer dropdown infinite scroll
+  $scope.customerSelectOptions = {
+    query: function (query){
+      $scope.customerInfiniteScroll.from = (query.page - 1) * $scope.customerInfiniteScroll.size;
+
+      UserProfileService.searchUserCustomers(query.term, $scope.customerInfiniteScroll.size, $scope.customerInfiniteScroll.from).then(function(data) {
+        // convert data to match select2 data object
+        var obj = {
+          results: [],
+          more: query.page * 15 < data.totalResults // boolean if there are more results to display using infinite scroll
+        };
+
+        data.results.forEach(function(customer) {
+          obj.results.push({
+            id: customer.customerNumber, // value
+            text: customer.displayname,  // display text
+            customer: customer
           });
-          query.callback(obj);
         });
-      }
-    };
-
-    // for guest users
-    $scope.changeBranch = function() {
-      LocalStorage.setSelectedBranchInfo($scope.selectedUserContext);
-
-      $state.transitionTo($state.current, $state.params, {
-        reload: true,
-        inherit: false,
-        notify: true
+        query.callback(obj);
       });
-    };
-
-    // for order-entry customers
-    $scope.changeCustomerLocation = function(selectedUserContext) {
-      LocalStorage.setSelectedCustomerInfo(selectedUserContext);
-
-      angular.copy([], CartService.shipDates);
-
-      $state.transitionTo($state.current, $state.params, {
-        reload: true,
-        inherit: false,
-        notify: true
-      });
-    };
-
-    $scope.logout = function() {
-      AuthenticationService.logout();
-      // refreshAccessPermissions();
-
-      $state.transitionTo('register');
-      $scope.displayUserMenu = false;
-    };
-
-    $scope.print = function () {
-      window.print();
-    };
-
-      $scope.backfunction = function(){
-        console.log('back');
-        $window.history.back();
-      };
-
-    $scope.openTechnicalSupportModal = function() {
-      var modalInstance = $modal.open({
-        templateUrl: 'views/modals/technicalsupportmodal.html',
-        controller: 'TechnicalSupportModalController',
-        windowClass: 'color-background-modal',
-        resolve: {
-          branch: function() {
-            var branchFound,
-              branchId = LocalStorage.getBranchId();
-            angular.forEach(branches, function(branch) {
-              if (branch.id.toUpperCase() === branchId.toUpperCase()) {
-                branchFound = branch;
-              }
-            });
-            return branchFound;
-          }
-        }
-      });
-    };
-
-    function refreshAccessPermissions() {
-      $scope.isLoggedIn = AccessService.isLoggedIn();
-      $scope.isOrderEntryCustomer = AccessService.isOrderEntryCustomer();
-
-      // $scope.isInternalUser = AccessService.isInternalUser();
-      // $scope.isBekAdmin = AccessService.isBekAdmin();
-
-      $scope.isDsr = AccessService.isDsr();
-
-      $scope.canBrowseCatalog = AccessService.canBrowseCatalog();
-      $scope.canSeePrices = AccessService.canSeePrices();
-      $scope.canManageLists = AccessService.canManageLists();
-      $scope.canCreateOrders = AccessService.canCreateOrders();
-      $scope.canSubmitOrders = AccessService.canSubmitOrders();
-      $scope.canPayInvoices = AccessService.canPayInvoices();
-      $scope.canManageAccount = AccessService.canManageAccount();
-      $scope.canManageAccounts = AccessService.canManageAccounts();
-
-
     }
-  }]);
+  };
+
+  function refreshPage() {
+    $state.transitionTo($state.current, $state.params, {
+      reload: true,
+      inherit: false,
+      notify: true
+    });
+  }
+
+  // change context menu selection for guest users
+  $scope.changeBranch = function() {
+    LocalStorage.setSelectedBranchInfo($scope.selectedUserContext);
+    refreshPage();
+  };
+
+  // change context menu selection for order-entry customers
+  $scope.changeCustomerLocation = function(selectedUserContext) {
+    LocalStorage.setSelectedCustomerInfo(selectedUserContext);
+
+    // reset ship dates in cache
+    angular.copy([], CartService.shipDates);
+
+    refreshPage();
+  };
+
+  /**********
+  MENU BUTTON CLICK HANDLERS
+  **********/
+
+  $scope.logout = function() {
+    AuthenticationService.logout();
+
+    $state.transitionTo('register');
+    $scope.displayUserMenu = false;
+  };
+
+  $scope.print = function () {
+    window.print();
+  };
+
+  $scope.navigateBack = function(){
+    $window.history.back();
+  };
+
+  $scope.openTechnicalSupportModal = function() {
+    var modalInstance = $modal.open({
+      templateUrl: 'views/modals/technicalsupportmodal.html',
+      controller: 'TechnicalSupportModalController',
+      windowClass: 'color-background-modal',
+      resolve: {
+        branch: function() {
+          var branchFound,
+            branchId = LocalStorage.getBranchId();
+          angular.forEach(branches, function(branch) {
+            if (branch.id.toUpperCase() === branchId.toUpperCase()) {
+              branchFound = branch;
+            }
+          });
+          return branchFound;
+        }
+      }
+    });
+  };
+
+  /**********
+  SET USER PERMISSIONS
+  these are used in all sub-states to show/hide accessible info
+  also used in state.js to determine if the user has access to a state
+  **********/
+
+  function refreshAccessPermissions() {
+    $scope.isLoggedIn = AccessService.isLoggedIn();
+    $scope.isOrderEntryCustomer = AccessService.isOrderEntryCustomer();
+
+    // $scope.isInternalUser = AccessService.isInternalUser();
+    // $scope.isBekAdmin = AccessService.isBekAdmin();
+
+    $scope.isDsr = AccessService.isDsr();
+
+    $scope.canBrowseCatalog = AccessService.canBrowseCatalog();
+    $scope.canSeePrices = AccessService.canSeePrices();
+    $scope.canManageLists = AccessService.canManageLists();
+    $scope.canCreateOrders = AccessService.canCreateOrders();
+    $scope.canSubmitOrders = AccessService.canSubmitOrders();
+    $scope.canPayInvoices = AccessService.canPayInvoices();
+    $scope.canManageAccount = AccessService.canManageAccount();
+    $scope.canManageAccounts = AccessService.canManageAccounts();
+
+
+  }
+}]);
