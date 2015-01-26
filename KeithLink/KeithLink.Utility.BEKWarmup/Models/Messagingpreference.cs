fa9e3fe30ab9385655aa0717005342e0f975e605
Josh Tirey <jtirey@credera.com>
2015-01-26 11:34:07 -0600

    Added BEKWarmup script to solution

---
+++ KeithLink/KeithLink.Utility.BEKWarmup/Models/Messagingpreference.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeithLink.Utility.BEKWarmup.Models
+{
+	public class Messagingpreference
+	{
+		public string customerNumber { get; set; }
+		public List<Preference> preferences { get; set; }
+	}
+}
