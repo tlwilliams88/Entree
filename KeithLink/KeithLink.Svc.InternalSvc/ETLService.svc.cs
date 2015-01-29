1a7390c92d8453177dec48077df9cecb94efa0da
Jason McMillan <jmmcmillan@benekeith.com>
2015-01-29 14:19:59 -0600

    re enable customer load (testing)

--- KeithLink/KeithLink.Svc.InternalSvc/ETLService.svc.cs
+++ KeithLink/KeithLink.Svc.InternalSvc/ETLService.svc.cs
@@ -39,10 +39,10 @@ namespace KeithLink.Svc.InternalSvc
 
         public bool ProcessCustomerData()
         {
-            /*
+            
             Task.Factory.StartNew(() => customerLogic.ImportCustomersToOrganizationProfile()).ContinueWith((t) =>
             { (new ErrorHandler()).HandleError(t.Exception); }, TaskContinuationOptions.OnlyOnFaulted);
-            */
+            
             Task.Factory.StartNew(() => customerLogic.ImportDsrInfo()).ContinueWith((t) =>
             { (new ErrorHandler()).HandleError(t.Exception); }, TaskContinuationOptions.OnlyOnFaulted);
 
