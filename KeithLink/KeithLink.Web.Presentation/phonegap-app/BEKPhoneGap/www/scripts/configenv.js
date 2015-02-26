8ce9f6e244638a30d939cba556185511c343cdd6
Maria Knabe <mknabe@credera.com>
2015-02-26 10:31:23 -0600

    add correct logo to phonegap app

--- KeithLink/KeithLink.Web.Presentation/phonegap-app/BEKPhoneGap/www/scripts/configenv.js
+++ KeithLink/KeithLink.Web.Presentation/phonegap-app/BEKPhoneGap/www/scripts/configenv.js
@@ -2,6 +2,6 @@
 
  angular.module('configenv', [])
 
-.constant('ENV', {name:'dev',apiKey:'web_test_v1',apiEndpoint:'https://shopqa.benekeith.com/api',loggingEnabled:true,username:'qauser@qa.com',password:'Ab12345',googleAnalytics:'UA-58495462-2',mobileApp: true})
+.constant('ENV', {name:'dev',apiKey:'web_test_v1',apiEndpoint:'https://shopqa.benekeith.com/api',loggingEnabled:true,username:'bek.qa.user@gmail.com',password:'Ab12345',googleAnalytics:'UA-58495462-2',mobileApp: true})
 
 ;
