df06786222ddfe79c157f141cda4a72aefd7ef48
unknown <mdjoiner@CORPMISDEV2A.bekco.com>
2015-03-23 09:09:29 -0500

    PowerMenu changes

---
+++ KeithLink/KeithLink.Svc.Core/Models/PowerMenu/PowerMenuRequest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+using System.Xml.Serialization;
+
+namespace KeithLink.Svc.Core.Models.PowerMenu {
+    [XmlRoot("PowerNetUserRequest")]
+    public class PowerMenuRequest {
+        #region attributes
+
+        public enum Operations {
+            Create,
+            Delete,
+            Edit
+        }
+
+        #endregion
+
+        #region constructor
+
+        public PowerMenuRequest() {
+            Login = new PowerMenuRequestLogin();
+            User = new PowerMenuRequestUser();
+            Operation = Operations.Edit;
+        }
+
+        #endregion
+
+        #region properties
+
+        public PowerMenuRequestLogin Login { get; set; }
+        public PowerMenuRequestUser User { get; set; }
+        public Operations Operation { get; set; }
+
+        #endregion
+    }
+}
---
+++ KeithLink/KeithLink.Svc.Tests/PowerMenu/PowerMenuRequest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Xml.Serialization;
+using System.Xml;
+using System.Runtime.Serialization;
+using KeithLink.Svc.Core.Models.PowerMenu;
+using System.IO;
+
+namespace KeithLink.Svc.Test.PowerMenu {
+    [TestClass]
+    public class PowerMenuRequestTest {
+
+        [TestMethod]
+        public void PowerMenuSerializingTest() {
+            PowerMenuRequest pm = new PowerMenuRequest();
+
+            pm.Login.AdminUsername = "MATTTEST";
+            pm.Login.AdminPassword = "MATTTEST";
+
+            pm.User.Username = "mj@thismat.com";
+            pm.User.Password = "RANDOM";
+            pm.User.EmailAddress = "mj@thismat.com";
+            pm.User.ContactName = "Matt";
+            pm.User.CustomerNumber = "123456";
+            pm.User.PhoneNumber = "817-507-5415";
+            pm.User.State = "TX";
+            pm.Operation = PowerMenuRequest.Operations.Edit;
+
+
+            XmlSerializer s = new XmlSerializer( typeof( PowerMenuRequest ) );
+
+            //TextWriter w = new StreamWriter(@"C:\test\powermenu\testxml.xml");
+            TextWriter w = new StringWriter();
+
+            try {
+                s.Serialize( w, pm );
+            } catch (Exception ex) {
+                Assert.Fail( ex.Message );
+            }
+           
+        }
+    }
+}
