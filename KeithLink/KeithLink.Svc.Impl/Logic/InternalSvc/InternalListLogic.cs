a591e10d4d2170852ce2477e74952e899a9cb722
Josh Tirey <jtirey@credera.com>
2015-03-31 09:45:08 -0500

    Additional list item position fix

--- KeithLink/KeithLink.Svc.Impl/Logic/InternalSvc/InternalListLogic.cs
+++ KeithLink/KeithLink.Svc.Impl/Logic/InternalSvc/InternalListLogic.cs
@@ -109,7 +109,8 @@ namespace KeithLink.Svc.Impl.Logic.InternalSvc
 			if (list.Items == null)
 				list.Items = new List<ListItem>();
 			else
-				nextPosition = list.Items.Max(i => i.Position);
+				if(list.Items.Any())
+					nextPosition = list.Items.Max(i => i.Position);
 
 			foreach (var item in items)
 			{
@@ -215,7 +216,7 @@ namespace KeithLink.Svc.Impl.Logic.InternalSvc
 			var newList = list.ToEFList();
 
 			//Set initial positions
-			if (newList.Items != null && newList.Items.Max(i => i.Position) == 0)
+			if (newList.Items != null && newList.Items.Any() && newList.Items.Max(i => i.Position) == 0)
 			{
 				var position = 0;
 				foreach (var item in newList.Items)
