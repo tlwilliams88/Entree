459b99ac7fbfa60a02dcbcaf42fd9732b4bf7782
Jeremy Ames <jwames@benekeith.com>
2015-04-06 16:19:41 -0500

    add new logging files for keithpay

---
+++ KeithLink/KeithLink.Svc.Core/Interface/OnlinePayments/Log/IKPayLogRepository.cs
@@ -0,0 +1,7 @@
+﻿using System;
+
+namespace KeithLink.Svc.Core.Interface.OnlinePayments.Log {
+    public interface IKPayLogRepository {
+        void Write(string userName, string message);
+    }
+}
