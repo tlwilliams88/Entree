c1d7fbc3237a83cf1f0e7dfdfab895d79b90cf54
Jeremy Ames <jwames@benekeith.com>
2015-04-08 09:55:12 -0500

    create a notification parser utility program for checking ETA notifications

--- KeithLink/KeithLink.Svc.Impl/Logic/Messaging/EtaNotificationHandlerImpl.cs
+++ KeithLink/KeithLink.Svc.Impl/Logic/Messaging/EtaNotificationHandlerImpl.cs
@@ -53,75 +53,7 @@ namespace KeithLink.Svc.Impl.Logic.Messaging
         }
         #endregion
 
-        public void ProcessNotification(Core.Models.Messaging.Queue.BaseNotification notification)
-        {
-            if (notification.NotificationType != NotificationType.Eta)
-                throw new ApplicationException("notification/handler type mismatch");
-
-            EtaNotification eta = (EtaNotification)notification;
-
-            // load up recipients, customer and message
-            eventLogRepository.WriteInformationLog("Received ETA Message with " + eta.Orders.Count + " orders");
-            List<string> invoiceNumbers = eta.Orders.Select(x => x.OrderId).ToList();
-            var orders = orderHistoryRepository.Read(x => invoiceNumbers.Contains(x.InvoiceNumber)).ToList(); // get all orders for order ETAs
-            
-            foreach(OrderHistoryHeader order in orders)
-            {
-                try
-                {
-                    var etaInfo = eta.Orders.Where(o => o.OrderId.Equals(order.InvoiceNumber) && o.BranchId.Equals(order.BranchId))
-                        .FirstOrDefault();
-
-                    if (etaInfo != null)
-                    {
-                        order.ScheduledDeliveryTime = String.IsNullOrEmpty(etaInfo.ScheduledTime) ? new Nullable<DateTime>() : DateTime.Parse(etaInfo.ScheduledTime).ToUniversalTime();
-                        order.EstimatedDeliveryTime = String.IsNullOrEmpty(etaInfo.EstimatedTime) ? new Nullable<DateTime>() : DateTime.Parse(etaInfo.EstimatedTime).ToUniversalTime();
-                        order.ActualDeliveryTime = String.IsNullOrEmpty(etaInfo.ActualTime) ? new Nullable<DateTime>() : DateTime.Parse(etaInfo.ActualTime).ToUniversalTime();
-                        order.RouteNumber = String.IsNullOrEmpty(etaInfo.RouteId) ? String.Empty : etaInfo.RouteId;
-                        order.StopNumber = String.IsNullOrEmpty(etaInfo.StopNumber) ? String.Empty : etaInfo.StopNumber;
-                        order.DeliveryOutOfSequence = etaInfo.OutOfSequence == null ? false : etaInfo.OutOfSequence;
-                    }
-                }
-                catch(Exception ex)
-                {
-                    eventLogRepository.WriteErrorLog("Error processing ETA notification for : " + order.InvoiceNumber + ".  " + ex.Message + ".  " + ex.StackTrace);
-                }
-                
-                
-            }
-                
-            foreach (var order in orders)
-            {
-                try
-                {
-                    orderHistoryRepository.Update(order);
-                }
-                catch (Exception ex)
-                {
-                    eventLogRepository.WriteErrorLog("Error saving ETA notification for : " + order.InvoiceNumber + ".  " + ex.Message + ".  " + ex.StackTrace);
-                } 
-            }
-
-            unitOfWork.SaveChanges();
-
-            // send out notifications by customer - this may be enabled eventually, but for now, we just display the data in the UI
-            //List<string> customerNumbers = orders
-            //    .GroupBy(o => o.CustomerNumber)
-            //    .Select(grp => grp.First().CustomerNumber)
-            //    .ToList(); // get list of customer numbers
-
-            // for now, just update the order history entry with the currently estimated/scheduled/arrived times
-            //foreach (string customerNumber in customerNumbers)
-            //{
-            //    Svc.Core.Models.Profile.Customer customer = customerRepository.GetCustomerByCustomerNumber(notification.CustomerNumber);
-            //    List<Recipient> recipients = base.LoadRecipients(NotificationType.OrderConfirmation, customer);
-            //    Message message = GetEmailMessageForNotification(eta.Orders, orders.Where(o => o.CustomerNumber == customerNumber), customer);
-
-                // send messages to providers...
-                //base.SendMessage(recipients, message);
-            //}
-        }
-
+        #region methods
         protected Message GetEmailMessageForNotification(IEnumerable<OrderEta> orderEtas, IEnumerable<OrderHistoryHeader> orders, Svc.Core.Models.Profile.Customer customer)
         {
             // TODO: Add logic for delivered orders (actualtime) - not needed now, but maybe in the future.
@@ -162,5 +94,61 @@ namespace KeithLink.Svc.Impl.Logic.Messaging
 			message.NotificationType = NotificationType.OrderConfirmation;
             return message;
         }
+
+        public void ProcessNotification(Core.Models.Messaging.Queue.BaseNotification notification) {
+            if (notification.NotificationType != NotificationType.Eta) { throw new ApplicationException("notification/handler type mismatch"); }
+
+            EtaNotification eta = (EtaNotification)notification;
+
+            // load up recipients, customer and message
+            eventLogRepository.WriteInformationLog("Received ETA Message with " + eta.Orders.Count + " orders");
+            List<string> invoiceNumbers = eta.Orders.Select(x => x.OrderId).ToList();
+            var orders = orderHistoryRepository.Read(x => invoiceNumbers.Contains(x.InvoiceNumber)).ToList(); // get all orders for order ETAs
+            
+            foreach(OrderHistoryHeader order in orders) {
+                try {
+                    var etaInfo = eta.Orders.Where(o => o.OrderId.Equals(order.InvoiceNumber) && o.BranchId.Equals(order.BranchId)).FirstOrDefault();
+
+                    if (etaInfo != null){
+                        order.ScheduledDeliveryTime = String.IsNullOrEmpty(etaInfo.ScheduledTime) ? new Nullable<DateTime>() : DateTime.Parse(etaInfo.ScheduledTime).ToUniversalTime();
+                        order.EstimatedDeliveryTime = String.IsNullOrEmpty(etaInfo.EstimatedTime) ? new Nullable<DateTime>() : DateTime.Parse(etaInfo.EstimatedTime).ToUniversalTime();
+                        order.ActualDeliveryTime = String.IsNullOrEmpty(etaInfo.ActualTime) ? new Nullable<DateTime>() : DateTime.Parse(etaInfo.ActualTime).ToUniversalTime();
+                        order.RouteNumber = String.IsNullOrEmpty(etaInfo.RouteId) ? String.Empty : etaInfo.RouteId;
+                        order.StopNumber = String.IsNullOrEmpty(etaInfo.StopNumber) ? String.Empty : etaInfo.StopNumber;
+                        order.DeliveryOutOfSequence = etaInfo.OutOfSequence == null ? false : etaInfo.OutOfSequence;
+                    }
+                } catch(Exception ex) {
+                    eventLogRepository.WriteErrorLog("Error processing ETA notification for : " + order.InvoiceNumber + ".  " + ex.Message + ".  " + ex.StackTrace);
+                }
+            }
+                
+            foreach (var order in orders){
+                try{
+                    orderHistoryRepository.Update(order);
+                } catch (Exception ex) {
+                    eventLogRepository.WriteErrorLog("Error saving ETA notification for : " + order.InvoiceNumber + ".  " + ex.Message + ".  " + ex.StackTrace);
+                } 
+            }
+
+            unitOfWork.SaveChanges();
+
+            // send out notifications by customer - this may be enabled eventually, but for now, we just display the data in the UI
+            //List<string> customerNumbers = orders
+            //    .GroupBy(o => o.CustomerNumber)
+            //    .Select(grp => grp.First().CustomerNumber)
+            //    .ToList(); // get list of customer numbers
+
+            // for now, just update the order history entry with the currently estimated/scheduled/arrived times
+            //foreach (string customerNumber in customerNumbers)
+            //{
+            //    Svc.Core.Models.Profile.Customer customer = customerRepository.GetCustomerByCustomerNumber(notification.CustomerNumber);
+            //    List<Recipient> recipients = base.LoadRecipients(NotificationType.OrderConfirmation, customer);
+            //    Message message = GetEmailMessageForNotification(eta.Orders, orders.Where(o => o.CustomerNumber == customerNumber), customer);
+
+                // send messages to providers...
+                //base.SendMessage(recipients, message);
+            //}
+        }
+        #endregion
     }
 }
---
+++ KeithLink/KeithLink.Utility.NotificationParser/Models/EtaNotification.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace KeithLink.Utility.NotificationParser.Models {
+    public class EtaNotification {
+        [Key]
+        public int NotificationId { get; set; }
+
+        public DateTime? ActualTime { get; set; }
+
+        [Column(TypeName="varchar"), MaxLength(3)]
+        public string Branch { get; set; }
+
+        public DateTime? EstimatedTime { get; set; }
+
+        [Column(TypeName = "varchar"), MaxLength(8)]
+        public string OrderId { get; set; }
+
+        public bool OutOfSequence { get; set; }
+
+        [Column(TypeName = "varchar"), MaxLength(5)]
+        public string RouteId { get; set; }
+
+        public DateTime? ScheduledTime { get; set; }
+
+        [Column(TypeName = "varchar"), MaxLength(3)]
+        public string StopNumber { get; set; }
+    }
+}
