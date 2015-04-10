16f1c485ec9c89f64e640fbc7acb25b8b5886f78
Jason McMillan <jmmcmillan@benekeith.com>
2015-04-10 10:18:50 -0500

    changed lookup for branch in UserProfileLogic.FillUserProfile() to return correct branch for FABQ

--- KeithLink/KeithLink.Svc.Impl/Logic/Profile/UserProfileLogicImpl.cs
+++ KeithLink/KeithLink.Svc.Impl/Logic/Profile/UserProfileLogicImpl.cs
@@ -490,24 +490,26 @@ namespace KeithLink.Svc.Impl.Logic.Profile {
                 DirectoryEntry directoryEntry = (DirectoryEntry)adUser.GetUnderlyingObject();
                 List<string> internalUserRoles = _intAd.GetAllGroupsUserBelongsTo(adUser, Svc.Core.Constants.INTERNAL_USER_ROLES);
 
+                userBranch = GetBranchFromOU(adUser.GetOrganizationalunit());
+
                 if (internalUserRoles.Intersect(Constants.BEK_SYSADMIN_ROLES).Count() > 0) {
                     userRole = Constants.ROLE_NAME_SYSADMIN;
                 } else if (internalUserRoles.Intersect(Constants.MIS_ROLES).Count() > 0) {
                     userRole = Constants.ROLE_NAME_BRANCHIS;
-                    userBranch = internalUserRoles.Intersect(Constants.MIS_ROLES).FirstOrDefault().ToString().Substring(0, 3);
+                    //userBranch = internalUserRoles.Intersect(Constants.MIS_ROLES).FirstOrDefault().ToString().Substring(0, 3);
                 } else if (internalUserRoles.Intersect(Constants.POWERUSER_ROLES).Count() > 0){
                     userRole = Constants.ROLE_NAME_POWERUSER;
-                    userBranch = internalUserRoles.Intersect(Constants.POWERUSER_ROLES).FirstOrDefault().ToString().Substring(0, 3);
+                    //userBranch = internalUserRoles.Intersect(Constants.POWERUSER_ROLES).FirstOrDefault().ToString().Substring(0, 3);
                 } else if (internalUserRoles.Intersect(Constants.DSM_ROLES).Count() > 0) {
                     dsmRole = internalUserRoles.Intersect(Constants.DSM_ROLES).FirstOrDefault().ToString();
                     userRole = Constants.ROLE_NAME_DSM;
-                    userBranch = dsmRole.Substring(0, 3);
+                    //userBranch = dsmRole.Substring(0, 3);
                     dsmNumber = StringExtensions.ToInt(adUser.Description) != null ? adUser.Description : string.Empty;
                 } else if (internalUserRoles.Intersect(Constants.DSR_ROLES).Count() > 0) {
                     dsrRole = internalUserRoles.Intersect(Constants.DSR_ROLES).FirstOrDefault().ToString();
                     userRole = Constants.ROLE_NAME_DSR;
                     dsrNumber = StringExtensions.ToInt(adUser.Description) != null ? adUser.Description : string.Empty;
-                    userBranch = dsrRole.Substring(0, 3);
+                    //userBranch = dsrRole.Substring(0, 3);
                 } else {
                     userRole = Constants.ROLE_NAME_GUEST;
                 }
