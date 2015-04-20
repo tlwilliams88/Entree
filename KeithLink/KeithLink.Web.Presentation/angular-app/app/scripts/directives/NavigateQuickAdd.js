c03da81f5e84055c29031eecefef16c324843f14
Maria Knabe <mknabe@credera.com>
2015-04-20 13:11:37 -0500

    move internal email validation into a directive, fix keyboard navigation for quick add modal

--- KeithLink/KeithLink.Web.Presentation/angular-app/app/scripts/directives/NavigateQuickAdd.js
+++ KeithLink/KeithLink.Web.Presentation/angular-app/app/scripts/directives/NavigateQuickAdd.js
@@ -10,31 +10,31 @@ angular.module('bekApp')
     // select all on focus
     element.keydown(function (e) {
         
-        var key = { enter: 13 };
+      var key = { enter: 13 };
 
-        // shortcut for key other than arrow keys
-        if (angular.element.inArray(e.which, [key.enter]) < 0) { return; }
+      // shortcut for key other than arrow keys
+      if (angular.element.inArray(e.which, [key.enter]) < 0) { return; }
 
-        var input = e.target;
-        var td = angular.element(e.target).closest('td');
-        var moveTo = td.next('td');
+      var input = e.target;
+      var td = angular.element(e.target).closest('td');
+      var moveTo = td.next('td');
 
-        // go to next input if available
-        if (moveTo && moveTo.length) {
-          e.preventDefault();
+      // go to next input if available
+      if (moveTo && moveTo.length && moveTo.find('input,textarea').length) {
+        e.preventDefault();
 
-          moveTo.find('input,textarea').each(function (i, input) {
-            input.focus();
-            input.select();
-          });
+        moveTo.find('input,textarea').each(function (i, input) {
+          input.focus();
+          input.select();
+        });
 
-        } else {
-          // go to next row
-          e.preventDefault();
-          addRow();
-          scope.$digest();
-          var newRow = td.next('tr');
-        }
+      } else {
+        // go to next row
+        e.preventDefault();
+        addRow();
+        scope.$digest();
+        var newRow = td.next('tr');
+      }
     });
       
     scope.$on('$destroy',function() {
