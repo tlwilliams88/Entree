d402ec313b3a1c7f5a003440e65bba2104f4c0ab
Maria Knabe <mknabe@credera.com>
2015-04-24 13:22:28 -0500

    fix when opening order import modal on cart page

--- KeithLink/KeithLink.Web.Presentation/angular-app/app/scripts/controllers/CartItemsController.js
+++ KeithLink/KeithLink.Web.Presentation/angular-app/app/scripts/controllers/CartItemsController.js
@@ -316,7 +316,12 @@ angular.module('bekApp')
  
       var modalInstance = $modal.open({
         templateUrl: 'views/modals/orderimportmodal.html',
-        controller: 'ImportModalController'
+        controller: 'ImportModalController',
+        resolve: {
+          customListHeaders: ['ListService', function(ListService) {
+            return ListService.getCustomListHeaders();
+          }]
+        }
       });
     };
  
