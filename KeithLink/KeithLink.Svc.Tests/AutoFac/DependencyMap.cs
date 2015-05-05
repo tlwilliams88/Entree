55dd13e19bf73483860cbbb6f9a2d22ace7dc649
Josh Tirey <jtirey@credera.com>
2015-05-05 10:37:41 -0500

    Test change

--- KeithLink/KeithLink.Svc.Tests/AutoFac/DependencyMap.cs
+++ KeithLink/KeithLink.Svc.Tests/AutoFac/DependencyMap.cs
@@ -57,7 +57,7 @@ namespace KeithLink.Svc.Test
 			//*******************************************
 			//Mock Items
 			//*******************************************
-			builder.RegisterType<UnitOfWorkMock>().As<IUnitOfWork>();
+			builder.RegisterType<UnitOfWorkMock>().As<IUnitOfWork>().InstancePerLifetimeScope(); ;
 
 			//*******************************************
             //Logic Classes
