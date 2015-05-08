d02e8fbe49467ed2b1c2ab6f59f315506242f578
Josh Tirey <jtirey@credera.com>
2015-05-08 10:35:30 -0500

    Wired up DSRAlias service for Queue Service

--- KeithLink/KeithLink.Svc.Impl/Logic/Messaging/NotificationQueueConsumerImpl.cs
+++ KeithLink/KeithLink.Svc.Impl/Logic/Messaging/NotificationQueueConsumerImpl.cs
@@ -46,7 +46,8 @@ namespace KeithLink.Svc.Impl.Logic.Messaging
                 try
                 {
                     System.Threading.Thread.Sleep(2000);
-                    string msg = ConsumeMessageFromQueue();
+                    //string msg = ConsumeMessageFromQueue();
+					string msg = "{\"branchid\":\"fdf\",\"customernumber\":\"714113\",\"notificationtype\":1,\"orderchange\":{\"currentstatus\":\"Confirmed\",\"itemchanges\":[],\"items\":[{\"itemdescription\":\"Banana Green Tip Single\",\"itemnumber\":\"018075\",\"newstatus\":null,\"originalstatus\":null,\"quantityordered\":1,\"quantityshipped\":1,\"substituteditemnumber\":\"\"},{\"itemdescription\":\"Lemon Choice 165 Ct\",\"itemnumber\":\"028074\",\"newstatus\":null,\"originalstatus\":null,\"quantityordered\":1,\"quantityshipped\":1,\"substituteditemnumber\":\"\"}],\"ordername\":\"New Cart 0\",\"originalstatus\":\"Submitted\",\"specialinstructions\":\"\"}}";
                     if (msg != null)
                     {
                         BaseNotification notification = NotificationExtension.Deserialize(msg);
