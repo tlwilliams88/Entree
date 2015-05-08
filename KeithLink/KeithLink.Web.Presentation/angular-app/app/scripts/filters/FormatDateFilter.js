fc1909880f5242544e7cde9ebd127a0146e1140d
Maria Knabe <mknabe@credera.com>
2015-05-08 11:39:05 -0500

    refactor formatDate directive

--- KeithLink/KeithLink.Web.Presentation/angular-app/app/scripts/filters/FormatDateFilter.js
+++ KeithLink/KeithLink.Web.Presentation/angular-app/app/scripts/filters/FormatDateFilter.js
@@ -1,42 +1,50 @@
 'use strict';
 
-function getFormattedDateTime(dateTime, formatString, useTimezone) {
-
-  // fix for dates that formatted like 2015-02-13T00:00:00Z
-  // when parsed they are returned at 2015-02-12 but should be 2015-02-13
-  if (dateTime && dateTime.indexOf && dateTime.indexOf('T00:00:00Z') > -1) {
-    dateTime = dateTime.substr(0, 10);
-  }
-
-  var date = moment(dateTime);
-  if (dateTime && date.isValid()) {
-    if (useTimezone) {
-      var timezoneName = jstz.determine().name();// 'America/Chicago';
-      date.tz(timezoneName);
-    }
-    return date.format(formatString);
-  } else {
-    return dateTime;
-  }
-}
-
 angular.module('bekApp')
-  .filter('formatDate', [ function(){
-    return function(dateTime, formatString){
-    if (!formatString) {
-      formatString = 'ddd, MMM D, YYYY';
-    }
-    
-    return getFormattedDateTime(dateTime, formatString);
-  };
-}])
-  .filter('formatDateWithTimezone', [ function(){
-    return function(dateTime, formatString){
+  .filter('formatDate', [function() {
+    function getFormattedDateTime(dateTime, formatString, useTimezone) {
 
-    if (!formatString) {
-      formatString = 'ddd, MMM D, YYYY h:mma z';
+      // fix for dates that formatted like 2015-02-13T00:00:00Z
+      // when parsed they are returned at 2015-02-12 but should be 2015-02-13
+      if (dateTime && dateTime.indexOf && dateTime.indexOf('T00:00:00Z') > -1) {
+        dateTime = dateTime.substr(0, 10);
+
+        // check UTC time and convert to central
+      } else if (dateTime && dateTime.indexOf && dateTime.indexOf('T') > -1 && dateTime.indexOf('Z') > -1) {
+        dateTime = dateTime.replace('T', ' ');
+        dateTime = dateTime.replace('Z', '');
+      }
+
+      // all times from the backend are Central time
+      var date = moment.tz(dateTime, 'America/Chicago');
+
+      if (useTimezone === true) {
+        date.tz(jstz.determine().name());
+      }
+
+      if (dateTime && date.isValid()) {
+        return date.format(formatString);
+      } else {
+        return dateTime;
+      }
     }
-    
-    return getFormattedDateTime(dateTime, formatString, true);
-  };
-}]);
+
+
+    return function(dateTime, formatString) {
+      if (!formatString) {
+        formatString = 'ddd, MMM D, YYYY';
+      }
+
+      return getFormattedDateTime(dateTime, formatString, true);
+    };
+  }])
+  .filter('formatDateWithTimezone', ['$filter', function($filter) {
+    return function(dateTime, formatString) {
+
+      if (!formatString) {
+        formatString = 'ddd, MMM D, YYYY h:mma z';
+      }
+
+      return $filter('formatDate')(dateTime, formatString);
+    };
+  }]);
