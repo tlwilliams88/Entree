3180b7c9376bb077e664907bd79831eb6e1bf559
Maria Knabe <mknabe@credera.com>
2015-05-14 13:19:34 -0500

    marketing page sorting logic, UtilityService comments

--- KeithLink/KeithLink.Web.Presentation/angular-app/app/scripts/controllers/MarketingController.js
+++ KeithLink/KeithLink.Web.Presentation/angular-app/app/scripts/controllers/MarketingController.js
@@ -9,10 +9,10 @@
  */
 
 angular.module('bekApp')
-  .controller('MarketingController', ['$scope', 'MarketingService',
-    function($scope, MarketingService) {
+  .controller('MarketingController', ['$scope', 'MarketingService', 'UtilityService',
+    function($scope, MarketingService, UtilityService) {
 
-      function loadRegisteredUsers() {
+      function getRegisteredUsers() {
         $scope.loadingResults = true;
 
 
@@ -22,15 +22,18 @@ angular.module('bekApp')
       $scope.loadingResults = false;
       $scope.registeredUsersQuery = {};
 
-      var initialFromDate = moment().subtract(6, 'month'),
-        initialToDate = moment();
-      $scope.registeredUsersQuery.fromDate = initialFromDate.format('YYYY-MM-DD');
-      $scope.registeredUsersQuery.toDate = initialToDate.format('YYYY-MM-DD');
+      // determine starting date of 1 month in the past
+      var initialFromDate = new Date();
+      initialFromDate = initialFromDate.setMonth(initialFromDate.getMonth() - 1);
+      initialFromDate = new Date(initialFromDate);
+
+      $scope.registeredUsersQuery.fromDate = initialFromDate;
+      $scope.registeredUsersQuery.toDate = new Date();
 
       $scope.sortField = 'date';
       $scope.sortDescending = true;
 
-      loadRegisteredUsers();
+      getRegisteredUsers();
 
       $scope.datepickerOptions = {
         minDate: initialFromDate,
@@ -43,39 +46,44 @@ angular.module('bekApp')
 
 
 
-      $scope.sortTable = function() {
+      var itemsPerPage = 50,
+        itemIndex = 0;
+      $scope.sortTable = function(field, sortDescending) {
+        $scope.sortDescending = field === $scope.sortField ? !sortDescending : false;
+        $scope.sortField = field;
 
+        getRegisteredUsers();
       };
 
       $scope.refreshSearch = function() {
-
+        getRegisteredUsers();
       };
     
       $scope.openExportModal = function() {
-        // var modalInstance = $modal.open({
-        //   templateUrl: 'views/modals/exportmodal.html',
-        //   controller: 'ExportModalController',
-        //   resolve: {
-        //     headerText: function() {
-        //       return 'Item Usage';
-        //     },
-        //     exportMethod: function() {
-        //       return ReportService.exportItem;
-        //     },
-        //     exportConfig: function() {
-        //       return ReportService.getExportConfig();
-        //     },
-        //     exportParams: function() {
-        //       var params = {
-        //         fromdate: $scope.itemusagequery.fromDate,
-        //         todate: $scope.itemusagequery.toDate,
-        //         sortfield: $scope.sortField,
-        //         sortdir: $scope.sortReverse === true ? 'desc' : 'asc'
-        //       };
-        //       return '/report/itemusage/export?' + jQuery.param(params);
-        //     }
-        //   }
-        // });
+        var modalInstance = $modal.open({
+          templateUrl: 'views/modals/exportmodal.html',
+          controller: 'ExportModalController',
+          resolve: {
+            headerText: function() {
+              return 'Registered Users';
+            },
+            exportMethod: function() {
+              return ReportService.exportItem;
+            },
+            exportConfig: function() {
+              return ReportService.getExportConfig();
+            },
+            exportParams: function() {
+              var params = {
+                fromdate: $scope.itemusagequery.fromDate,
+                todate: $scope.itemusagequery.toDate,
+                sortfield: $scope.sortField,
+                sortdir: $scope.sortReverse === true ? 'desc' : 'asc'
+              };
+              return '/report/itemusage/export?' + jQuery.param(params);
+            }
+          }
+        });
       };
     }
 
