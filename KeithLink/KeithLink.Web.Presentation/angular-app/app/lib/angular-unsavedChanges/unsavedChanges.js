17a6acecf782cc755f4c24ec969f380c59f2384a
Dan DeRusha <dmderusha@benekeith.com>
2015-06-04 11:35:54 -0500

    Pivotal_ID: 96080774 Added check in unsavedChangesWarning.js to prevent a second confirmation message when navigating away from page. Now when an event triggers the unsaved changes warning, we check the event for a flag that is set to true when the user selects either of the options on the first confirmation dialouge.

--- KeithLink/KeithLink.Web.Presentation/angular-app/app/lib/angular-unsavedChanges/unsavedChanges.js
+++ KeithLink/KeithLink.Web.Presentation/angular-app/app/lib/angular-unsavedChanges/unsavedChanges.js
@@ -203,17 +203,20 @@ angular.module('unsavedChanges', ['resettable'])
 
             angular.forEach(eventsToWatchFor, function(aEvent) {
                 //calling this function later will unbind this, acting as $off()
+                var fffbreak = 5;
                 var removeFn = $rootScope.$on(aEvent, function(event, next, current) {
                     unsavedWarningsConfig.log("user is moving with " + aEvent);
                     // @todo this could be written a lot cleaner! 
-                    if (!allFormsClean()) {
+                    if (!allFormsClean() && !event.defaultPrevented) {
                         unsavedWarningsConfig.log("a form is dirty");
                         if (!confirm(messages.navigate)) {
                             unsavedWarningsConfig.log("user wants to cancel leaving");
                             event.preventDefault(); // user clicks cancel, wants to stay on page 
+                           
                         } else {
                             unsavedWarningsConfig.log("user doesn't care about loosing stuff");
                             $rootScope.$broadcast('resetResettables');
+
                         }
                     } else {
                         unsavedWarningsConfig.log("all forms are clean");
