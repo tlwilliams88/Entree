f2af388288f571af5d97b82e5de20d153057c094
Dan DeRusha <dmderusha@benekeith.com>
2015-06-18 16:13:47 -0500

    Always load new version of cart headers to update Pending Orders section on Home page.

--- KeithLink/KeithLink.Web.Presentation/angular-app/app/scripts/controllers/HomeController.js
+++ KeithLink/KeithLink.Web.Presentation/angular-app/app/scripts/controllers/HomeController.js
@@ -11,14 +11,11 @@ angular.module('bekApp')
   .controller('HomeController', [ '$scope', '$state', '$modal', '$filter', 'CartService', 'OrderService', 'MarketingService', 'NotificationService', 'CustomerService',
     function($scope, $state, $modal, $filter, CartService, OrderService, MarketingService, NotificationService, CustomerService) {
     
-    if(CartService.cartHeaders.length > 0){
-      $scope.cartHeaders = CartService.cartHeaders;
-    }
-    else{
+
       CartService.getCartHeaders().then(function(cartHeaders){
         $scope.cartHeaders = cartHeaders;
       })
-    }
+
 
     // get orders
     $scope.orders = [];
