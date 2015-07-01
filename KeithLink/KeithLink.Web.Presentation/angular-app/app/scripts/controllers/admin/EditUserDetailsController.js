b25157e41a3daaf77db5b3389cbd597f66df773b
Dan DeRusha <dmderusha@benekeith.com>
2015-07-01 16:55:32 -0500

    9543763-Moving logic to directive to prevent duplicate and unneccessary code per code review.

--- KeithLink/KeithLink.Web.Presentation/angular-app/app/scripts/controllers/admin/EditUserDetailsController.js
+++ KeithLink/KeithLink.Web.Presentation/angular-app/app/scripts/controllers/admin/EditUserDetailsController.js
@@ -44,7 +44,7 @@ angular.module('bekApp')
 
 
   // TODO: get available roles <----NEEDS ENDPOINT
-  $scope.roles =  [{ "value": 'owner', "text": "owner" }, { "value": 'accounting', "text": "accounting" },{ "value": 'approver', "text": "buyer" }, { "value": 'buyer', "text": "shopper" }, { "value": 'guest', "text": "guest" }];
+  $scope.roles =  [{ "value": "owner", "text": "owner" }, { "value": "accounting", "text": "accounting" },{ "value": "approver", "text": "buyer" }, { "value": "buyer", "text": "shopper" }, { "value": "guest", "text": "guest" }];
 
   processProfile(userProfile);
   checkIfUserExistsOnAnotherGroup();
