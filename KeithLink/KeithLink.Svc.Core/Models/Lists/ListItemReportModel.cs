61cc448aebd01ac8239fc3a7dbadf114d77b472f
matt <m@thismat.com>
2015-07-08 09:56:20 -0500

    Added report changes to show notes

--- KeithLink/KeithLink.Svc.Core/Models/Lists/ListItemReportModel.cs
+++ KeithLink/KeithLink.Svc.Core/Models/Lists/ListItemReportModel.cs
@@ -8,9 +8,20 @@ using System.Threading.Tasks;
 namespace KeithLink.Svc.Core.Models.Lists
 {
 	[DataContract]
-	public class ListItemReportModel
-	{
-		[DataMember]
+	public class ListItemReportModel {
+        #region attributes
+
+        private string _notes;
+
+        #endregion
+
+        public ListItemReportModel() {
+            _notes = String.Empty;
+        }
+
+        #region properties
+
+        [DataMember]
 		public string ItemNumber { get; set; }
 		[DataMember]
 		public string Name { get; set; }
@@ -22,5 +33,24 @@ namespace KeithLink.Svc.Core.Models.Lists
 		public string Brand { get; set; }
         [DataMember]
         public decimal ParLevel { get; set; }
-	}
+
+        /// <summary>
+        /// Report should only show 15 characters and if longer needs to mark it as a continuation.
+        /// </summary>
+        [DataMember]
+        public string Notes {
+            get { return this._notes; }
+            set {
+                if (value != null) {
+                    if (value.Length < 15) {
+                        this._notes = value;
+                    } else {
+                        this._notes = String.Format( "{0}...", value.Substring( 0, 12 ) );
+                    }
+                }
+            }
+        }
+
+        #endregion
+    }
 }
