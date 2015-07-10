d015f9ffb41e0d29e76257439ddd4bb176847ad3
Jeremy Ames <jwames@benekeith.com>
2015-07-10 15:15:36 -0500

    96885464 - start working with ES and add edge-ngram filters to the index setup

--- KeithLink/KeithLink.Svc.Impl/Repository/InternalCatalog/ElasticSearchRepositoryImpl.cs
+++ KeithLink/KeithLink.Svc.Impl/Repository/InternalCatalog/ElasticSearchRepositoryImpl.cs
@@ -6,16 +6,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace KeithLink.Svc.Impl.Repository.InternalCatalog
-{
-    public class ElasticSearchRepositoryImpl: IElasticSearchRepository
-    {
+namespace KeithLink.Svc.Impl.Repository.InternalCatalog {
+    public class ElasticSearchRepositoryImpl: IElasticSearchRepository {
+        #region attributes
         private RestClient client;
+        #endregion
 
+        #region ctor
         public ElasticSearchRepositoryImpl()
         {
             client = new RestClient(Configuration.ElasticSearchURL);
         }
+        #endregion
+
+        #region methods
+		public bool CheckIfIndexExist(string branchId)
+		{
+			var request = new RestRequest(branchId.ToLower(), Method.HEAD);
+			var response = client.Execute(request);
+			return response == null ? false : response.StatusCode == System.Net.HttpStatusCode.OK;
+		}
 
         public void Create(string json)
         {
@@ -24,58 +34,28 @@ namespace KeithLink.Svc.Impl.Repository.InternalCatalog
             client.Execute(request);
         }
 
-		public void MapProductProperties(string branchId, string json)
-		{
-			var request = new RestRequest(string.Format("{0}/product/_mapping", branchId), Method.POST);
-			request.AddParameter("application/json", json, ParameterType.RequestBody);
-			client.Execute(request);
-		}
-		
-		public void DeleteBranch(string branchId)
-		{
-			var request = new RestRequest(branchId, Method.DELETE);
-			client.Execute(request);
-		}
-
-
-		public bool CheckIfIndexExist(string branchId)
-		{
-			var request = new RestRequest(branchId.ToLower(), Method.HEAD);
-			var response = client.Execute(request);
-			return response == null ? false : response.StatusCode == System.Net.HttpStatusCode.OK;
-		}
-
-		public void RefreshSynonyms(string branchId)
-		{
-			//Close then open the index. This will cause it to see any changes made to the synonyms file
-			var requestClose = new RestRequest(string.Format("{0}/_close",branchId.ToLower()), Method.POST);
-			client.Execute(requestClose);
-
-			var requestOpen = new RestRequest(string.Format("{0}/_open", branchId.ToLower()), Method.POST);
-			client.Execute(requestOpen);
-		}
-
-		public void CreateEmptyIndex(string branchId)
-		{
+		public void CreateEmptyIndex(string branchId) {
 			var request = new RestRequest(branchId, Method.PUT);
-			//System.Dynamic.ExpandoObject snowballAnalyzer = new System.Dynamic.ExpandoObject();
-			//(snowballAnalyzer as IDictionary<string, object>).Add("default", new { type = "custom", filter = new List<string>(){ "standard", "lowercase", "snowball", "my_synonym_filter" }, tokenizer = "standar", language = "English" } );
-			//dynamic indexSettings = 
-			//	new { settings = 
-			//		new { 
-			//			index = 
-			//			new { number_of_replicas = 1, number_of_shards = 1, analysis =
-			//				new { analyzer = 
-			//					snowballAnalyzer
-			//				}
-			//			}
-			//		}
-			//	};
+            //dynamic synonymFilter = new { my_synonym_filter = new { type = "synonym", synonyms_path = "synonyms.txt" } };
+            //dynamic autoCompleteFilter = new { autcomplete_filter = new {type = "edge_ngram", min_gram = 2, max_gram = 20 }};
 
-			dynamic filterDynamic = new { my_synonym_filter = new { type = "synonym", synonyms_path = "synonyms.txt" } };
+            dynamic filters = new {
+                my_synonym_filter = new { type = "synonym", synonyms_path = "synonyms.txt" },
+                autcomplete_filter = new { type = "edge_ngram", min_gram = 2, max_gram = 20 }
+            };
+
 			System.Dynamic.ExpandoObject dynamicAnalyzer = new System.Dynamic.ExpandoObject();
-			(dynamicAnalyzer as IDictionary<string, object>).Add("default", new { type = "custom", filter = new List<string>() {"my_synonym_filter", "standard", "lowercase"}, tokenizer = "whitespace" });
-
+			(dynamicAnalyzer as IDictionary<string, object>).Add("default", new { 
+                                                                                    type = "custom", 
+                                                                                    filter = new List<string>() {
+                                                                                        "my_synonym_filter", 
+                                                                                        //"autcomplete_filter",
+                                                                                        "standard", 
+                                                                                        "lowercase"
+                                                                                    }, 
+                                                                                    tokenizer = "whitespace" 
+                                                                                }
+                                                                            );
 
 			dynamic indexSettings =
 				new
@@ -90,7 +70,7 @@ namespace KeithLink.Svc.Impl.Repository.InternalCatalog
 								number_of_shards = 1,
 								analysis = new
 								{
-									filter = filterDynamic,
+                                    filter = filters,
 									analyzer = dynamicAnalyzer
 								}
 							}
@@ -103,8 +83,29 @@ namespace KeithLink.Svc.Impl.Repository.InternalCatalog
             request.AddBody(indexSettings);
 			IRestResponse res = client.Execute(request);
 		}
-
-
 		
+        public void DeleteBranch(string branchId)
+		{
+			var request = new RestRequest(branchId, Method.DELETE);
+			client.Execute(request);
+		}
+		
+        public void MapProductProperties(string branchId, string json)
+		{
+			var request = new RestRequest(string.Format("{0}/product/_mapping", branchId), Method.POST);
+			request.AddParameter("application/json", json, ParameterType.RequestBody);
+			client.Execute(request);
+		}
+		
+		public void RefreshSynonyms(string branchId)
+		{
+			//Close then open the index. This will cause it to see any changes made to the synonyms file
+			var requestClose = new RestRequest(string.Format("{0}/_close",branchId.ToLower()), Method.POST);
+			client.Execute(requestClose);
+
+			var requestOpen = new RestRequest(string.Format("{0}/_open", branchId.ToLower()), Method.POST);
+			client.Execute(requestOpen);
+		}
+        #endregion
 	}
 }
