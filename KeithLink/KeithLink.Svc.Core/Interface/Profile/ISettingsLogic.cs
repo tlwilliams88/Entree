00e2d84451eea207657f3f1f1fee43abbe8a450d
matt <m@thismat.com>
2015-07-13 11:10:44 -0500

    98021164 - Partial commit of preferences saving

---
+++ KeithLink/KeithLink.Svc.Core/Interface/Profile/ISettingsLogic.cs
@@ -0,0 +1,17 @@
+﻿// KeithLink
+using KeithLink.Svc.Core.Models.Profile.EF;
+
+// Core
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeithLink.Svc.Core.Interface.Profile {
+    public interface ISettingsLogic {
+        List<Settings> GetAllUserSettings( string userId );
+        Settings GetUserSetting( string userId, string key );
+        void CreateOrUpdateSettings( Settings settings );
+    }
+}
