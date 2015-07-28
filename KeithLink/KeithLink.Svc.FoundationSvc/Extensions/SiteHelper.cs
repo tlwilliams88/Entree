5f404683bb49e06927b428f636ea1f3c2dc5d56a
Jeremy Ames <jwames@benekeith.com>
2015-07-28 11:25:31 -0500

    Incident 36515 - test for zero line items in the foundation service before trying to process custom properties. Clean up of all of Foundation Service Extension classes

--- KeithLink/KeithLink.Svc.FoundationSvc/Extensions/SiteHelper.cs
+++ KeithLink/KeithLink.Svc.FoundationSvc/Extensions/SiteHelper.cs
@@ -1,50 +1,58 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using CommerceServer.Core.Orders;
+using CommerceServer.Core.Runtime.Configuration;
+using CommerceServer.Core.Runtime.Orders;
 using CommerceServer.Foundation;
 using CommerceServer.Foundation.SequenceComponents.Utility;
-using CommerceServer.Core.Orders;
 
-namespace KeithLink.Svc.FoundationSvc.Extensions
-{
-    public class SiteHelper
-    {
-        public static string GetSiteName()
-        {
-            return System.Configuration.ConfigurationManager.AppSettings["CS_Sitename"];
-        }
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
 
-        static CommerceServer.Core.Runtime.Configuration.CommerceResourceCollection siteConfig = null;
-        public static CommerceServer.Core.Runtime.Configuration.CommerceResourceCollection GetCsConfig()
-        {
-            if (siteConfig == null)
-            {
-                siteConfig = new CommerceServer.Core.Runtime.Configuration.CommerceResourceCollection(SiteHelper.GetSiteName());
+namespace KeithLink.Svc.FoundationSvc.Extensions {
+    public class SiteHelper {
+        #region attributes
+        static CommerceResourceCollection siteConfig = null;
+        static OrderContext orderContext = null;
+        static OrderManagementContext orderMgmtContext = null;
+        #endregion
+
+        #region ctor
+        #endregion
+
+        #region methods
+        public static CommerceResourceCollection GetCsConfig() {
+            if (siteConfig == null) {
+                siteConfig = new CommerceResourceCollection(SiteHelper.GetSiteName());
             }
+
             return siteConfig;
         }
 
-        static CommerceServer.Core.Runtime.Orders.OrderContext orderContext = null;
-        public static CommerceServer.Core.Runtime.Orders.OrderContext GetOrderContext()
-        {
-            if (orderContext == null)
-            {
-                orderContext = CommerceServer.Core.Runtime.Orders.OrderContext.Create(GetSiteName());
+        public static OrderContext GetOrderContext() {
+            if (orderContext == null) {
+                orderContext = OrderContext.Create(GetSiteName());
             }
+
             return orderContext;
         }
 
-		static CommerceServer.Core.Orders.OrderManagementContext orderMgmtContext = null;
-		public static CommerceServer.Core.Orders.OrderManagementContext GetOrderManageContext()
-		{
-			if (orderMgmtContext == null)
-			{
-				var ordersAgent = new OrderSiteAgent(GetSiteName());
-				orderMgmtContext = OrderManagementContext.Create(ordersAgent);
-			}
-			return orderMgmtContext;
-		}
+        public static OrderManagementContext GetOrderManageContext() {
+            if (orderMgmtContext == null) {
+                var ordersAgent = new OrderSiteAgent(GetSiteName());
+                orderMgmtContext = OrderManagementContext.Create(ordersAgent);
+            }
 
+            return orderMgmtContext;
+        }
+
+        public static string GetSiteName() {
+            return ConfigurationManager.AppSettings["CS_Sitename"];
+        }
+        #endregion
+
+        #region properties
+        #endregion
     }
 }
