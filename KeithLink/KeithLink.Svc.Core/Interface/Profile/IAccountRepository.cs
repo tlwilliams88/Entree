a1af9ba82630b78a8786be1e6957197f4537e831
BEKCO\jcswaringen <jcswaringen@benekeith.com>
2015-07-30 13:35:30 -0500

    no story - recompiled to remove bug with KeithLink.AccessService.Setup project

---
+++ KeithLink/KeithLink.Svc.Core/Interface/Profile/IAccountRepository.cs
@@ -0,0 +1,22 @@
+﻿using KeithLink.Svc.Core.Models.Profile;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeithLink.Svc.Core.Interface.Profile
+{
+    public interface IAccountRepository
+    {
+        Guid CreateAccount(string createdBy, string name);
+		void DeleteAccount(string deletedBy, Guid accountId);
+        List<Account> GetAccounts();
+        List<Account> GetAccountsForUser(Guid userId);
+        void AddCustomerToAccount(string addedBy, Guid accountId, Guid customerId);
+        void RemoveCustomerFromAccount(string removedBy, Guid accountId, Guid customerId);
+		void AddUserToAccount(string addedBy, Guid accountId, Guid userId);
+		void RemoveUserFromAccount(string removedBy, Guid accountId, Guid userId);
+		Guid UpdateAccount(string name, Guid accountId);
+    }
+}
