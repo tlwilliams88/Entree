3f734c0330c064ee1a03281265d8909d7560cb4d
matt <m@thismat.com>
2015-09-14 15:57:07 -0500

    Hotfix - added new navigation parameters to all keyboard navigation pages.

--- KeithLink/KeithLink.Web.Presentation/angular-app/app/scripts/directives/NavigateTable.js
+++ KeithLink/KeithLink.Web.Presentation/angular-app/app/scripts/directives/NavigateTable.js
@@ -1,73 +1,78 @@
 'use strict';
 
 /*
-* takes one optional attr
-* default -- behaves like a regular table with no extra rows
-* "lists" attr (navigate-table="lists") -- tbody tags can be used as rows, see lists.html page
-* "mobile" attr -- allows for .mobile-details-row, see searchresults.html page
-* 
-*/
+ * Navigates between items with the class of 'tabstop' in a table with repeating tbody
+ *
+ * takes one optional attr
+ * default -- behaves like a regular table with no extra rows
+ * "lists" attr (navigate-table="lists") -- tbody tags can be used as rows, see lists.html page
+ * "mobile" attr -- allows for .mobile-details-row, see searchresults.html page
+ * 
+ */
 
 angular.module('bekApp')
-  .directive('navigateTable', [ function(){
-    return function(scope, element, attr) {
+.directive('navigateTable', [ function(){
+  return function(scope, element, attr) {
 
     element.on('keyup', 'input[type="text"]', handleNavigation);
-    
+
     function handleNavigation(e) {
       // select all on focus
       element.find('input').keydown(function (e) {
-          var key = { left: 37, up: 38, right: 39, down: 40, enter: 13, tab: 9 };
+        var key = { left: 37, up: 38, right: 39, down: 40, enter: 13, tab: 9 };
 
-          // shortcut for key other than arrow keys
-          if (angular.element.inArray(e.which, [key.left, key.up, key.right, key.down, key.enter, key.tab]) < 0) { return; }
+        // shortcut for key other than arrow keys
+        if (angular.element.inArray(e.which, [key.left, key.up, key.right, key.down, key.enter, key.tab]) < 0) { return; }
 
-          var input = e.target;
-          var td = angular.element(e.target).closest('td');
-          var row = td.closest('tbody');
-          var moveTo = null;
+        var input = e.target;
+        var td = angular.element(e.target).closest('td');
+        var row = td.closest('tr');
+        if (attr.navigateTable === 'lists') {
+          row = td.closest('tbody');
+        }
+        var moveTo = null;
 
-          switch (e.which) {
+        switch (e.which) {
 
-              case key.left: {
-                  if (input.type === 'checkbox' || input.selectionStart === 0) {
-                      moveTo = td.prev('td:has(input,textarea)').find('input,textarea')[0];
-                  }
-                  break;
-              }
-              case key.right: {
-                  if (input.type == 'checkbox' || input.selectionEnd === input.value.length) {
-                      moveTo = td.next('td:has(input,textarea)').find('input,textarea')[0];
-                  }
-                  break;
-              }
-
-              case key.up: {
-                moveTo = row.prevUntil('tr td .tabstop').find('.tabstop').last()[0];
-                break;
-              }
-              case key.down:
-              case key.enter:
-              case key.tab: {
-                var isNavigatingDown = e.which === key.down || e.which === key.enter || e.which === key.tab;
-                moveTo = row.nextUntil('tr td .tabstop').find('.tabstop')[0]; //findNextInput(td, attr.navigateTable, isNavigatingDown);
-                break;
-              }
-
+          case key.left: {
+            if (input.type === 'checkbox' || input.selectionStart === 0) {
+              moveTo = td.prev('td:has(input,textarea)').find('input,textarea')[0];
+            }
+            break;
+          }
+          case key.right: {
+            if (input.type == 'checkbox' || input.selectionEnd === input.value.length) {
+              moveTo = td.next('td:has(input,textarea)').find('input,textarea')[0];
+            }
+            break;
           }
 
-          if (moveTo) {
-              e.preventDefault();
-              
-                moveTo.focus();
-                if (moveTo.type != 'checkbox') {
-                  moveTo.select();
-                }
+          case key.up: {
+            moveTo = row.prevUntil('tr td .tabstop').find('.tabstop').last()[0];
+            break;
           }
+          case key.down:
+            case key.enter:
+            case key.tab: {
+            var isNavigatingDown = e.which === key.down || e.which === key.enter || e.which === key.tab;
+            moveTo = row.nextUntil('tr td .tabstop').find('.tabstop')[0]; //findNextInput(td, attr.navigateTable, isNavigatingDown);
+            break;
+          }
+
+        }
+
+        if (moveTo) {
+          e.preventDefault();
+
+          moveTo.focus();
+          if (moveTo.type != 'checkbox') {
+            moveTo.select();
+          }
+        }
 
       });
-      
-      
+
+
       var key = e.keyCode ? e.keyCode : e.which;
       // // enter key
       // if(key === 13) {
@@ -87,4 +92,4 @@ angular.module('bekApp')
       angular.element( window ).off( 'resize.Viewport' );
     });
   };
-  }]);
+}]);
