16c544480ecc328134a2a955689f277d128d134e
Jeremy Ames <jwames@benekeith.com>
2015-10-21 09:18:34 -0500

    98468472 - add check for _keepListening flag while processing order history to allow the queue service to terminate correctly

--- KeithLink/KeithLink.Svc.Core/Interface/Orders/History/IInternalOrderHistoryLogic.cs
+++ KeithLink/KeithLink.Svc.Core/Interface/Orders/History/IInternalOrderHistoryLogic.cs
@@ -1,23 +1,24 @@
-﻿using KeithLink.Svc.Core.Models.Orders.History;
-using KeithLink.Svc.Core.Models.SiteCatalog;
-using KeithLink.Svc.Core.Models.Orders;
-using System.Collections.Generic;
-using System;
+﻿using KeithLink.Svc.Core.Models.Orders;
+using KeithLink.Svc.Core.Models.Orders.History;
 using KeithLink.Svc.Core.Models.Paging;
+using KeithLink.Svc.Core.Models.SiteCatalog;
+
+using System;
+using System.Collections.Generic;
 
 namespace KeithLink.Svc.Core.Interface.Orders.History {
     public interface IInternalOrderHistoryLogic {
         Order GetOrder(string branchId, string invoiceNumber);
 
-        List<Order> GetOrders(System.Guid userId, UserSelectedContext customerInfo);
-
 		List<Order> GetOrderHeaderInDateRange(UserSelectedContext customerInfo, DateTime startDate, DateTime endDate);
 
-        void SaveOrder(OrderHistoryFile historyFile);
+        List<Order> GetOrders(System.Guid userId, UserSelectedContext customerInfo);
 
 		PagedResults<Order> GetPagedOrders(Guid userId, UserSelectedContext customerInfo, PagingModel paging);
 
 		void ListenForQueueMessages();
+
+        void SaveOrder(OrderHistoryFile historyFile);
 
 		void StopListening();
     }
