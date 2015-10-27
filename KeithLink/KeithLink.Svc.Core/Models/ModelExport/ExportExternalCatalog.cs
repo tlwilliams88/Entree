8f61f798f3a76ee61b092ba0282fbccab6174778
unknown <corp-ssa-CrederaDev@CORPMISDEV2G.bekco.com>
2015-10-27 14:57:30 -0500

    Added DTO files for ExternalCatalog objexts

---
+++ KeithLink/KeithLink.Svc.Core/Models/ModelExport/ExportExternalCatalog.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeithLink.Svc.Core.Models.ModelExport
+{
+	[DataContract]
+	public class ExportExternalCatalog
+	{
+        [DataMember(Name = "BekBranchId")]
+        public string BekBranchId { get; set; }
+        [DataMember(Name = "ExternalBranchId")]
+        public string ExternalBranchId { get; set; }
+        [DataMember(Name = "Type")]
+        public string Type { get; set; }
+	}
+}
