5eeff77eae7ad98ea1228f0958047ad2a6e96cdf
Brett Killins <bakillins@benekeith.com>
2016-02-08 10:36:35 -0600

    No story - sql file

--- KeithLink/v1.5.0.sql
+++ KeithLink/v1.5.0.sql
@@ -1,38 +1,7 @@
-﻿-- Extend Order History Detail for UNFI
-ALTER TABLE [Orders].[OrderHistoryDetail] ADD [Source] [char](3)
-ALTER TABLE [Orders].[OrderHistoryDetail] ADD [ManufacturerId] [nvarchar](25)
-ALTER TABLE [Orders].[OrderHistoryDetail] ADD [SpecialOrderHeaderId] [char](7)
-ALTER TABLE [Orders].[OrderHistoryDetail] ADD [SpecialOrderLineNumber] [char](3)
+﻿-- The following migrations exist:
+--	201511172254161_Extend Order History Detail for UNFI.cs
+--	201511210615272_2015-11-20_Merge_UNFI_ExternalCatalog_Mapping_Changes.cs
+--	201512030739583_2015-12-03_gsSyncMigrations.cs
+--	201602052337398_Add Special Order Flag to Order History Header.cs
+--	201602052337298_Add RelatedControlNumber to Order History Header.cs
 
--- Merge_UNFI_ExternalCatalog_Mapping_Changes
-IF schema_id('Configuration') IS NULL
-    EXECUTE('CREATE SCHEMA [Configuration]')
-CREATE TABLE [Configuration].[ExternalCatalogs] (
-    [Id] [bigint] NOT NULL IDENTITY,
-    [BekBranchId] [nvarchar](24),
-    [ExternalBranchId] [nvarchar](24),
-    [Type] [int] NOT NULL,
-    [CreatedUtc] [datetime] NOT NULL DEFAULT GETUTCDATE(),
-    [ModifiedUtc] [datetime] NOT NULL DEFAULT GETUTCDATE(),
-    CONSTRAINT [PK_Configuration.ExternalCatalogs] PRIMARY KEY ([Id])
-)
-IF schema_id('Profile') IS NULL
-    EXECUTE('CREATE SCHEMA [Profile]')
-CREATE TABLE [Profile].[Settings] (
-    [Id] [bigint] NOT NULL IDENTITY,
-    [UserId] [uniqueidentifier] NOT NULL,
-    [Key] [varchar](100) NOT NULL,
-    [Value] [varchar](250) NOT NULL,
-    [CreatedUtc] [datetime] NOT NULL DEFAULT GETUTCDATE(),
-    [ModifiedUtc] [datetime] NOT NULL DEFAULT GETUTCDATE(),
-    CONSTRAINT [PK_Profile.Settings] PRIMARY KEY ([Id])
-)
-CREATE INDEX [IX_UserId] ON [Profile].[Settings]([UserId])
-ALTER TABLE [List].[ListItems] ADD [CatalogId] [nvarchar](24)
-ALTER TABLE [Orders].[OrderHistoryHeader] ADD [OriginalControlNumber] [char](7)
-
--- Add Special Order Flag to Order History Header
-ALTER TABLE [Orders].[OrderHistoryHeader] ADD [IsSpecialOrder] [bit] NOT NULL DEFAULT 0
-
--- Add RelatedControlNumber to Order History Header
-ALTER TABLE [Orders].[OrderHistoryHeader] ADD [RelatedControlNumber] [char](7)
