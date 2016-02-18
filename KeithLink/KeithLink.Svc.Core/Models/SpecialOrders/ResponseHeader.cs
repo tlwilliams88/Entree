01f55d17efb3c7527ecc30aa836217e61589bf50
Brett Killins <bakillins@benekeith.com>
2016-02-18 16:23:08 -0600

    113584355 - Added entree side of responding to updates from KSOS about special orders

---
+++ KeithLink/KeithLink.Svc.Core/Models/SpecialOrders/ResponseHeader.cs
@@ -0,0 +1,73 @@
+﻿using KeithLink.Svc.Core.Interface.ModelExport;
+using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+
+
+namespace KeithLink.Svc.Core.Models.SpecialOrders
+{
+    [DataContract]
+    public class ResponseHeader
+	{
+        [DataMember(Name = "requestheaderid")]
+        public string RequestHeaderId { get; set; }
+
+        [DataMember(Name = "branch")]
+		public string BranchId { get; set; }
+
+        [DataMember(Name = "buyernumber")]
+        public string BuyerNumber { get; set; }
+
+        [DataMember(Name = "dsrnumber")]
+        public string DsrNumber { get; set; }
+
+        [DataMember(Name = "customernumber")]
+        public string CustomerNumber { get; set; }
+
+        [DataMember(Name = "address")]
+        public string Address { get; set; }
+
+        [DataMember(Name = "city")]
+        public string City { get; set; }
+
+        [DataMember(Name = "state")]
+        public string State { get; set; }
+
+        [DataMember(Name = "zip")]
+        public string Zip { get; set; }
+
+        [DataMember(Name = "contact")]
+        public string Contact { get; set; }
+
+        [DataMember(Name = "manufacturername")]
+        public string ManufacturerName { get; set; }
+
+        [DataMember(Name = "shipmethod")]
+        public byte ShipMethodId { get; set; } 
+
+        [DataMember(Name = "orderstatusid")]
+        public string OrderStatusId { get; set; }
+
+        [DataMember(Name = "specialinstructions")]
+        public string SpecialInstructions { get; set; }
+
+        [DataMember(Name = "modifiedshippingaddress")]
+        public bool? ModifiedShippingAddress { get; set; }
+
+        [DataMember(Name = "creditapproval")]
+        public bool? CreditApproval { get; set; }
+
+        [DataMember(Name = "statusdate")]
+        public DateTime? StatusDate { get; set; }
+
+        [DataMember(Name = "submitdate")]
+        public DateTime? SubmitDate { get; set; }
+
+        [DataMember(Name = "updatedby")]
+        public string UpdatedBy { get; set; }
+
+        [DataMember(Name = "source")]
+        public string Source { get; set; }
+    }
+}
---
+++ KeithLink/KeithLink.Svc.Core/Models/SpecialOrders/SpecialOrderResponseModel.cs
@@ -0,0 +1,22 @@
+﻿using KeithLink.Svc.Core.Interface.ModelExport;
+using KeithLink.Svc.Core.Models.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeithLink.Svc.Core.Models.SpecialOrders
+{
+    [DataContract(Name = "specialorderresponse")]
+    public class SpecialOrderResponseModel : BaseQueueMessage
+    {
+        [DataMember(Name = "header")]
+        public ResponseHeader Header { get; set; }
+
+        [DataMember(Name = "item")]
+        public ResponseItem Item { get; set; }
+    }
+}
