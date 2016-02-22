17abb6ae7db38e0b383c14f86e5d6a7393a7b92c
Brett Killins <bakillins@benekeith.com>
2016-02-22 11:10:46 -0600

    113584355 - Synchronize changes for KSOS/Entree exchange for special order updates

--- KeithLink/KeithLink.Svc.Core/Models/SpecialOrders/ResponseHeader.cs
+++ KeithLink/KeithLink.Svc.Core/Models/SpecialOrders/ResponseHeader.cs
@@ -10,64 +10,64 @@ namespace KeithLink.Svc.Core.Models.SpecialOrders
     [DataContract]
     public class ResponseHeader
 	{
-        [DataMember(Name = "requestheaderid")]
+        [DataMember(Name = "RequestHeaderId")]
         public string RequestHeaderId { get; set; }
 
-        [DataMember(Name = "branch")]
+        [DataMember(Name = "BranchId")]
 		public string BranchId { get; set; }
 
-        [DataMember(Name = "buyernumber")]
+        [DataMember(Name = "BuyerNumber")]
         public string BuyerNumber { get; set; }
 
-        [DataMember(Name = "dsrnumber")]
+        [DataMember(Name = "DsrNumber")]
         public string DsrNumber { get; set; }
 
-        [DataMember(Name = "customernumber")]
+        [DataMember(Name = "CustomerNumber")]
         public string CustomerNumber { get; set; }
 
-        [DataMember(Name = "address")]
+        [DataMember(Name = "Address")]
         public string Address { get; set; }
 
-        [DataMember(Name = "city")]
+        [DataMember(Name = "City")]
         public string City { get; set; }
 
-        [DataMember(Name = "state")]
+        [DataMember(Name = "State")]
         public string State { get; set; }
 
-        [DataMember(Name = "zip")]
+        [DataMember(Name = "Zip")]
         public string Zip { get; set; }
 
-        [DataMember(Name = "contact")]
+        [DataMember(Name = "Contact")]
         public string Contact { get; set; }
 
-        [DataMember(Name = "manufacturername")]
+        [DataMember(Name = "ManufacturerName")]
         public string ManufacturerName { get; set; }
 
-        [DataMember(Name = "shipmethod")]
-        public byte ShipMethodId { get; set; } 
+        [DataMember(Name = "ShipMethodId")]
+        public byte ShipMethodId { get; set; }
 
-        [DataMember(Name = "orderstatusid")]
+        [DataMember(Name = "OrderStatusId")]
         public string OrderStatusId { get; set; }
 
-        [DataMember(Name = "specialinstructions")]
+        [DataMember(Name = "SpecialInstructions")]
         public string SpecialInstructions { get; set; }
 
-        [DataMember(Name = "modifiedshippingaddress")]
+        [DataMember(Name = "ModifiedShippingAddress")]
         public bool? ModifiedShippingAddress { get; set; }
 
-        [DataMember(Name = "creditapproval")]
+        [DataMember(Name = "CreditApproval")]
         public bool? CreditApproval { get; set; }
 
-        [DataMember(Name = "statusdate")]
-        public DateTime? StatusDate { get; set; }
+        [DataMember(Name = "StatusDate")]
+        public string StatusDate { get; set; }
 
-        [DataMember(Name = "submitdate")]
-        public DateTime? SubmitDate { get; set; }
+        [DataMember(Name = "SubmitDate")]
+        public string SubmitDate { get; set; }
 
-        [DataMember(Name = "updatedby")]
+        [DataMember(Name = "UpdatedBy")]
         public string UpdatedBy { get; set; }
 
-        [DataMember(Name = "source")]
+        [DataMember(Name = "Source")]
         public string Source { get; set; }
     }
 }
