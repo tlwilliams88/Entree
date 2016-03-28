2c301de8534385a835c1109e597fc84b2e06fc9a
Brett Killins <bakillins@benekeith.com>
2016-03-28 12:49:01 -0500

    116302621 - change recent calls to operationreturnmodel

--- KeithLink/KeithLink.Svc.WebApi/Controllers/RecentItemController.cs
+++ KeithLink/KeithLink.Svc.WebApi/Controllers/RecentItemController.cs
@@ -1,4 +1,5 @@
-﻿using KeithLink.Svc.Core.Interface.Lists;
+﻿using KeithLink.Common.Core.Logging;
+using KeithLink.Svc.Core.Interface.Lists;
 using KeithLink.Svc.Core.Interface.Profile;
 using KeithLink.Svc.Core.Models.Lists;
 using System;
@@ -13,36 +14,64 @@ namespace KeithLink.Svc.WebApi.Controllers
     public class RecentItemController : BaseController {
         #region attributes
         private readonly IListServiceRepository listServiceRepository;
-        
+        private readonly IEventLogRepository _log;
         #endregion
 
         #region ctor
-        public RecentItemController(IListServiceRepository listServiceRepository,  IUserProfileLogic profileLogic)  : base(profileLogic) {
+        public RecentItemController(IListServiceRepository listServiceRepository,  IUserProfileLogic profileLogic, IEventLogRepository logRepo)  : base(profileLogic) {
 			this.listServiceRepository = listServiceRepository;
+            _log = logRepo;
         }
         #endregion
 
         #region methods
-		/// <summary>
-		/// Retrieve recently viewed items
-		/// </summary>
-		/// <returns></returns>
+        /// <summary>
+        /// Retrieve recently viewed items
+        /// </summary>
+        /// <returns></returns>
         [HttpGet]
 		[ApiKeyedRoute("recent/")]
-		public List<RecentItem> Recent() {
-			return listServiceRepository.ReadRecent(this.AuthenticatedUser, this.SelectedUserContext);
-		}
+		public Models.OperationReturnModel<List<RecentItem>> Recent() {
+            Models.OperationReturnModel<List<RecentItem>> retVal = new Models.OperationReturnModel<List<RecentItem>>();
+            try
+            {
+                retVal.SuccessResponse = listServiceRepository.ReadRecent(this.AuthenticatedUser, this.SelectedUserContext);
+                retVal.IsSuccess = true;
+            }
+            catch (Exception ex)
+            {
+                _log.WriteErrorLog("Recent", ex);
+                retVal.ErrorMessage = ex.Message;
+                retVal.IsSuccess = false;
+            }
 
-		/// <summary>
-		/// Add a new recently viewed item
-		/// </summary>
-		/// <param name="itemnumber"></param>
-		[HttpPost]
+            return retVal;
+        }
+
+        /// <summary>
+        /// Add a new recently viewed item
+        /// </summary>
+        /// <param name="itemnumber"></param>
+        [HttpPost]
 		[ApiKeyedRoute("recent/{itemnumber}")]
-		public void Recent(string itemnumber) {
-			listServiceRepository.AddRecentlyViewedItem(this.AuthenticatedUser, this.SelectedUserContext, itemnumber);
-		}
-				
+		public Models.OperationReturnModel<bool> Recent(string itemnumber) {
+            Models.OperationReturnModel<bool> retVal = new Models.OperationReturnModel<bool>();
+            try
+            {
+                listServiceRepository.AddRecentlyViewedItem(this.AuthenticatedUser, this.SelectedUserContext, itemnumber);
+                retVal.SuccessResponse = true;
+                retVal.IsSuccess = retVal.SuccessResponse;
+            }
+            catch (Exception ex)
+            {
+                _log.WriteErrorLog("Recent(string itemnumber)", ex);
+                retVal.ErrorMessage = ex.Message;
+                retVal.IsSuccess = false;
+            }
+
+            return retVal;
+        }
+
         #endregion
 
     }
