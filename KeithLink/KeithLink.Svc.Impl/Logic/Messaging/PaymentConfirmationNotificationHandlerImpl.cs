930e6007d05cc7d052914cf2a4ab477565de0eec
Brett Killins <bakillins@benekeith.com>
2016-04-08 09:08:13 -0500

    96879952 - Add payment confirmation improvements, and logging for email client

--- KeithLink/KeithLink.Svc.Impl/Logic/Messaging/PaymentConfirmationNotificationHandlerImpl.cs
+++ KeithLink/KeithLink.Svc.Impl/Logic/Messaging/PaymentConfirmationNotificationHandlerImpl.cs
@@ -3,6 +3,7 @@ using KeithLink.Common.Core.Logging;
 using KeithLink.Svc.Core.Enumerations.Messaging;
 using KeithLink.Svc.Core.Extensions.Messaging;
 using KeithLink.Svc.Core.Interface.Email;
+using KeithLink.Svc.Core.Interface.Invoices;
 using KeithLink.Svc.Core.Interface.Messaging;
 using KeithLink.Svc.Core.Interface.OnlinePayments.Customer;
 using KeithLink.Svc.Core.Interface.OnlinePayments.Invoice;
@@ -14,15 +15,16 @@ using KeithLink.Svc.Core.Models.Messaging.Queue;
 using KeithLink.Svc.Core.Models.OnlinePayments.Customer;
 using KeithLink.Svc.Core.Models.OnlinePayments.Payment;
 using KeithLink.Svc.Impl.Helpers;
-
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace KeithLink.Svc.Impl.Logic.Messaging {
-    public class PaymentConfirmationNotificationHandlerImpl : BaseNotificationHandlerImpl, INotificationHandler {
+namespace KeithLink.Svc.Impl.Logic.Messaging
+{
+    public class PaymentConfirmationNotificationHandlerImpl : BaseNotificationHandlerImpl, INotificationHandler
+    {
         #region attributes
         private const string MESSAGE_TEMPLATE_PAYMENTCONFIRMATION = "PaymentConfirmation";
         private const string MESSAGE_TEMPLATE_PAYMENTDETAIL = "PaymentConfirmationDetail";
@@ -38,13 +40,13 @@ namespace KeithLink.Svc.Impl.Logic.Messaging {
         #endregion
 
         #region ctor
-        public PaymentConfirmationNotificationHandlerImpl(IEventLogRepository eventLogRepository, IUserProfileLogic userProfileLogic, IUserPushNotificationDeviceRepository userPushNotificationDeviceRepository, 
-                                                          ICustomerRepository customerRepository, IUserMessagingPreferenceRepository userMessagingPreferenceRepository, Func<Channel, IMessageProvider> messageProviderFactory,
-														  IMessageTemplateLogic messageTemplateLogic, IKPayInvoiceRepository kpayInvoiceRepo, ICustomerBankRepository customerBankRepo, 
+        public PaymentConfirmationNotificationHandlerImpl(IEventLogRepository eventLogRepository, IUserProfileLogic userProfileLogic,
+                                                          IUserPushNotificationDeviceRepository userPushNotificationDeviceRepository, ICustomerRepository customerRepository,
+                                                          IUserMessagingPreferenceRepository userMessagingPreferenceRepository, Func<Channel, IMessageProvider> messageProviderFactory,
+                                                          IMessageTemplateLogic messageTemplateLogic, IKPayInvoiceRepository kpayInvoiceRepo, ICustomerBankRepository customerBankRepo,
                                                           IDsrLogic dsrLogic)
-            : base(userProfileLogic, userPushNotificationDeviceRepository, customerRepository, 
-                   userMessagingPreferenceRepository, messageProviderFactory, eventLogRepository, 
-                   dsrLogic)
+            : base(userProfileLogic, userPushNotificationDeviceRepository, customerRepository,
+                   userMessagingPreferenceRepository, messageProviderFactory, eventLogRepository, dsrLogic)
         {
             _log = eventLogRepository;
             _userLogic = userProfileLogic;
@@ -58,59 +60,64 @@ namespace KeithLink.Svc.Impl.Logic.Messaging {
         #endregion
 
         #region methods
-        
-        private Message GetEmailMessageForNotification(PaymentConfirmationNotification notification, Svc.Core.Models.Profile.Customer customer) {
+
+        private Message GetEmailMessageForNotification(PaymentConfirmationNotification notification, Svc.Core.Models.Profile.Customer customer)
+        {
             MessageTemplateModel template = _messageTemplateLogic.ReadForKey(MESSAGE_TEMPLATE_PAYMENTCONFIRMATION);
             MessageTemplateModel detailTemplate = _messageTemplateLogic.ReadForKey(MESSAGE_TEMPLATE_PAYMENTDETAIL);
 
             StringBuilder orderDetails = new StringBuilder();
 
-            foreach(var payment in notification.Payments){
-				var invoice = _invoiceRepo.GetInvoiceHeader(DivisionHelper.GetDivisionFromBranchId(notification.BranchId), notification.CustomerNumber, payment.InvoiceNumber);
-
-                orderDetails.Append(detailTemplate.Body.Inject(new { 
-                                                                        InvoiceType = invoice.InvoiceType,
-                                                                        InvoiceNumber = payment.InvoiceNumber,
-                                                                        InvoiceDate = invoice.InvoiceDate,
-                                                                        DueDate = invoice.DueDate,
-                                                                        ScheduledDate = payment.PaymentDate,
-                                                                        PaymentAmount = payment.PaymentAmount
-                                                                   }));
+            foreach (var payment in notification.Payments)
+            {
+                var invoice = _invoiceRepo.GetInvoiceHeader(DivisionHelper.GetDivisionFromBranchId(notification.BranchId), notification.CustomerNumber, payment.InvoiceNumber);
+                var invoiceTyped = KeithLink.Svc.Core.Extensions.InvoiceExtensions.DetermineType(invoice.InvoiceType);
+                orderDetails.Append(detailTemplate.Body.Inject(new
+                {
+                    InvoiceType = invoiceTyped,
+                    InvoiceNumber = payment.InvoiceNumber,
+                    InvoiceDate = invoice.InvoiceDate,
+                    DueDate = invoice.DueDate,
+                    ScheduledDate = payment.PaymentDate,
+                    PaymentAmount = payment.PaymentAmount
+                }));
             }
 
-			var bank = _bankRepo.GetBankAccount(DivisionHelper.GetDivisionFromBranchId(notification.BranchId), notification.CustomerNumber, notification.Payments[0].AccountNumber);
+            var bank = _bankRepo.GetBankAccount(DivisionHelper.GetDivisionFromBranchId(notification.BranchId), notification.CustomerNumber, notification.Payments[0].AccountNumber);
             var confirmationId = notification.Payments.FirstOrDefault().ConfirmationId;
 
             Message message = new Message();
             message.BodyIsHtml = template.IsBodyHtml;
-			message.MessageSubject = template.Subject.Inject(customer);
-            message.MessageBody = template.Body.Inject(new { 
-                                                                ConfirmationId = confirmationId,
-                                                                CustomerNumber = customer.CustomerNumber,
-                                                                CustomerName = customer.CustomerName,
-                                                                BranchId = notification.BranchId,
-                                                                BankAccount = bank.AccountNumber + " - " + bank.Name,
-                                                                PaymentDetailLines = orderDetails.ToString(),
-                                                                TotalPayments = notification.Payments.Sum(p => p.PaymentAmount)
-                                                           });
+            message.MessageSubject = template.Subject.Inject(customer);
+            StringBuilder header = _messageTemplateLogic.BuildHeader("Thank you for your payment", customer);
+            message.MessageBody = template.Body.Inject(new
+            {
+                NotifHeader = header.ToString(),
+                ConfirmationId = confirmationId,
+                BankAccount = bank.AccountNumber + " - " + bank.Name,
+                PaymentDetailLines = orderDetails.ToString(),
+                TotalPayments = notification.Payments.Sum(p => p.PaymentAmount)
+            });
             message.CustomerNumber = customer.CustomerNumber;
-			message.CustomerName = customer.CustomerName;
-			message.BranchId = customer.CustomerBranch;
+            message.CustomerName = customer.CustomerName;
+            message.BranchId = customer.CustomerBranch;
             message.NotificationType = NotificationType.PaymentConfirmation;
             return message;
         }
 
-        public void ProcessNotification(BaseNotification notification) {
+        public void ProcessNotification(BaseNotification notification)
+        {
             if (notification.NotificationType != Core.Enumerations.Messaging.NotificationType.PaymentConfirmation)
                 throw new ApplicationException("notification/handler type mismatch");
 
             // had to setup a translation for this type in Svc.Core.Extensions to deserialize the message with the concrete type
             PaymentConfirmationNotification confirmation = (PaymentConfirmationNotification)notification;
-            
+
             // load up recipients, customer and message
             Svc.Core.Models.Profile.Customer customer = _customerRepo.GetCustomerByCustomerNumber(confirmation.CustomerNumber, confirmation.BranchId);
 
-            if (customer == null) {
+            if (customer == null)
+            {
                 System.Text.StringBuilder warningMessage = new StringBuilder();
                 warningMessage.AppendFormat("Could not find customer({0}-{1}) to send Payment Confirmation notification.", notification.BranchId, notification.CustomerNumber);
                 warningMessage.AppendLine();
@@ -119,12 +126,14 @@ namespace KeithLink.Svc.Impl.Logic.Messaging {
                 warningMessage.AppendLine(notification.ToJson());
 
                 _log.WriteWarningLog(warningMessage.ToString());
-            } else {
+            }
+            else {
                 List<Recipient> recipients = base.LoadRecipients(confirmation.NotificationType, customer);
                 Message message = GetEmailMessageForNotification(confirmation, customer);
 
                 // send messages to providers...
-                if (recipients != null && recipients.Count > 0) {
+                if (recipients != null && recipients.Count > 0)
+                {
                     base.SendMessage(recipients, message);
                 }
             }
@@ -154,7 +163,7 @@ namespace KeithLink.Svc.Impl.Logic.Messaging {
             }
             else
             {
-                List<Recipient> recipients = base.LoadRecipients(confirmation.NotificationType, customer, false, false, true); 
+                List<Recipient> recipients = base.LoadRecipients(confirmation.NotificationType, customer, false, false, true);
                 Message message = GetEmailMessageForNotification(confirmation, customer);
 
                 // send messages to providers...
@@ -167,7 +176,7 @@ namespace KeithLink.Svc.Impl.Logic.Messaging {
                     catch (Exception ex)
                     {
                         _log.WriteErrorLog(String.Format("Error sending messages {0} {1}", ex.Message, ex.StackTrace));
-                    } 
+                    }
                 }
             }
         }
@@ -209,11 +218,11 @@ namespace KeithLink.Svc.Impl.Logic.Messaging {
                     catch (Exception ex)
                     {
                         _log.WriteErrorLog(String.Format("Error sending messages {0} {1}", ex.Message, ex.StackTrace));
-                    } 
+                    }
                 }
             }
         }
-        
+
         #endregion
     }
 }
