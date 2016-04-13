d2e53e450ee94b7e32c8969ba2017ccbe9a83725
Dan DeRusha <dmderusha@benekeith.com>
2016-04-13 16:55:54 -0500

    115038631/115038499- Added extra check to prevent js error when item.status is null.

--- KeithLink/KeithLink.Web.Presentation/angular-app/app/scripts/controllers/CartItemsController.js
+++ KeithLink/KeithLink.Web.Presentation/angular-app/app/scripts/controllers/CartItemsController.js
@@ -203,10 +203,10 @@ angular.module('bekApp')
     function invalidItemCheck(items) {
       var invalidItemFound = false;
       items.forEach(function(item){
-        if (!item.extPrice && !(item.extPrice > 0) && !item.isMandatory && (item.status.toUpperCase() !== 'OUT OF STOCK')){
+        if (!item.extPrice && !(item.extPrice > 0) && !item.isMandatory && (item.status && item.status.toUpperCase() !== 'OUT OF STOCK')){
           invalidItemFound = true;
           $scope.displayMessage('error', 'Please delete or enter a quantity for item ' + item.itemnumber +' before saving or submitting the cart.');
-        } else if(item.isMandatory && item.status.toUpperCase() !== "OUT OF STOCK" && item.quantity == 0 && $scope.isChangeOrder){
+        } else if(item.isMandatory && item.status && item.status.toUpperCase() !== "OUT OF STOCK" && item.quantity == 0 && $scope.isChangeOrder){
           invalidItemFound = true;
           $scope.displayMessage('error', 'Please enter a quantity for item ' + item.itemnumber +' before saving or submitting the cart.');
         }
@@ -370,7 +370,7 @@ angular.module('bekApp')
         });
 
         changeOrder.items = $filter('filter')( changeOrder.items, function(item){
-          return (item.quantity > 0 || (item.quantity == 0 && item.status.toUpperCase() === 'OUT OF STOCK')) && (PricingService.hasPackagePrice(item) || PricingService.hasCasePrice(item) || (item.price && PricingService.hasPrice(item.price)));
+          return (item.quantity > 0 || (item.quantity == 0 && item.status && item.status.toUpperCase() === 'OUT OF STOCK')) && (PricingService.hasPackagePrice(item) || PricingService.hasCasePrice(item) || (item.price && PricingService.hasPrice(item.price)));
         });
 
         return OrderService.updateOrder(changeOrder).then(function(order) {
