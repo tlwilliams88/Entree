7b9357811b87fc8122d84f83c8163f4e95c96228
Justin Mills <jmmills@bekco.com>
2016-05-13 17:15:22 -0500

    119401735- Resolved issue with valid token being passed from backend but not being referenced properly on front end causing users to see an error even though the token for password reset was correct.

--- KeithLink/KeithLink.Web.Presentation/angular-app/app/scripts/controllers/ForgotPasswordController.js
+++ KeithLink/KeithLink.Web.Presentation/angular-app/app/scripts/controllers/ForgotPasswordController.js
@@ -4,11 +4,14 @@ angular.module('bekApp')
   .controller('ForgotPasswordController', ['$scope', 'UserProfileService', '$state', 'validToken', '$stateParams', 'toaster',
     function ($scope, UserProfileService, $state, validToken, $stateParams, toaster) {
 
-    $scope.isTokenValid = (validToken.data && validToken.data.length > 0) ? true : false;
+    var token = validToken.data;
+    var validToken = validToken.data.emailAddress;
+
+    $scope.isTokenValid = (validToken && validToken.length > 0) ? true : false;
     
-    //If token is valid, validToken.data will be the email address of the current user. Otherwise, it will be null.
+    //If token is valid, variable validToken will be the email address of the current user. Otherwise, it will be null.
     if($scope.isTokenValid){
-      $scope.emailAddress = validToken.data;      
+      $scope.emailAddress = validToken;
     }
     
     $scope.resetPassword = function(passwordData) {
