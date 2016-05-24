8cf1806f1a431695f15d4db01657971a373f2975
Brett Killins <bakillins@benekeith.com>
2016-05-24 16:02:52 -0500

    120236703 - Changed archet. of how the settings work to address the problem

---
+++ KeithLink/KeithLink.Common.Impl/Repository/Settings/AppSettingsDictionary.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeithLink.Common.Impl.Repository.Settings
+{
+    public class AppSettingsDictionary
+    {
+        private static Dictionary<string, string> dict;
+
+        public static int CountSettings => dict.Keys.Count;
+
+        public static StringBuilder UniqueValue { get; set; }
+
+        public static DateTime UniqueCheck { get; set; }
+
+        public static void AddSetting(string key, string value)
+        {
+            if (dict.ContainsKey(key) == false)
+            {
+                dict.Add(key, value);
+            }
+            else
+            {
+                dict[key] = value; 
+            }
+        }
+
+        public static void AddSettings(Dictionary<string, string> newdict)
+        {
+            dict = newdict;
+        }
+
+        public static void Reset()
+        {
+            if(dict == null)
+            {
+                dict = new Dictionary<string, string>();
+            }
+            if (dict.Keys.Count > 0)
+            {
+                dict.Clear();
+            }
+        }
+        public static string GetSetting(string key)
+        {
+            return dict[key];
+        }
+    }
+}
