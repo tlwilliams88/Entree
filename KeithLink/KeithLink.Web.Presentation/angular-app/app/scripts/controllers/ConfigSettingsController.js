a6587dfc1dad62650b14728d047618b7865f2377
jmmills <jmmills@benekeith.com>
2016-05-24 16:53:57 -0500

    118951725- Additional changes to display original value and provide a separate input field for the user to enter the new value into without immediately overwriting previous setting value. Will be adding confirmation prompt prior to saving so that the user can confirm the values they would like to change.

--- KeithLink/KeithLink.Web.Presentation/angular-app/app/scripts/controllers/ConfigSettingsController.js
+++ KeithLink/KeithLink.Web.Presentation/angular-app/app/scripts/controllers/ConfigSettingsController.js
@@ -4,20 +4,26 @@ angular.module('bekApp')
   .controller('ConfigSettingsController', ['$scope', 'ConfigSettingsService',
     function($scope, ConfigSettingsService) {
 
-    	ConfigSettingsService.getAppSettings().then(function(resp){
-    		$scope.configSettings = resp;
-    	});
+        ConfigSettingsService.getAppSettings().then(function(resp){
+            $scope.configSettings = resp;
+        });
 
-    	$scope.saveConfig = function(setting){
-    		if($scope.configSettingsForm.$dirty){
-    			var key = setting.key;
-    			ConfigSettingsService.saveAppSettings(setting).then(function(resp){
-    				$scope.configSettingsForm.$setPristine();
-    				$scope.displayMessage('success', 'Successfully saved config setting ' + key);
-    			});
-    		}else {
-    			return;
-    		}
+        $scope.saveConfig = function(settings){
+            if($scope.configSettingsForm.$dirty){
+                var settingValues = [];
+                settings.forEach(function(setting){
+                    if(setting.newvalue){
+                        setting.value = setting.newvalue;
+                        settingValues.push(setting);
+                    }
+                })
+                ConfigSettingsService.saveAppSettings(settingValues).then(function(resp){
+                    $scope.configSettingsForm.$setPristine();
+                    $scope.displayMessage('success', 'Successfully saved config settings');
+                });
+            } else {
+                return;
+            }
 
-    	}
+        }
   }]);
