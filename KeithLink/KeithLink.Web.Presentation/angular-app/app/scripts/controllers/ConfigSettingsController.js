049d0551b7ddb2363097eba287bf528d91b86f98
jmmills <jmmills@benekeith.com>
2016-05-25 16:26:32 -0500

    118951725- Added new functionality to prompt user to verify and select each value they are wanting to change prior to allowing saving.  Also, instead of sending the entire settings object I am identifying only the settings that have changed to reduce the object size sent.

--- KeithLink/KeithLink.Web.Presentation/angular-app/app/scripts/controllers/ConfigSettingsController.js
+++ KeithLink/KeithLink.Web.Presentation/angular-app/app/scripts/controllers/ConfigSettingsController.js
@@ -4,24 +4,36 @@ angular.module('bekApp')
   .controller('ConfigSettingsController', ['$scope', 'ConfigSettingsService',
     function($scope, ConfigSettingsService) {
 
+        $scope.settingsVerified = false;
+
         ConfigSettingsService.getAppSettings().then(function(resp){
             $scope.configSettings = resp;
         });
+
+        $scope.setSettingsVerified = function(setting){
+            if(!$scope.isverified){
+                $scope.isverified = !$scope.isverified;
+            }
+            setting.isverified = !setting.isverified;
+        }
 
         $scope.saveConfig = function(settings){
             if($scope.configSettingsForm.$dirty){
                 var settingValues = [];
                 settings.forEach(function(setting){
-                    if(setting.newvalue){
+                    if(setting.newvalue && setting.isverified){
                         setting.value = setting.newvalue;
                         settingValues.push(setting);
                     }
                 })
                 ConfigSettingsService.saveAppSettings(settingValues).then(function(resp){
+                    $scope.configSettings = resp;
                     $scope.configSettingsForm.$setPristine();
                     $scope.displayMessage('success', 'Successfully saved config settings');
                 });
+                $scope.verifySettings = false;
             } else {
+                $scope.verifySettings = false;
                 return;
             }
 
