7baf31fbdd83953c8c667bd4cdd924910ef16654
Justin Mills <jmmills@benekeith.com>
2016-07-26 16:45:21 -0500

    120955765 - Resolved bug where last item in list added was being moved to the top of the report. Also, resolved issues with sorting by position and other fields.

--- KeithLink/KeithLink.Web.Presentation/angular-app/app/scripts/controllers/InventoryReportController.js
+++ KeithLink/KeithLink.Web.Presentation/angular-app/app/scripts/controllers/InventoryReportController.js
@@ -121,7 +121,7 @@ angular.module('bekApp')
       $scope.addRow = function(item, useListItemId) {
         var reportItem = {
           itemnumber: item.itemnumber,
-          position: $scope.report.items.length + 1,
+          position: item.position,
           name: item.name,
           packsize: item.packsize,
           pack: item.pack,
@@ -176,7 +176,7 @@ angular.module('bekApp')
        
       };
 
-      $scope.sortTable = function(field, oldSortDescending) {
+      $scope.sortTable = function(field, oldSortDescending, isSortingPosition) {
         var sortDescending = !oldSortDescending;
         if (oldSortDescending) {
           sortDescending = false;
@@ -184,16 +184,21 @@ angular.module('bekApp')
         $scope.sortField = field;
         $scope.sortDescending = sortDescending;
 
-        $scope.report.items = orderBy($scope.report.items, field, sortDescending);
+        if(!isSortingPosition){
+          $scope.report.items = orderBy($scope.report.items, field, sortDescending);
+        }
         if($scope.report.items.length && !$scope.report.items[($scope.report.items.length -1)].listitemid){
           var dummy = $scope.report.items.slice($scope.report.items.length -1, $scope.report.items.length );
           $scope.report.items = $scope.report.items.slice(0, $scope.report.items.length -1);
-          $scope.report.items.splice(0,0,dummy[0]);
+          $scope.report.items.push(dummy[0]);
+        }
+
+        if(isSortingPosition){
+          $scope.report.items = $scope.report.items.reverse();
         }
 
         $scope.report.items.forEach(function(item, index) {
-            var itemIndex = index + 1;
-            item.position = itemIndex;
+          item.position = (index + 1);
         });
 
         $scope.inventoryForm.$setDirty();
