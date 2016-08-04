86fa9075f0b0340d6a52169320eff06091ffd250
Brett Killins <bakillins@benekeith.com>
2016-08-04 08:41:50 -0500

    126594833 - Add OrderSubmissionHelper for preventing multiple submits on a cart/orderchange

--- KeithLink/KeithLink.Svc.WebApi/Controllers/OrderController.cs
+++ KeithLink/KeithLink.Svc.WebApi/Controllers/OrderController.cs
@@ -52,6 +52,27 @@ namespace KeithLink.Svc.WebApi.Controllers
         #endregion
 
         #region methods
+        [HttpGet]
+        [ApiKeyedRoute("order/issubmitted/{orderNumber}")]
+        public Models.OperationReturnModel<bool> IsSubmitted(string ordernumber)
+        {
+            Models.OperationReturnModel<bool> retVal = new Models.OperationReturnModel<bool>();
+            try
+            {
+                retVal.SuccessResponse = true;
+                retVal.IsSuccess = true;
+            }
+            catch (Exception ex)
+            {
+                _log.WriteErrorLog("IsSubmitted", ex);
+                retVal.ErrorMessage = ex.Message;
+                retVal.IsSuccess = false;
+            }
+
+            return retVal;
+        }
+
+
         /// <summary>
         /// Retrieve possible ship dates
         /// </summary>
