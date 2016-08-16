1035160c536290725f0abbbfd8313e5fc1cdc10e
Brett Killins <bakillins@benekeith.com>
2016-08-16 10:50:18 -0500

    96668766 - Adjustments to write Kdoelog file

--- KeithLink/KeithLink.Svc.Impl/Logic/Orders/OrderHistoryWriterImpl.cs
+++ KeithLink/KeithLink.Svc.Impl/Logic/Orders/OrderHistoryWriterImpl.cs
@@ -97,8 +97,8 @@ namespace KeithLink.Svc.Impl.Logic.Orders
 
                     System.IO.File.WriteAllText
                         (Path.Combine(Configuration.OrderServiceKDOELogPath, 
-                                      string.Format("{0}{1}.{2}", order.Header.OrderingSystem.ToShortString(), 
-                                                                  order.Header.ControlNumber.ToString("0000000"), 
+                                      string.Format("{0}{1}.{2}", Configuration.OrderServiceKDOELogPre, 
+                                                                  order.Header.ControlNumber.ToString("D7"), 
                                                                   Configuration.OrderServiceKDOELogExtension)), 
                          json);
                 }
