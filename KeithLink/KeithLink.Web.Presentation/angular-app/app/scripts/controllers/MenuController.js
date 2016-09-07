913fc9085e0deee96e738de892451a47f84814bc
Justin Mills <jmmills@benekeith.com>
2016-09-07 08:25:31 -0500

    129730255 - Re-add enableSubmit toggle to block multiple presses of Create Cart button on Quick Add, Update version # to 1.10.0, add semi-colon to setOrderCanceled function

--- KeithLink/KeithLink.Web.Presentation/angular-app/app/scripts/controllers/MenuController.js
+++ KeithLink/KeithLink.Web.Presentation/angular-app/app/scripts/controllers/MenuController.js
@@ -46,9 +46,9 @@ angular.module('bekApp')
   $scope.iOS = (/iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream && $scope.isMobileApp);
   $scope.Android = (!(/iPad|iPhone|iPod/.test(navigator.userAgent)) && !window.MSStream && $scope.isMobileApp);
  
-  $scope.webVersionNum = '1.9.0';
-  $scope.androidVersionNum = '1.9.0';
-  $scope.iOSVersionNum = '1.9.0';
+  $scope.webVersionNum = '1.10.0';
+  $scope.androidVersionNum = '1.10.0';
+  $scope.iOSVersionNum = '1.10.0';
 
   // KBIT ACCESS
   var usernameToken = $scope.userProfile.usernametoken;
