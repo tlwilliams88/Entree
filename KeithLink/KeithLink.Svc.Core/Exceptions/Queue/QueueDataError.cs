dcbb997ac904a89e2b6bfeaff5296f37d4a12706
Matthew Joiner <mdjoiner@benekeith.com>
2016-09-15 09:46:35 -0500

    126541617 - Added pending changes not included in original commit to Order Confirmations. Generic QueueDataError, Better logging, heartbeat check

--- KeithLink/KeithLink.Svc.Core/Exceptions/Queue/QueueDataError.cs
+++ KeithLink/KeithLink.Svc.Core/Exceptions/Queue/QueueDataError.cs
@@ -7,11 +7,10 @@ using System.Threading.Tasks;
 using RabbitMQ.Client.Exceptions;
 
 namespace KeithLink.Svc.Core.Exceptions.Queue {
-    public class QueueDataError : System.Exception {
+    public class QueueDataError<T> : System.Exception {
         #region constructor
-        public QueueDataError(string data, int lineNumber, string method, string process, string message, Exception ex) : base(message, ex) {
-            this.Data = data;
-            this.LineNumber = lineNumber;
+        public QueueDataError(T data, string method, string process, string message, Exception ex) : base(message, ex) {
+            this.ProcessingObject = data;
             this.Method = method;
             this.Process = process;
         }
@@ -25,16 +24,14 @@ namespace KeithLink.Svc.Core.Exceptions.Queue {
 
                 m.AppendLine(String.Format("Process: {0}", Process));
                 m.AppendLine(String.Format("Method:{0}", Method));
-                m.AppendLine(String.Format("Line Number: {0}", LineNumber));
-                m.AppendLine(String.Format("Data: {0}", Data));
+                m.AppendLine(String.Format("Processing Object: {0}", Newtonsoft.Json.JsonConvert.SerializeObject(ProcessingObject)));
                 m.AppendLine(String.Format("Exception Message: {0}", base.Message));
 
                 return m.ToString();
             }
         }
 
-        public string Data { get; set; }
-        public int LineNumber { get; set; }
+        public T ProcessingObject { get; set; }
         public string Method { get; set; }
         public string Process { get; set; }
         #endregion
