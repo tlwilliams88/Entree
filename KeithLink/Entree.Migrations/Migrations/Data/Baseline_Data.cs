f916ee5eb6e541b43c67d1844da608f057d9e498
Matthew Joiner <mdjoiner@benekeith.com>
2016-10-31 16:52:37 -0500

    [Finishes #133302573] - Finished the migration to fluent-migrator

---
+++ KeithLink/Entree.Migrations/Migrations/Data/Baseline_Data.cs
@@ -0,0 +1,21 @@
+﻿using FluentMigrator;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entree.Migrations.Data {
+    [Migration(2, "Baseline seed data")]
+    public class Baseline : Migration {
+        public override void Up() {
+            this.Execute.Script(@"SQL\v1.11_Baseline\v1.11_BEK_Commerce_AppData_Seed.sql");
+            this.Execute.Script(@"SQL\Messaging\Configuration.MessageTemplates.Bootstrap.sql");
+        }
+
+        public override void Down() {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ KeithLink/Entree.Migrations/Migrations/v1.11_Baseline/v1.11_Baseline.cs
@@ -0,0 +1,21 @@
+﻿using FluentMigrator;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entree.Migrations.v1_11 {
+    [Migration(1, "Baseline schema and data migrations for the new database versioning tool. Matches v1.11")]
+    public class Baseline : Migration {
+
+        public override void Up() {
+            this.Execute.Script(@"SQL\v1.11_Baseline\v1.11_BEK_Commerce_AppData.sql");
+        }
+
+        public override void Down() {
+            throw new NotImplementedException();
+        }
+    }
+}
