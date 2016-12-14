8dfd0394e5e91a77575c8df3f18af2946c237bb0
Brett Killins <bakillins@benekeith.com>
2016-12-14 16:51:24 -0600

    [NOSTORY] Add sp to migrations

---
+++ KeithLink/Entree.Migrations/Migrations/v1.12/v1.12.0_UpdateProcessContractItemList.cs
@@ -0,0 +1,20 @@
+﻿using FluentMigrator;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entree.Migrations {
+    [Migration(5, "Update ETL.ProcessContractItemList stored procedure")]
+    public class UpdateProcessContractItemList : Migration {
+        public override void Up() {
+            this.Execute.Script(@"SQL\v1.12.0\v1.12.0-ProcessContractItemList-up.sql");
+        }
+
+        public override void Down() {
+            this.Execute.Script(@"SQL\v1.12.0\v1.12.0-ProcessContractItemList-down.sql");
+        }
+    }
+}
