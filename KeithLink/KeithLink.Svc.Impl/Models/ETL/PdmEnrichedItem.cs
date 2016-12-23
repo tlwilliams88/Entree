5f17765e5e0da4fbda6fe103644f1b3e485e7630
Jeremy Ames <jwames@benekeith.com>
2016-12-23 13:38:44 -0600

    136441843 - add the stored procedure and DTO for the Enriched Item

---
+++ KeithLink/KeithLink.Svc.Impl/Models/ETL/PdmEnrichedItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeithLink.Svc.Impl.Models.ETL {
+    public class PdmEnrichedItem {
+        public string ItemNumber { get; set; }
+        public string Status { get; set; }
+        public string Name { get; set; }
+        public string Brand { get; set; }
+        public string Manufacturer { get; set; }
+        public string Desription { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime CreatedUTC { get; set; }
+        public string UpdatedBy { get; set; }
+        public DateTime ModifiedUTC { get; set; }
+    }
+}
