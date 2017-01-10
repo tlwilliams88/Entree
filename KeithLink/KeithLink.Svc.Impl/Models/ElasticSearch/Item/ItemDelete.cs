bdaffc1f647ff4960bdb2b5d57785864bf041ced
Brett Killins <bakillins@benekeith.com>
2017-01-10 12:59:20 -0600

    [#137098063] Review changes

--- KeithLink/KeithLink.Svc.Impl/Models/ElasticSearch/Item/ItemDelete.cs
+++ KeithLink/KeithLink.Svc.Impl/Models/ElasticSearch/Item/ItemDelete.cs
@@ -8,11 +8,12 @@ using KeithLink.Svc.Core.Interface.ElasticSearch;
 
 namespace KeithLink.Svc.Impl.Models.ElasticSearch.Item
 {
-    public class ItemDelete : IESItem
+    public class ItemDelete : ItemBase, IESItem
     {
-        public RootData delete { get; set; }
+        public RootData delete { get; set; } // The rootdata variable name is serialized into the action during the ES load process
+                                             // the "delete" action serves to delete an existing ES document during the batch load of elasticsearch
 
-        public string ToJson()
+        public new string ToJson() // a specialized ToJson is needed for this type of variable
         {
             return string.Format("{0}\n", JsonConvert.SerializeObject(this));
         }
