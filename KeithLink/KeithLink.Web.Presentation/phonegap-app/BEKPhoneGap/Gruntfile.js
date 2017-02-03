a91cdb8db004ee9cd252e6498c090651255f861c
Justin Mills <jmmills@benekeith.com>
2017-02-03 11:31:27 -0600

    [#139016837 #139026929 #115274677] Updated application font styling to match Foodshow on menu and display more bold across the application.  Also resolved issue with DSR section on sidebar menu and column headers on list page.

--- KeithLink/KeithLink.Web.Presentation/phonegap-app/BEKPhoneGap/Gruntfile.js
+++ KeithLink/KeithLink.Web.Presentation/phonegap-app/BEKPhoneGap/Gruntfile.js
@@ -25,7 +25,7 @@ module.exports = function(grunt) {
           'https://github.com/apache/cordova-plugin-whitelist.git'
           // ,'https://github.com/brodysoft/Cordova-SQLitePlugin.git'
         ],
-        maxBuffer: 500,
+        maxBuffer: 5000,
         platforms: ['ios', 'android'],
         config: {
           template: '_config.xml',
