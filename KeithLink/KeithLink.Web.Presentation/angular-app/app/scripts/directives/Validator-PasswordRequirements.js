0168203c4b7dae2b59008dc143966e22b51d9224
Justin Mills <jmmills@benekeith.com>
2017-03-09 09:22:14 -0600

    [#140683045] Modified regex to allow any string that does not contain an ampersand.

--- KeithLink/KeithLink.Web.Presentation/angular-app/app/scripts/directives/Validator-PasswordRequirements.js
+++ KeithLink/KeithLink.Web.Presentation/angular-app/app/scripts/directives/Validator-PasswordRequirements.js
@@ -9,7 +9,7 @@
 angular.module('bekApp')
 .directive('passwordRequirements', function () {
     
-    var EMAIL_REGEX = /[a-zA-Z0-9]+/;
+    var EMAIL_REGEX = /^[^&]*$/;
 
     return {
       require: 'ngModel',
@@ -19,7 +19,7 @@ angular.module('bekApp')
 
         ctrl.$validators.passwordRequirements = function(modelValue, viewValue) {
           var email_name_check = new RegExp(attrs.emailAddress.split('@')[0], 'i');
-          return ctrl.$isEmpty(modelValue) || ( EMAIL_REGEX.test(viewValue) && viewValue.length >7 && !email_name_check.test(viewValue) );
+          return ctrl.$isEmpty(modelValue) || ( EMAIL_REGEX.test(viewValue) && viewValue.length > 7 && !email_name_check.test(viewValue) );
         };
       }
     };
