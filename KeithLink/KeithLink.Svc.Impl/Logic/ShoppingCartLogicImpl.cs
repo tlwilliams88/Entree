ea0da02e3fd01f4b7dc5e47075bd65ce0859f357
Brett Killins <bakillins@benekeith.com>
2017-03-28 14:53:31 -0500

    [#142316497] Review change; move check for canviewunfi to helper

--- KeithLink/KeithLink.Svc.Impl/Logic/ShoppingCartLogicImpl.cs
+++ KeithLink/KeithLink.Svc.Impl/Logic/ShoppingCartLogicImpl.cs
@@ -824,7 +824,7 @@ namespace KeithLink.Svc.Impl.Logic
       
             if(products.Products.Count() < productsToValidate.Count())
             {
-                if (CheckCanViewUNFI(user, catalogInfo.CustomerId, catalogInfo.BranchId))
+                if (ProfileHelper.CheckCanViewUNFI(user, catalogInfo.CustomerId, catalogInfo.BranchId, customerRepository, _log))
                 {
                     LookupUnfoundProductsInExternalCatalogs(catalogInfo, productsToValidate, products);
                 }
@@ -896,56 +896,6 @@ namespace KeithLink.Svc.Impl.Logic
                 products.Products.AddRange(tempProducts.Products);
             }
         }
-
-        private bool CheckCanViewUNFI(UserProfile user, string customernumber, string customerbranch)
-        {
-            if ((user != null) &&
-                (user.IsInternalUser) &&
-                (KeithLink.Svc.Impl.Configuration.WhiteListedUNFIBEKUsers.Contains(user.UserName, StringComparer.CurrentCultureIgnoreCase)))
-            {
-                return true;
-            }
-            if ((user != null) &&
-                (user.RoleName.Equals("dsr", StringComparison.CurrentCultureIgnoreCase)) &&
-                (KeithLink.Svc.Impl.Configuration.WhiteListedUNFIDSRs.Contains(user.UserName, StringComparer.CurrentCultureIgnoreCase)))
-            {
-                return true;
-            }
-            if (customernumber != null && customerbranch != null)
-            {
-                if (KeithLink.Svc.Impl.Configuration.WhiteListedUNFIBranches.Contains(customerbranch, StringComparer.CurrentCultureIgnoreCase))
-                {
-                    return true;
-                }
-
-                Customer existingCustomer = customerRepository.GetCustomerByCustomerNumber(customernumber, customerbranch);
-
-                if (existingCustomer == null)
-                {
-                    _log.WriteWarningLog(string.Format("customer lookup; customer or its DSR are null with {0} on branch {1}", customernumber, customerbranch));
-                }
-                else
-                {
-                    if (existingCustomer.Dsr == null)
-                    {
-                        _log.WriteWarningLog(string.Format("customer lookup; customer or its DSR are null with {0} on branch {1}", customernumber, customerbranch));
-                    }
-                }
-
-                if (existingCustomer != null && existingCustomer.Dsr != null && existingCustomer.Dsr.Name != null &&
-                    KeithLink.Svc.Impl.Configuration.WhiteListedUNFIDSRs.Contains(existingCustomer.Dsr.Name, StringComparer.CurrentCultureIgnoreCase))
-                {
-                    return true;
-                }
-
-                if (KeithLink.Svc.Impl.Configuration.WhiteListedUNFICustomers.Contains(customernumber))
-                {
-                    return true;
-                }
-            }
-            return false;
-        }
-
         #endregion
     }
 }
