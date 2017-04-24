65b87dd98c4061cf5e60adcaf59759d775a8bbcb
Brett Killins <bakillins@benekeith.com>
2017-04-24 11:28:01 -0500

    [#139045417] Add cap to purge of data

--- KeithLink/Entree.Migrations/Scripts/AppData/List/Stored Procedures/PurgeContractListChanges/Migration0019_v2017-4-0_up.sql
+++ KeithLink/Entree.Migrations/Scripts/AppData/List/Stored Procedures/PurgeContractListChanges/Migration0019_v2017-4-0_up.sql
@@ -5,7 +5,7 @@ CREATE PROCEDURE [List].[PurgeContractListChanges]
 			@PurgeDays		INT
 AS
 BEGIN
-	DELETE	[List].[ListItemsDelta]
+	DELETE TOP (100000) [List].[ListItemsDelta]
 	WHERE
 		CreatedUtc < DATEADD(day, @PurgeDays, GETDATE())
 END
