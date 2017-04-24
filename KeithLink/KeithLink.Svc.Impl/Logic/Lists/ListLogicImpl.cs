3134a0c3af8de6305e6c4c24e15d7afd4ac7b7ec
Brett Killins <bakillins@benekeith.com>
2017-04-24 14:37:21 -0500

    [#139045417] Refactoring

--- KeithLink/KeithLink.Svc.Impl/Logic/Lists/ListLogicImpl.cs
+++ KeithLink/KeithLink.Svc.Impl/Logic/Lists/ListLogicImpl.cs
@@ -1729,89 +1729,7 @@ namespace KeithLink.Svc.Impl.Logic.Lists
 
                 while (changes != null && changes.Count > 0)
                 {
-                    try
-                    {
-                        //_log.WriteInformationLog(string.Format("Changes started, {0}",
-                        //    JsonConvert.SerializeObject(changes)));
-
-                        Customer customer = _customerRepo.GetCustomerByCustomerNumber(changes[0].CustomerId,
-                            changes[0].BranchId);
-
-                        if (customer != null)
-                        {
-                            StringBuilder header =
-                                _messageTemplateLogic.BuildHeader("Some items on your contract have changed", customer);
-
-                            MessageTemplateModel template = _messageTemplateLogic.ReadForKey(MESSAGE_TEMPLATE_CONTRACTCHANGE);
-
-                            StringBuilder itemsContent = new StringBuilder();
-                            foreach (ContractChange change in changes)
-                            {
-                                Product itemdetail = null;
-                                ProductsReturn tempProducts = null;
-                                try
-                                {
-                                    List<string> tmp = new List<string>();
-                                    tmp.Add(change.ItemNumber);
-                                    tempProducts = _catalogLogic.GetProductsByIds(change.BranchId, tmp);
-                                }
-                                catch
-                                {
-                                    _log.WriteWarningLog(string.Format("Failure to look up item {0}, branch {1}", change.ItemNumber, change.BranchId));
-                                }
-                                if (tempProducts != null)
-                                {
-                                    itemdetail = 
-                                                tempProducts.Products.Where(p => p.ItemNumber == change.ItemNumber).FirstOrDefault();
-                                }
-                                MessageTemplateModel itemTemplate =
-                                    _messageTemplateLogic.ReadForKey(MESSAGE_TEMPLATE_CONTRACTCHANGEITEMS);
-                                itemsContent.Append(itemTemplate.Body.Inject(new
-                                {
-                                    Status =
-                                    (change.Status.Equals("Added"))
-                                        ? "<font color=green>" + change.Status + "</font>"
-                                        : "<font color=red>" + change.Status + "</font>",
-                                    ProductNumber = change.ItemNumber,
-                                    ProductDescription = (itemdetail != null) ? itemdetail.Name : "",
-                                    Brand = (itemdetail != null) ? itemdetail.BrandExtendedDescription : "",
-                                    Pack = (itemdetail != null) ? itemdetail.Pack : "",
-                                    Size = (itemdetail != null) ? itemdetail.Size : ""
-                                }));
-                            }
-
-                            HasNewsNotification notifcation = new HasNewsNotification()
-                            {
-                                CustomerNumber = changes[0].CustomerId,
-                                BranchId = changes[0].BranchId,
-                                Subject = template.Subject.Inject(new
-                                {
-                                    CustomerNumber = customer.CustomerNumber,
-                                    CustomerName = customer.CustomerName
-                                }),
-                                Notification = template.Body.Inject(new
-                                {
-                                    NotifHeader = header.ToString(),
-                                    ContractChangeItems = itemsContent.ToString()
-                                })
-                            };
-                            _queueRepo.PublishToDirectedExchange(notifcation.ToJson(),
-                                Configuration.RabbitMQNotificationServer,
-                                Configuration.RabbitMQNotificationUserNamePublisher,
-                                Configuration.RabbitMQNotificationUserPasswordPublisher,
-                                Configuration.RabbitMQVHostNotification,
-                                Configuration.RabbitMQExchangeNotificationV2,
-                                Constants.RABBITMQ_NOTIFICATION_HASNEWS_ROUTEKEY);
-                            _log.WriteInformationLog(string.Format("Published to notification exchange, {0}",
-                                notifcation.ToJson()));
-                        }
-                    }
-                    catch (Exception ex)
-                    {
-                        _log.WriteErrorLog("Error creating contract change notification", ex);
-                    }
-
-                    _contractChangesRepo.Update(changes[0].ParentList_Id, true);
+                    BuildContractChangeNotifications(changes);
 
                     changes = _contractChangesRepo.ReadNextSet();
                 }
@@ -1823,6 +1741,116 @@ namespace KeithLink.Svc.Impl.Logic.Lists
                 _log.WriteErrorLog("Error processing contract change notifications", outer);
             }
         }
+
+        private void BuildContractChangeNotifications(List<ContractChange> changes)
+        {
+            try
+            {
+                //_log.WriteInformationLog(string.Format("Changes started, {0}",
+                //    JsonConvert.SerializeObject(changes)));
+
+                Customer customer = _customerRepo.GetCustomerByCustomerNumber(changes[0].CustomerId,
+                    changes[0].BranchId);
+
+                if (customer != null)
+                {
+                    var notifcation = BuildContractChangeNotification(changes, customer);
+                    _queueRepo.PublishToDirectedExchange(notifcation.ToJson(),
+                        Configuration.RabbitMQNotificationServer,
+                        Configuration.RabbitMQNotificationUserNamePublisher,
+                        Configuration.RabbitMQNotificationUserPasswordPublisher,
+                        Configuration.RabbitMQVHostNotification,
+                        Configuration.RabbitMQExchangeNotificationV2,
+                        Constants.RABBITMQ_NOTIFICATION_HASNEWS_ROUTEKEY);
+                    _log.WriteInformationLog(string.Format("Published to notification exchange, {0}",
+                        notifcation.ToJson()));
+                }
+            }
+            catch (Exception ex)
+            {
+                _log.WriteErrorLog("Error creating contract change notification", ex);
+            }
+
+            _contractChangesRepo.Update(changes[0].ParentList_Id, true);
+        }
+
+        private HasNewsNotification BuildContractChangeNotification(List<ContractChange> changes, Customer customer)
+        {
+            StringBuilder header =
+                _messageTemplateLogic.BuildHeader("Some items on your contract have changed", customer);
+
+            MessageTemplateModel template = _messageTemplateLogic.ReadForKey(MESSAGE_TEMPLATE_CONTRACTCHANGE);
+
+            StringBuilder itemsContent = new StringBuilder();
+            foreach (ContractChange change in changes)
+            {
+                AddContractChangeToNotification(change, itemsContent);
+            }
+
+            HasNewsNotification notifcation = new HasNewsNotification()
+            {
+                CustomerNumber = changes[0].CustomerId,
+                BranchId = changes[0].BranchId,
+                Subject = template.Subject.Inject(new
+                {
+                    CustomerNumber = customer.CustomerNumber,
+                    CustomerName = customer.CustomerName
+                }),
+                Notification = template.Body.Inject(new
+                {
+                    NotifHeader = header.ToString(),
+                    ContractChangeItems = itemsContent.ToString()
+                })
+            };
+            return notifcation;
+        }
+
+        private void AddContractChangeToNotification(ContractChange change, StringBuilder itemsContent)
+        {
+            itemsContent.Append(ToContractChangeString(change, TryToFindProductForContractChange(change)));
+        }
+
+        private string ToContractChangeString(ContractChange change, Product itemdetail)
+        {
+            MessageTemplateModel itemTemplate =
+                _messageTemplateLogic.ReadForKey(MESSAGE_TEMPLATE_CONTRACTCHANGEITEMS);
+            return itemTemplate.Body.Inject(new
+            {
+                Status =
+                (change.Status.Equals("Added"))
+                    ? "<font color=green>" + change.Status + "</font>"
+                    : "<font color=red>" + change.Status + "</font>",
+                ProductNumber = change.ItemNumber,
+                ProductDescription = (itemdetail != null) ? itemdetail.Name : "",
+                Brand = (itemdetail != null) ? itemdetail.BrandExtendedDescription : "",
+                Pack = (itemdetail != null) ? itemdetail.Pack : "",
+                Size = (itemdetail != null) ? itemdetail.Size : ""
+            });
+        }
+
+        private Product TryToFindProductForContractChange(ContractChange change)
+        {
+            Product itemdetail = null;
+            try
+            {
+                ProductsReturn tempProducts = null;
+                List<string> tmp = new List<string>();
+                tmp.Add(change.ItemNumber);
+                tempProducts = _catalogLogic.GetProductsByIds(change.BranchId, tmp);
+                if (tempProducts != null)
+                {
+                    itemdetail =
+                        tempProducts.Products.Where(p => p.ItemNumber == change.ItemNumber).FirstOrDefault();
+                }
+            }
+            catch
+            {
+                _log.WriteWarningLog(string.Format("Failure to look up item {0}, branch {1}", change.ItemNumber,
+                    change.BranchId));
+            }
+            return itemdetail;
+        }
+
         #endregion
     }
 }
