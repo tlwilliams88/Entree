038deb353469bec094e035e0b270a1f13aa5516a
Matthew Joiner <mdjoiner@benekeith.com>
2017-05-11 15:17:26 -0500

    [#143454335] - Added base migration runner.

---
+++ KeithLink/Entree.Migrations/Migrations/Helpers/ScriptsCollectionHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entree.Migrations.Migrations.Helpers
+{
+    public static class ScriptsCollectionHelper
+    {
+        /// <summary>
+        /// Get all the migrations for a specific migration number
+        /// </summary>
+        /// <param name="migrationNumber">0000 must be at least a length of 4</param>
+        /// <returns></returns>
+        public static List<string> GetAllMigrationFiles(string migrationNumber) {
+            List<string> returnValue = new List<string>();
+
+            // Make sure the formatting length is what we expect
+            if (migrationNumber.Length < 4)
+                throw new ApplicationException(string.Format("Migration {0} is not valid. A four or more character string is required"));
+
+            returnValue.AddRange(Directory.GetFiles(@"Scripts\", string.Format("_{0}_", migrationNumber), SearchOption.AllDirectories).ToList<string>());
+
+            return returnValue;
+        }
+    }
+}
--- KeithLink/Entree.Migrations/Migrations/v1.11_Baseline/Migration0001.cs
+++ KeithLink/Entree.Migrations/Migrations/v1.11_Baseline/Migration0001.cs
@@ -1,4 +1,6 @@
-﻿using FluentMigrator;
+﻿using Entree.Migrations.Migrations.Helpers;
+
+using FluentMigrator;
 
 using System;
 using System.Collections.Generic;
@@ -6,14 +8,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Entree.Migrations.v1_11 {
+namespace Entree.Migrations {
     [Migration(1, "Baseline schema and data migrations for the new database versioning tool. Matches v1.11")]
-    public class Baseline : Migration {
+    public class Migration0001 : Migration {
+
+        private const string APP_DATA = "AppData";
+        private const string 
 
         public override void Up() {
             // Only run this if the baseline needs scaffolded
             if (this.ApplicationContext != null && this.ApplicationContext.Equals("BaselineSetup")) {
-                this.Execute.Script(@"SQL\v1.11_Baseline\v1.11_BEK_Commerce_AppData.sql");
+                
+                foreach (string file in ScriptsCollectionHelper.GetAllMigrationFiles("0001")) {
+                    this.Execute.Script(file);
+                }
+
             }
         }
 
