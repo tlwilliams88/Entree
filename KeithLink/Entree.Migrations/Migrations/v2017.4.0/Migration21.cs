51ffaa921f910d158d1106028f8cec69cec8d7be
Matthew Joiner <mdjoiner@benekeith.com>
2017-05-11 11:26:21 -0500

    [#143454335] - Started working on contract list separation. Also started working on cleaning up and fixing all migrations with the new standard

--- KeithLink/Entree.Migrations/Migrations/v2017.4.0/Migration21.cs
+++ KeithLink/Entree.Migrations/Migrations/v2017.4.0/Migration21.cs
@@ -10,11 +10,13 @@ namespace Entree.Migrations {
     [Migration(21, "Build and use new history lists")]
     public class Migration21 : Migration {
         public override void Up() {
-            Execute.Script(@"SQL\v2017.2.0\up\stored procedures\ETL.ProcessContractItemList_up.sql");
+            Execute.Script(@"Scripts/AppData/List/Tables/ContractHeaders/2017.4.0/up.sql");
+            Execute.Script(@"Scripts/AppData/List/Tables/ContractItems/2017.4.0/up.sql");
         }
 
         public override void Down() {
-            Execute.Script(@"SQL\v2017.2.0\down\stored procedures\ETL.ProcessContractItemList_down.sql");
+            Execute.Script(@"Scripts/AppData/List/Tables/ContractHeaders/2017.4.0/down.sql");
+            Execute.Script(@"Scripts/AppData/List/Tables/ContractItems/2017.4.0/down.sql");
         }
     }
 }
---
+++ KeithLink/Entree.Migrations/Scripts/AppData/List/Tables/ContractItems/2017.4.0/up.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [List].[ContractItems]
+(
+	[Id] INT PRIMARY KEY,
+	[ParentContractHeaderId] INT NOT NULL,
+	[LineNumber] INT NOT NULL,
+	[ItemNumber] CHAR(10) NOT NULL,
+	[FromDate] DATETIME NOT NULL,
+	[ToDate] DATETIME NOT NULL,
+	[Each] BIT NULL,
+	[Category] VARCHAR(40) NOT NULL,
+	[CatalogId] VARCHAR(24) NOT NULL,
+	[CreatedUtc] DATETIME NOT NULL DEFAULT GETUTCDATE(),
+	[ModifiedUtc] DATETIME NOT NULL DEFAULT GETUTCDATE()
+);
