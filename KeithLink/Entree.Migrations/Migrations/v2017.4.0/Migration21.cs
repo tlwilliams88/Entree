cbe48d8c83fba6a694249d694c38774f52239397
Brett Killins <bakillins@benekeith.com>
2017-05-11 14:07:13 -0500

    [#143454335] Rename files

--- KeithLink/Entree.Migrations/Migrations/v2017.4.0/Migration21.cs
+++ KeithLink/Entree.Migrations/Migrations/v2017.4.0/Migration21.cs
@@ -10,15 +10,15 @@ namespace Entree.Migrations {
     [Migration(21, "Build and use new history lists")]
     public class Migration21 : Migration {
         public override void Up() {
-            Execute.Script(@"Scripts\AppData\List\Tables\HistoryHeader\Migration0021_v2017-4-0_up.sql");
-            Execute.Script(@"Scripts\AppData\List\Tables\HistoryDetail\Migration0021_v2017-4-0_up.sql");
-            Execute.Script(@"Scripts\AppData\ETL\Stored Procedures\ProcessWorksheetList\Migration0021_v2017-4-0_up.sql");
+            Execute.Script(@"Scripts\AppData\List\Tables\HistoryHeader\HistoryHeader_Migration0021_v2017-04-00_up.sql");
+            Execute.Script(@"Scripts\AppData\List\Tables\HistoryDetail\HistoryDetail_Migration0021_v2017-04-00_up.sql");
+            Execute.Script(@"Scripts\AppData\ETL\Stored Procedures\ProcessWorksheetList\ProcessWorksheetList_Migration0021_v2017-04-00_up.sql");
         }
 
         public override void Down() {
-            Execute.Script(@"Scripts\AppData\List\Tables\HistoryHeader\Migration0021_v2017-4-0_down.sql");
-            Execute.Script(@"Scripts\AppData\List\Tables\HistoryDetail\Migration0021_v2017-4-0_down.sql");
-            Execute.Script(@"Scripts\AppData\ETL\Stored Procedures\ProcessWorksheetList\Migration0021_v2017-4-0_up.sql");
+            Execute.Script(@"Scripts\AppData\List\Tables\HistoryHeader\HistoryHeader_Migration0021_v2017-04-00_down.sql");
+            Execute.Script(@"Scripts\AppData\List\Tables\HistoryDetail\HistoryDetail_Migration0021_v2017-04-00_down.sql");
+            Execute.Script(@"Scripts\AppData\ETL\Stored Procedures\ProcessWorksheetList\ProcessWorksheetList_Migration0021_v2017-4-0_up.sql");
         }
     }
 }
