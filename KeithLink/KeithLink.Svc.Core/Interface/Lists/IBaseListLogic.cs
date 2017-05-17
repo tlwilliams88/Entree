d8a4c040197c7c45ff6c03f8b6ccef7f2979ce28
Brett Killins <bakillins@benekeith.com>
2017-05-17 15:51:37 -0500

    [#142983763] Refinements

---
+++ KeithLink/KeithLink.Svc.Core/Interface/Lists/IBaseListLogic.cs
@@ -0,0 +1,20 @@
+﻿using KeithLink.Svc.Core.Models.Lists;
+using KeithLink.Svc.Core.Models.Profile;
+using KeithLink.Svc.Core.Models.SiteCatalog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeithLink.Svc.Core.Interface.Lists
+{
+    public interface IBaseListLogic
+    {
+        List<ListModel> ReadList(UserSelectedContext catalogInfo, bool headerOnly = false);
+
+        ListModel GetListModel(UserProfile user,
+                                UserSelectedContext catalogInfo,
+                                long Id);
+    }
+}
