6f94ebf0a650c26838482e3439de1412232e8bf5
Justin Mills <jmmills@benekeith.com>
2017-05-31 11:52:59 -0500

    Revert "Merge branch 'dev_2017.4_lists' of https://github.com/BEK1906/entree into dev_2017.4_lists"
    This reverts commit 8801eda7c2d1bcd9f49e0b18844b28d46206683d, reversing
    changes made to 062bcaf3b710f44c8aaec6f19259336758a8f902.

--- KeithLink/KeithLink.Web.Presentation/angular-app/app/scripts/state.js
+++ KeithLink/KeithLink.Web.Presentation/angular-app/app/scripts/state.js
@@ -269,10 +269,7 @@ angular.module('bekApp')
       }
     })
     .state('menu.lists.items', {
-      url: ':listId/?renameList',
-      params: {
-        listType : null
-      },
+      url: ':listType/:listId/?renameList',
       templateUrl: 'views/lists.html',
       controller: 'ListController',
       data: {
@@ -295,32 +292,42 @@ angular.module('bekApp')
               stillExists = false,
               pageSize = $stateParams.pageSize = LocalStorage.getPageSize(),
               params = {size: pageSize, from: 0, sort: [], message: 'Loading List...'},
-              listToBeUsed = {},
-              listHeader = {};
+              listToBeUsed = [];
 
           ListService.lists.forEach(function(list){
             if(last && last.listId && (last.listId == list.listid)){
               stillExists = true;
-              listHeader = $filter('filter')(lists, {listid: last.listId})[0];
+              var timeoutDate  = DateService.momentObject().subtract(ENV.lastListStorageTimeout, 'hours').format(Constants.dateFormat.yearMonthDayHourMinute);
+              if(last.timeset < timeoutDate){
+                stillExists = false;
+              }
             }
           });
 
           if((last && stillExists && (!$stateParams.renameList || $stateParams.renameList === 'false')) || last && last.listId == 'nonbeklist'){
+            last.timeset =  DateService.momentObject().format(Constants.dateFormat.yearMonthDayHourMinute);
+            LocalStorage.setLastList(last);
             listToBeUsed.listId = last.listId.listid ? last.listId.listid : last.listId;
-            listToBeUsed.listType = last.listId.listtype ? last.listId.listtype : last.listType;
           }
+          else{
+            LocalStorage.setLastList({});
+            listToBeUsed.listId = validListId;
+           }
+
+          var listHeader = $filter('filter')(lists, {listid: listToBeUsed.listId})[0];
+
+          listToBeUsed.listType = listHeader.type;
 
           if(listHeader && (listHeader.read_only || listHeader.isrecommended || listHeader.ismandatory)){
-            ListService.getParamsObject(params, 'lists').then(function(storedParams){
+              ListService.getParamsObject(params, 'lists').then(function(storedParams){
               $stateParams.sortingParams = storedParams;
               params = storedParams;
             });
           }
 
-          listToBeUsed.listId = listToBeUsed.listId != 'nonbeklist' ? parseInt(listToBeUsed.listId, 10) : listToBeUsed.listId;
-          listToBeUsed.listType = listHeader ? listHeader.type : listToBeUsed.listType;
+          listToBeUsed.listId = parseInt(listToBeUsed.listId, 10);
 
-          if((isNaN(listToBeUsed.listId) || !listHeader) && listToBeUsed.listId != 'nonbeklist'){
+          if((isNaN(listToBeUsed.listId) || !listHeader)){
             var historyList = $filter('filter')(ListService.lists, {name: 'History'}),
                 favoritesList = $filter('filter')(ListService.lists, {name: 'Favorites'});
 
@@ -328,7 +335,7 @@ angular.module('bekApp')
             listToBeUsed.listType = historyList.length ? historyList[0].type : favoritesList[0].type;
           }
 
-          if(listToBeUsed.listId == 'nonbeklist'){
+          if(listToBeUsed == 'nonbeklist'){
             return ListService.getCustomInventoryList();
           } else {
             LocalStorage.setLastList(listToBeUsed);
