ca5c3a1cfc6f2a886b03a7e5f59b0ffd74ca7c89
Jeremy Ames <jwames@benekeith.com>
2017-06-12 12:01:05 -0500

    [#142983763] - update the inventory valuation list logic class

--- KeithLink/KeithLink.Svc.Core/Interface/Lists/IInventoryValuationListLogic.cs
+++ KeithLink/KeithLink.Svc.Core/Interface/Lists/IInventoryValuationListLogic.cs
@@ -4,20 +4,9 @@ using System.Collections.Generic;
 using KeithLink.Svc.Core.Models.Lists;
 
 namespace KeithLink.Svc.Core.Interface.Lists {
-    public interface IInventoryValuationListLogic : IBaseListLogic
-    {
-
+    public interface IInventoryValuationListLogic : IBaseMultiListLogic {
         List<ListModel> ReadLists(UserProfile user, UserSelectedContext catalogInfo, bool headerOnly);
 
-        List<ListModel> ReadList(long reportId, UserSelectedContext catalogInfo, bool headerOnly);
-
-        void AddOrUpdateInventoryValuationItem(UserSelectedContext catalogInfo,
-                                long listId,
-                                string listName,
-                                string itemNumber,
-                                bool each,
-                                decimal quantity,
-                                string catalogId,
-                                bool active);
+        ListModel ReadList(long reportId, UserSelectedContext catalogInfo, bool headerOnly);
     }
 }
--- KeithLink/KeithLink.Svc.Impl/Logic/Lists/InventoryValuationListLogicImpl.cs
+++ KeithLink/KeithLink.Svc.Impl/Logic/Lists/InventoryValuationListLogicImpl.cs
@@ -5,16 +5,15 @@ using System.Text;
 using System.Threading.Tasks;
 using Amazon.CloudFront.Model;
 using KeithLink.Svc.Core.Extensions;
+using KeithLink.Svc.Core.Extensions.Lists;
 using KeithLink.Svc.Core.Interface.Lists;
 using KeithLink.Svc.Core.Models.Lists;
 using KeithLink.Svc.Core.Models.Profile;
 using KeithLink.Svc.Core.Models.SiteCatalog;
 using KeithLink.Svc.Core.Models.Lists.InventoryValuationList;
 
-namespace KeithLink.Svc.Impl.Logic.Lists
-{
-    public class InventoryValuationListLogicImpl : IInventoryValuationListLogic
-    {
+namespace KeithLink.Svc.Impl.Logic.Lists {
+    public class InventoryValuationListLogicImpl : IInventoryValuationListLogic {
         #region attributes
         private readonly IInventoryValuationListDetailsRepository _detailsRepo;
         private readonly IInventoryValuationListHeadersRepository _headersRepo;
@@ -29,66 +28,39 @@ namespace KeithLink.Svc.Impl.Logic.Lists
         #endregion
 
         #region methods
-        public List<ListModel> ReadLists(UserProfile user, UserSelectedContext catalogInfo, bool headerOnly)
-        {
+        private ListModel GetCompletedLists(InventoryValuationListHeader header, bool headerOnly) {
+            List<InventoryValuationListDetail> items = null;
+
+            if (!headerOnly) {
+                items = _detailsRepo.GetInventoryValuationDetails(header.Id);
+            }
+
+            return header.ToListModel(items);
+        }
+
+        public List<ListModel> ReadLists(UserProfile user, UserSelectedContext catalogInfo, bool headerOnly) {
             List<InventoryValuationListHeader> headers = _headersRepo.GetInventoryValuationListHeaders(catalogInfo);
             List<ListModel> list = new List<ListModel>();
 
-            if (headers != null && headerOnly == false)
-            {
-                foreach (var header in headers)
-                {
-                    header.Items = _detailsRepo.GetInventoryValuationDetails(header.Id);
-                    list.Add(header.ToListModel(catalogInfo));
-                }
-                return list;
-            }
-            return null;
+            headers.ForEach(h => {
+                list.Add(GetCompletedLists(h, headerOnly));
+            });
+
+            return list;
         }
 
-        public List<ListModel> ReadList(long reportId, UserSelectedContext catalogInfo, bool headerOnly)
-        {
+        public ListModel ReadList(long reportId, UserSelectedContext catalogInfo, bool headerOnly) {
             InventoryValuationListHeader header = _headersRepo.GetInventoryValuationListHeader(reportId);
 
-            if (header != null && headerOnly == false)
-            {
-               header.Items = _detailsRepo.GetInventoryValuationDetails(header.Id);
-            }
-            if (header != null)
-            {
-                return new List<ListModel>() { header.ToListModel(catalogInfo)};
-            }
-            return null;
+            return header == null ? null : GetCompletedLists(header, headerOnly);
         }
 
-        public void AddOrUpdateInventoryValuationItem(UserSelectedContext catalogInfo,
-                                long listId,
-                                string listName,
-                                string itemNumber,
-                                bool each,
-                                decimal quantity,
-                                string catalogId,
-                                bool active)
-        {
-            _detailsRepo.AddOrUpdateRecommendedItem(catalogInfo.CustomerId,
-                catalogInfo.BranchId,
-                listId,
-                listName,
-                itemNumber,
-                each,
-                quantity,
-                catalogId,
-                active);
+        public ListModel GetListModel(UserProfile user, UserSelectedContext catalogInfo, long id) {
+            return ReadList(id, catalogInfo, false);
         }
 
-        public ListModel GetListModel(UserProfile user, UserSelectedContext catalogInfo, long Id)
-        {
-            throw new NotImplementedException();
-        }
-
-        public List<ListModel> ReadList(UserProfile user, UserSelectedContext catalogInfo, bool headerOnly = false)
-        {
-            throw new NotImplementedException();
+        public List<ListModel> ReadList(UserProfile user, UserSelectedContext catalogInfo, bool headerOnly = false) {
+            return ReadLists(user, catalogInfo, headerOnly);
         }
         #endregion
     }
