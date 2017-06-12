ceae79e45433e9ce296a0e6923d8d5dbff843503
Jeremy Ames <jwames@benekeith.com>
2017-06-12 14:08:59 -0500

    [#142983763] - clean up the reminder items logic

--- KeithLink/KeithLink.Svc.Core/Interface/Lists/IRemindersListLogic.cs
+++ KeithLink/KeithLink.Svc.Core/Interface/Lists/IRemindersListLogic.cs
@@ -3,14 +3,7 @@ using KeithLink.Svc.Core.Models.SiteCatalog;
 using System.Collections.Generic;
 
 namespace KeithLink.Svc.Core.Interface.Lists {
-    public interface IRemindersListLogic : IBaseListLogic
-    {
+    public interface IRemindersListLogic : IBaseListLogic {
         List<string> GetRemindersNumbers(UserProfile user, UserSelectedContext catalogInfo);
-
-        void AddOrUpdateReminder(UserSelectedContext catalogInfo,
-            string itemNumber,
-            bool each,
-            string catalogId,
-            bool active);
     }
 }
