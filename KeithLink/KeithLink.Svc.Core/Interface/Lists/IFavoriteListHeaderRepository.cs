6d9c4bf866c28f882e0abf1ad4ef26d202d6c972
Jeremy Ames <jwames@benekeith.com>
2017-06-12 11:39:44 -0500

    [#142983763] - update the favorites logic class

--- KeithLink/KeithLink.Svc.Core/Interface/Lists/IFavoriteListHeaderRepository.cs
+++ KeithLink/KeithLink.Svc.Core/Interface/Lists/IFavoriteListHeaderRepository.cs
@@ -1,10 +1,12 @@
-﻿using KeithLink.Svc.Core.Models.Lists.Favorites;
+﻿using System;
+
+using KeithLink.Svc.Core.Models.Lists.Favorites;
 using KeithLink.Svc.Core.Models.SiteCatalog;
 
 namespace KeithLink.Svc.Core.Interface.Lists {
     public interface IFavoriteListHeaderRepository
     {
-        FavoritesListHeader GetFavoritesList(string userId, UserSelectedContext catalogInfo);
+        FavoritesListHeader GetFavoritesList(Guid userId, UserSelectedContext catalogInfo);
 
         void SaveFavoriteListHeader(FavoritesListHeader model);
     }
