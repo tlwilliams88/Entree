3aa5e2d9b914d039617bc7c9fb207f46f9d18dc3
Jeremy Ames <jwames@benekeith.com>
2017-06-15 15:38:31 -0500

    [#142983763] - add unit tests for ReminderItemsListDetailTests

---
+++ KeithLink/KeithLink.Svc.Core.Tests.Unit/Models/Lists/ReminderItems/ReminderItemsListDetailTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using FluentAssertions;
+using Xunit;
+
+using KeithLink.Svc.Core.Models.Lists.ReminderItems;
+
+namespace KeithLink.Svc.Core.Tests.Unit.Models.Lists.ReminderItems {
+    public class ReminderItemsListDetailTests {
+        private static ReminderItemsListDetail MakeItem() {
+            return new ReminderItemsListDetail() {
+                Active = true
+            };
+        }
+
+        public class Get_Active {
+            [Fact]
+            public void GoodItem_ReturnsDefaultValue() {
+                // arrange
+                var fakeItem = MakeItem();
+                var expected = true;
+
+                // act
+
+                // assert
+                fakeItem.Active
+                        .Should()
+                        .Be(expected);
+            }
+
+            [Fact]
+            public void IniatlizedItem_HasDefaultValue() {
+                // arrange
+                var test = new ReminderItemsListDetail();
+                var expected = false;
+
+                // act
+
+                // assert
+                test.Active
+                    .Should()
+                    .Be(expected);
+            }
+        }
+
+    }
+}
--- KeithLink/KeithLink.Svc.Core/Models/Lists/ReminderItems/ReminderItemsListDetail.cs
+++ KeithLink/KeithLink.Svc.Core/Models/Lists/ReminderItems/ReminderItemsListDetail.cs
@@ -1,16 +1,5 @@
-﻿using System;
-
-namespace KeithLink.Svc.Core.Models.Lists.ReminderItems
-{
-    public class ReminderItemsListDetail
-    {
-        public long Id { get; set; }
-        public long HeaderId { get; set; }
-        public string ItemNumber { get; set; }
-        public bool? Each { get; set; }
-        public string CatalogId { get; set; }
+﻿namespace KeithLink.Svc.Core.Models.Lists.ReminderItems {
+    public class ReminderItemsListDetail : BaseListDetail {
         public bool Active { get; set; }
-        public DateTime CreatedUtc { get; set; }
-        public DateTime ModifiedUtc { get; set; }
     }
 }
