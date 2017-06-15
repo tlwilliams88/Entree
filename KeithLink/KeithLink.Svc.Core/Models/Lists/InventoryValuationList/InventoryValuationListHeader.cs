91095826282fd52393becd934d33091b5b372791
Jeremy Ames <jwames@benekeith.com>
2017-06-15 15:26:48 -0500

    [#142983763] - add unit tests for InventoryValuationListDetail

--- KeithLink/KeithLink.Svc.Core/Models/Lists/InventoryValuationList/InventoryValuationListHeader.cs
+++ KeithLink/KeithLink.Svc.Core/Models/Lists/InventoryValuationList/InventoryValuationListHeader.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace KeithLink.Svc.Core.Models.Lists.InventoryValuationList {
+﻿namespace KeithLink.Svc.Core.Models.Lists.InventoryValuationList {
     public class InventoryValuationListHeader : BaseListHeader {
         public string Name { get; set; }
     }
