2f4321d65e6a4b1a0f79aac951a4dfa2263606b8
Matthew Joiner <mdjoiner@benekeith.com>
2017-06-28 13:12:05 -0500

    [#142983763] - Added types back to RecommendedItemsHeaders table for createdutc and updatedutc

--- KeithLink/Entree.Migrations/Scripts/AppData/List/Tables/RecommendedItemsHeaders/RecommendedItemsHeaders_0027_2017.04.00_up.sql
+++ KeithLink/Entree.Migrations/Scripts/AppData/List/Tables/RecommendedItemsHeaders/RecommendedItemsHeaders_0027_2017.04.00_up.sql
@@ -2,7 +2,7 @@ CREATE TABLE [List].[RecommendedItemsHeaders] (
     [Id]                BIGINT PRIMARY KEY IDENTITY(1,1) NOT NULL,
     [CustomerNumber]    CHAR(6) NULL,
     [BranchId]          CHAR(3) NULL,
-    [CreatedUtc]        ,
-    [ModifiedUtc]       
+    [CreatedUtc]        DATETIME,
+    [ModifiedUtc]       DATETIME
 );
 GO
