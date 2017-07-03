8a7d3dac7116230bc1e9c7db08f1add6320a76c6
Jeremy Ames <jwames@benekeith.com>
2017-07-03 16:56:45 -0500

    [#142983763] - modify the pre-deployment scripts. Fix tests for reminder headers. Fix save stored procedure for reminder headers

--- KeithLink/Entree.Migrations/Migrations/Data/IntegrationTests/Lists/RemindersHeaders.cs
+++ KeithLink/Entree.Migrations/Migrations/Data/IntegrationTests/Lists/RemindersHeaders.cs
@@ -6,19 +6,19 @@ namespace Entree.Migrations.Migrations.Data.IntegrationTests.Lists {
     [Profile("IntegrationTests")]
     public class RemindersHeaders : Migration {
         public override void Up() {
-            Insert.IntoTable("RemindersHeader")
+            Insert.IntoTable("RemindersHeaders")
                   .InSchema("List")
                   .Row(new { 
                     BranchId = "FDF",
-                    CustomListShares = "123456",
+                    CustomerNumber = "123456",
                     CreatedUtc = new DateTime(2017, 7, 3, 16, 8, 0, DateTimeKind.Utc),
                     ModifiedUtc = new DateTime(2017, 7, 3, 16, 9, 0, DateTimeKind.Utc)
                   });
-            Insert.IntoTable("RemindersHeader")
+            Insert.IntoTable("RemindersHeaders")
                   .InSchema("List")
                   .Row(new {
                         BranchId = "FDF",
-                        CustomListShares = "234567",
+                        CustomerNumber = "234567",
                         CreatedUtc = new DateTime(2017, 7, 3, 16, 8, 0, DateTimeKind.Utc),
                         ModifiedUtc = new DateTime(2017, 7, 3, 16, 9, 0, DateTimeKind.Utc)
                     });
