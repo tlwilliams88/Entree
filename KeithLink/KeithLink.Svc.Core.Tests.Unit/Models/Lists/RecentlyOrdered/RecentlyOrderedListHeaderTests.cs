8408f45a962b8d3ac2c9dd34476aa5ceb1fb37f1
Matthew Joiner <mdjoiner@benekeith.com>
2017-07-05 09:28:48 -0500

    [#148095087] - Finished MandatoryItems, Notes, RecentlyOrdered, RecentlyViewed, RecommendedItems model tests

---
+++ KeithLink/KeithLink.Svc.Core.Tests.Unit/Models/Lists/RecentlyOrdered/RecentlyOrderedListHeaderTests.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xunit;
+using FluentAssertions;
+
+using KeithLink.Svc.Core.Models.Lists.RecentlyOrdered;
+
+namespace KeithLink.Svc.Core.Tests.Unit.Models.Lists.RecentlyOrdered
+{
+    public class RecentlyOrderedListHeaderTests
+    {
+        public static RecentlyOrderedListHeader MakeHeader() {
+            return new RecentlyOrderedListHeader() {
+                UserId = Guid.Parse("12345678-1234-1234-1234-123456789012")
+            };
+        }
+
+        public class Get_UserId {
+            [Fact]
+            public void GoodItem_ReturnsValue() {
+                // arrange
+                RecentlyOrderedListHeader test = MakeHeader();
+                var expected = Guid.Parse("12345678-1234-1234-1234-123456789012");
+                // act
+
+                // assert
+                test.UserId
+                    .Should()
+                    .Be(expected);
+            } 
+
+            [Fact]
+            public void GoodItem_HasDefaultValue() {
+                // arrange
+                RecentlyOrderedListHeader test = new RecentlyOrderedListHeader();
+                Guid expected = Guid.Parse("00000000-0000-0000-0000-000000000000");
+                Guid notExpected = Guid.Parse("12345678-1234-1234-1234-123456789012");
+
+                // act
+
+                // assert
+                test.UserId
+                    .Should()
+                    .NotBe(notExpected);
+
+                test.UserId
+                    .Should()
+                    .Be(expected);
+            }
+        }
+    }
+}
---
+++ KeithLink/KeithLink.Svc.Core.Tests.Unit/Models/Lists/RecentlyViewed/RecentlyViewedListHeaderTests.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xunit;
+using FluentAssertions;
+
+using KeithLink.Svc.Core.Models.Lists.RecentlyViewed;
+
+namespace KeithLink.Svc.Core.Tests.Unit.Models.Lists.RecentlyViewed
+{
+    public class RecentlyViewedListHeaderTests
+    {
+        public static RecentlyViewedListHeader MakeHeader() {
+            return new RecentlyViewedListHeader() {
+                UserId = Guid.Parse("12345678-1234-1234-1234-123456789012")
+            };
+        }
+
+        public class Get_UserId {
+            [Fact]
+            public void GoodItem_ReturnsValue() {
+                // arrange
+                RecentlyViewedListHeader test = MakeHeader();
+                var expected = Guid.Parse("12345678-1234-1234-1234-123456789012");
+                // act
+
+                // assert
+                test.UserId
+                    .Should()
+                    .Be(expected);
+            } 
+
+            [Fact]
+            public void GoodItem_HasDefaultValue() {
+                // arrange
+                RecentlyViewedListHeader test = new RecentlyViewedListHeader();
+                Guid expected = Guid.Parse("00000000-0000-0000-0000-000000000000");
+                Guid notExpected = Guid.Parse("12345678-1234-1234-1234-123456789012");
+
+                // act
+
+                // assert
+                test.UserId
+                    .Should()
+                    .NotBe(notExpected);
+
+                test.UserId
+                    .Should()
+                    .Be(expected);
+            }
+        }
+    }
+}
