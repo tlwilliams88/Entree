49d52c1cca2ad2ec5202be632fe3679ca89de6b3
Brett Killins <bakillins@benekeith.com>
2017-07-06 09:07:07 -0500

    [#142983763] - Review changes

--- KeithLink/KeithLink.Svc.Impl/Service/ImportServiceImpl.cs
+++ KeithLink/KeithLink.Svc.Impl/Service/ImportServiceImpl.cs
@@ -10,13 +10,9 @@ using Excel;
 
 using KeithLink.Common.Core.Extensions;
 using KeithLink.Common.Core.Interfaces.Logging;
-using KeithLink.Svc.Core.Enumerations.List;
-using KeithLink.Svc.Core.Interface.Cart;
-using KeithLink.Svc.Core.Interface.Lists;
 using KeithLink.Svc.Core.Interface.SiteCatalog;
 using KeithLink.Svc.Core.Models.ImportFiles;
 using KeithLink.Svc.Core.Models.Lists;
-using KeithLink.Svc.Core.Models.Orders;
 using KeithLink.Svc.Core.Models.Profile;
 using KeithLink.Svc.Core.Models.SiteCatalog;
 
@@ -25,14 +21,7 @@ namespace KeithLink.Svc.Impl.Service
     public class ImportServiceImpl : IImportService
     {
         #region attributes
-        private IListLogic listServiceRepository;
-        private readonly IListService _listService;
         private ICatalogLogic catalogLogic;
-        private readonly ISiteCatalogService _catalogService;
-        private ICustomInventoryItemsRepository _customInventoryRepo;
-        private IEventLogRepository eventLogRepository;
-        private IShoppingCartLogic shoppingCartLogic;
-        private IPriceLogic priceLogic;
 
         private StringBuilder _errors;
         private StringBuilder _warnings;
@@ -49,21 +38,11 @@ namespace KeithLink.Svc.Impl.Service
         private const char TAB_DELIMITER = '\t';
         private const string BINARY_EXCEL_EXTENSION = ".xls";
 
-        private const int ITEM_NUMBER_INDEX = 0;
-        private const int ITEM_QUANTITY_INDEX = 1;
         #endregion
         #region ctor
-        public ImportServiceImpl(IListLogic listServiceRepository, ICatalogLogic catalogLogic, IListService listService,
-            IEventLogRepository eventLogRepository, IShoppingCartLogic shoppingCartLogic, IPriceLogic priceLogic,
-            ICustomInventoryItemsRepository customInventoryRepo, ISiteCatalogService catalogService)
+        public ImportServiceImpl(ICatalogLogic catalogLogic)
         {
-            _listService = listService;
             this.catalogLogic = catalogLogic;
-            _catalogService = catalogService;
-            this.eventLogRepository = eventLogRepository;
-            this.shoppingCartLogic = shoppingCartLogic;
-            this.priceLogic = priceLogic;
-            _customInventoryRepo = customInventoryRepo;
 
             _errors = new StringBuilder();
             _warnings = new StringBuilder();
@@ -185,113 +164,6 @@ namespace KeithLink.Svc.Impl.Service
                     Label = labelColumn == -1 ? string.Empty : rdr.GetString(labelColumn),
                     CatalogId = catalogInfo.BranchId
                 });
-            }
-
-            return returnValue;
-        }
-
-        private string DetermineItemNumber(string itemNumber, OrderImportOptions options, UserProfile user, UserSelectedContext catalogInfo)
-        {
-            string returnValue = null;
-
-            itemNumber = itemNumber.Replace("\"", "");
-
-            if (itemNumber.ToInt().Equals(null) && itemNumber.ToLong().Equals(null))
-            {
-                Warning(String.Format("There were problems importing the file. Item: {0} is not a valid item or UPC.", itemNumber));
-            }
-            else
-            {
-                switch (options.ItemNumber)
-                {
-                    case ItemNumberType.ItemNumberOrUPC:
-                        if (itemNumber.Length > 6)
-                        { // It is a UPC - lookup the item number
-                            returnValue = GetItemNumberFromUPC(itemNumber, options, user, catalogInfo);
-                        }
-                        else
-                        {
-                            returnValue = itemNumber.PadLeft(6, '0');
-                        }
-                        break;
-                    case ItemNumberType.UPC:
-                        returnValue = GetItemNumberFromUPC(itemNumber, options, user, catalogInfo);
-                        break;
-                    default: //ItemNumber
-                        //Just return value
-                        returnValue = itemNumber.PadLeft(6, '0'); ;
-                        break;
-                }
-            }
-
-            return returnValue;
-        }
-
-        private decimal DetermineQuantity(string itemNumber, string quantities, OrderImportOptions options, ListModel parList)
-        {
-            decimal? returnValue = null;
-
-            quantities = quantities.Replace("\"", "");
-
-            if (options.Contents.Equals(FileContentType.ItemOnly))
-            {
-                returnValue = 0;
-            }
-            else
-            {
-                if (options.ImportByInventory && parList != null && parList.Items != null)
-                {
-                    var onHandQuantity = quantities.ToDecimal();
-
-                    var parValue = parList.Items.Where(i => i.ItemNumber.Equals(itemNumber)).FirstOrDefault();
-                    if (parValue != null)
-                    {
-                        var orderQuantity = parValue.ParLevel - onHandQuantity;
-                        returnValue = orderQuantity > 0 ? orderQuantity : 0;
-                    }
-                    else
-                        returnValue = 0;
-
-                }
-                else
-                    returnValue = quantities.ToDecimal();
-            }
-
-            if (returnValue.Equals(null))
-            {
-                Warning("There was a problem during import. Quantity: {0} does not appear to be a valid number. Some quantities may not have imported properly.");
-            }
-
-            return returnValue.HasValue ? returnValue.Value : 0;
-        }
-
-        private bool DetermineBrokenCaseItem(string brokenCase, OrderImportOptions options)
-        {
-            bool returnValue = false;
-
-            brokenCase = brokenCase.Replace("\"", "");
-
-            if (brokenCase.Equals("y", StringComparison.InvariantCultureIgnoreCase))
-            {
-                returnValue = true;
-            }
-
-            return returnValue;
-        }
-
-        private string GetItemNumberFromUPC(string upc, OrderImportOptions options, UserProfile user, UserSelectedContext catalogInfo)
-        {
-            string returnValue = String.Empty;
-
-            upc = upc.Replace("\"", "");
-
-            ProductsReturn products = _catalogService.GetProductsBySearch(catalogInfo, upc, new SearchInputModel() { From = 0, Size = 10, SField = "upc" }, user);
-            foreach (Product p in products.Products)
-            {
-                if (p.UPC == upc)
-                {
-                    returnValue = p.ItemNumber;
-                }
             }
 
             return returnValue;
