3e14112ec1fcad8c7c56df5526cb3e3616025d2a
Brett Killins <bakillins@benekeith.com>
2017-07-12 09:23:39 -0500

    [#142983763] - adding history(worksheet) to migration

---
+++ KeithLink/Entree.Migrations/Scripts/AppData/List/Tables/HistoryHeader/PostDeployment/MigrateHistoryList_0021_2017.04.00_p.sql
@@ -0,0 +1,37 @@
+﻿	INSERT -- create customer contract lists from staging customerbids if they don't exist 
+		INTO [List].[HistoryHeaders]
+            ([CustomerNumber]
+            ,[BranchId]
+            ,[CreatedUtc]
+            ,[ModifiedUtc])
+        SELECT DISTINCT
+            ws.[CustomerNumber],
+            ws.[DivisionNumber]
+            ,GETUTCDATE()
+            ,GETUTCDATE()
+        FROM 
+            [ETL].[Staging_WorksheetItems] as ws
+
+    INSERT -- insert new items into all customer lists
+		INTO [List].[HistoryDetails]
+             ([HeaderId]
+              ,[LineNumber]
+              ,[ItemNumber]
+              ,[Each]
+              ,[CatalogId]
+              ,[CreatedUtc]
+              ,[ModifiedUtc])
+        SELECT
+		     h.[Id]
+		     ,0
+			 ,LTRIM(RTRIM(ws.ItemNumber))
+			 ,CASE WHEN ws.BrokenCaseCode = 'Y' THEN 1 ELSE 0 END
+			 ,LTRIM(RTRIM(ws.DivisionNumber))
+			 ,GETUTCDATE()
+			 ,GETUTCDATE()
+			FROM 
+				[ETL].[Staging_WorksheetItems] ws
+			INNER JOIN 
+				[List].[HistoryHeaders] h
+				ON h.[CustomerNumber] = ltrim(rtrim(ws.CustomerNumber)) and h.BranchId = ltrim(rtrim(ws.DivisionNumber))
+GO
