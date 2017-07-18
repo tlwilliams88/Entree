f27ee24cc26b0133b804e26622bb6a141e7f2b23
Jeremy Ames <jwames@benekeith.com>
2017-07-18 17:05:35 -0500

    [#148095087] - fix compiler warnings

--- KeithLink/KeithLink.Svc.Core/Models/Marketing/CatalogCampaignReturnModel.cs
+++ KeithLink/KeithLink.Svc.Core/Models/Marketing/CatalogCampaignReturnModel.cs
@@ -4,10 +4,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace KeithLink.Svc.Core.Models.Marketing
-{
-    public class CatalogCampaignReturnModel : CatalogCampaignHeader
-    {
-        public List<CatalogCampaignItem> Items { get; set; }
-    }
+namespace KeithLink.Svc.Core.Models.Marketing {
+    public class CatalogCampaignReturnModel : CatalogCampaignHeader { }
 }
