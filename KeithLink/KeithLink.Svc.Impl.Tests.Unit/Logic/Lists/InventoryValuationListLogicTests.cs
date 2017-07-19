226353a05e8a025b5dd6a4b0833d605b270400a1
Brett Killins <bakillins@benekeith.com>
2017-07-19 10:32:53 -0500

    [#142983763] - More review changes for inventoryvaluationlogictests

--- KeithLink/KeithLink.Svc.Impl.Tests.Unit/Logic/Lists/InventoryValuationListLogicTests.cs
+++ KeithLink/KeithLink.Svc.Impl.Tests.Unit/Logic/Lists/InventoryValuationListLogicTests.cs
@@ -1,19 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 using Autofac;
 using FluentAssertions;
+using Moq;
+using Xunit;
 
 using KeithLink.Svc.Core.Interface.Lists;
 using KeithLink.Svc.Core.Models.Lists;
 using KeithLink.Svc.Core.Models.Lists.InventoryValuationList;
 using KeithLink.Svc.Core.Models.Profile;
-
-using Moq;
-using Xunit;
 using KeithLink.Svc.Core.Models.SiteCatalog;
 using KeithLink.Svc.Impl.Logic.Lists;
 
