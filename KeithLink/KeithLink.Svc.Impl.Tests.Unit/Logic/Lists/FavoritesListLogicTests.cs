bcdd0eff63f4f18ee10a4d3db1af6206f7f0fa58
Brett Killins <bakillins@benekeith.com>
2017-07-20 09:09:11 -0500

    [#142983763] - Cleanup favoriteslistlogictests

--- KeithLink/KeithLink.Svc.Impl.Tests.Unit/Logic/Lists/FavoritesListLogicTests.cs
+++ KeithLink/KeithLink.Svc.Impl.Tests.Unit/Logic/Lists/FavoritesListLogicTests.cs
@@ -13,7 +13,7 @@ using KeithLink.Svc.Core.Models.Lists;
 using KeithLink.Svc.Core.Models.Lists.Favorites;
 using KeithLink.Svc.Core.Models.Profile;
 using KeithLink.Svc.Core.Models.SiteCatalog;
-using KeithLink.Svc.Impl.Repository.SmartResolver;
+using KeithLink.Svc.Impl.Logic.Lists;
 
 namespace KeithLink.Svc.Impl.Tests.Unit.Logic.Lists
 {
@@ -105,6 +105,7 @@ namespace KeithLink.Svc.Impl.Tests.Unit.Logic.Lists
                     CustomerId = "123456"
                 };
                 var fakeUser = new UserProfile();
+                var expectedEmptyListCount = 0;
 
                 // act
                 var results = testunit.GetFavoritedItemNumbers(fakeUser, testcontext);
@@ -112,7 +113,7 @@ namespace KeithLink.Svc.Impl.Tests.Unit.Logic.Lists
                 // assert
                 results.Count()
                        .Should()
-                       .Be(0);
+                       .Be(expectedEmptyListCount);
             }
 
             [Fact]
@@ -126,6 +127,7 @@ namespace KeithLink.Svc.Impl.Tests.Unit.Logic.Lists
                     CustomerId = "223456"
                 };
                 var fakeUser = new UserProfile();
+                var expectedEmptyListCount = 0;
 
                 // act
                 var results = testunit.GetFavoritedItemNumbers(fakeUser, testcontext);
@@ -133,7 +135,7 @@ namespace KeithLink.Svc.Impl.Tests.Unit.Logic.Lists
                 // assert
                 results.Count()
                        .Should()
-                       .Be(0);
+                       .Be(expectedEmptyListCount);
             }
 
             [Fact]
@@ -202,7 +204,7 @@ namespace KeithLink.Svc.Impl.Tests.Unit.Logic.Lists
             }
 
             [Fact]
-            public void GoodCustomerIdAndBranchHeaderOnly_ReturnsList()
+            public void GoodCustomerIdAndBranchHeaderOnly_ReturnsListWithExpectedId()
             {
                 // arrange
                 var testunit = MakeTestsUnit();
@@ -212,20 +214,15 @@ namespace KeithLink.Svc.Impl.Tests.Unit.Logic.Lists
                     CustomerId = "123456"
                 };
                 var fakeUser = new UserProfile();
-                var expected = new ListModel() {
-                                                   CustomerNumber = "123456",
-                                                   BranchId = "FUT",
-                                                   ListId=1L,
-                                                   Type = ListType.Favorite,
-                                                   Name = "Favorites"
-                };
+                var expectedId = 1;
 
                 // act
                 var results = testunit.GetFavoritesList(fakeUser.UserId, testcontext, true);
 
                 // assert
-                results.Should()
-                       .NotBeNull();
+                results.ListId
+                       .Should()
+                       .Be(expectedId);
             }
 
             [Fact]
@@ -239,7 +236,7 @@ namespace KeithLink.Svc.Impl.Tests.Unit.Logic.Lists
                     CustomerId = "123456"
                 };
                 var fakeUser = new UserProfile();
-                var expected = 1;
+                var expectedCount = 1;
 
                 // act
                 var results = testunit.GetFavoritesList(fakeUser.UserId, testcontext, false);
@@ -248,7 +245,7 @@ namespace KeithLink.Svc.Impl.Tests.Unit.Logic.Lists
                 results.Items
                        .Count()
                        .Should()
-                       .Be(expected);
+                       .Be(expectedCount);
             }
 
             [Fact]
@@ -273,6 +270,192 @@ namespace KeithLink.Svc.Impl.Tests.Unit.Logic.Lists
                        .ItemNumber
                        .Should()
                        .Be(expected);
+            }
+        }
+
+        public class GetListModel
+        {
+            [Fact]
+            public void BadBranchId_ReturnsNull()
+            {
+                // arrange
+                var testunit = MakeTestsUnit();
+                var testcontext = new UserSelectedContext()
+                {
+                    BranchId = "XXX",
+                    CustomerId = "123456"
+                };
+                var fakeUser = new UserProfile();
+                var fakeId = 0;
+
+                // act
+                var results = testunit.GetListModel(fakeUser, testcontext, fakeId);
+
+                // assert
+                results.Should()
+                       .BeNull();
+            }
+
+            [Fact]
+            public void BadCustomerId_ReturnsNull()
+            {
+                // arrange
+                var testunit = MakeTestsUnit();
+                var testcontext = new UserSelectedContext()
+                {
+                    BranchId = "FUT",
+                    CustomerId = "223456"
+                };
+                var fakeUser = new UserProfile();
+                var fakeId = 0;
+
+                // act
+                var results = testunit.GetListModel(fakeUser, testcontext, fakeId);
+
+                // assert
+                results.Should()
+                       .BeNull();
+            }
+
+            [Fact]
+            public void GoodCustomerIdAndBranchHeaderOnly_ReturnsListWithExpectedId()
+            {
+                // arrange
+                var testunit = MakeTestsUnit();
+                var testcontext = new UserSelectedContext()
+                {
+                    BranchId = "FUT",
+                    CustomerId = "123456"
+                };
+                var fakeUser = new UserProfile();
+                var expectedId = 1;
+                var fakeId = 0;
+
+                // act
+                var results = testunit.GetListModel(fakeUser, testcontext, fakeId);
+
+                // assert
+                results.ListId
+                       .Should()
+                       .Be(expectedId);
+            }
+
+            [Fact]
+            public void GoodCustomerIdAndBranch_ReturnsListWithAnItem()
+            {
+                // arrange
+                var testunit = MakeTestsUnit();
+                var testcontext = new UserSelectedContext()
+                {
+                    BranchId = "FUT",
+                    CustomerId = "123456"
+                };
+                var fakeUser = new UserProfile();
+                var expectedCount = 1;
+                var fakeId = 0;
+
+                // act
+                var results = testunit.GetListModel(fakeUser, testcontext, fakeId);
+
+                // assert
+                results.Items
+                       .Count()
+                       .Should()
+                       .Be(expectedCount);
+            }
+
+            [Fact]
+            public void GoodCustomerIdAndBranch_ReturnsListExpectedItemNumber()
+            {
+                // arrange
+                var testunit = MakeTestsUnit();
+                var testcontext = new UserSelectedContext()
+                {
+                    BranchId = "FUT",
+                    CustomerId = "123456"
+                };
+                var fakeUser = new UserProfile();
+                var expected = "123456";
+                var fakeId = 0;
+
+                // act
+                var results = testunit.GetListModel(fakeUser, testcontext, fakeId);
+
+                // assert
+                results.Items
+                       .First()
+                       .ItemNumber
+                       .Should()
+                       .Be(expected);
+            }
+        }
+
+        public class SaveList
+        { // works differently if you want to verify a mock is called; we can't go through autofac
+            [Fact]
+            public void AnyCustomerIdAndBranch_CallsSaveHeaderEveryTime()
+            {
+                // arrange
+                var mockHeaderRepo = new Mock<IFavoriteListHeadersRepository>();
+                var mockDetailsRepo = new Mock<IFavoriteListDetailsRepository>();
+                var testunit = new FavoritesListLogicImpl(mockDetailsRepo.Object, mockHeaderRepo.Object);
+                var testcontext = new UserSelectedContext()
+                {
+                    BranchId = "FUT",
+                    CustomerId = "123456"
+                };
+                var fakeUser = new UserProfile();
+                var testList = new ListModel()
+                {
+                    ListId = 1,
+                    CustomerNumber = "123456",
+                    BranchId = "FUT",
+                    Items = new List<ListItemModel>() {
+                                                                                         new ListItemModel() {
+                                                                                                                 ItemNumber = "123456"
+                                                                                                             }
+                                                                                     }
+                };
+                // act
+                testunit.SaveList(fakeUser, testcontext, testList);
+
+                // assert - Always returns what is setup provided the mock is called
+                mockDetailsRepo.Verify(h => h.SaveFavoriteListDetail(It.IsAny<FavoritesListDetail>()), Times.Once(), "Error updating");
+            }
+        }
+
+        public class Save
+        { // works differently if you want to verify a mock is called; we can't go through autofac
+            [Fact]
+            public void AnyCustomerIdAndBranch_CallsSaveDetailEveryTime()
+            {
+                // arrange
+                var mockHeaderRepo = new Mock<IFavoriteListHeadersRepository>();
+                var mockDetailsRepo = new Mock<IFavoriteListDetailsRepository>();
+                var testunit = new FavoritesListLogicImpl(mockDetailsRepo.Object, mockHeaderRepo.Object);
+                var testcontext = new UserSelectedContext()
+                {
+                    BranchId = "FUT",
+                    CustomerId = "123456"
+                };
+                var fakeUser = new UserProfile();
+                var testDetail = new FavoritesListDetail()
+                {
+                    CatalogId = "FUT",
+                    ItemNumber = "123456",
+                    Each = false,
+                    LineNumber = 1,
+                    Active = true,
+                    CreatedUtc = new DateTime(2017, 7, 14, 16, 41, 0, DateTimeKind.Utc),
+                    Id = 1,
+                    ModifiedUtc = new DateTime(2017, 7, 14, 16, 41, 0, DateTimeKind.Utc)
+                }
+;
+                // act
+                testunit.Save(fakeUser, testcontext, testDetail);
+
+                // assert - Always returns what is setup provided the mock is called
+                mockDetailsRepo.Verify(h => h.SaveFavoriteListDetail(testDetail), Times.Once(), "Error updating");
             }
         }
     }
