d9eb423c3a83631bba97076fad73e613475e2fb9
Justin Mills <jmmills@benekeith.com>
2017-07-24 13:42:20 -0500

    Added migration for updating division names to support new naming convention.

---
+++ KeithLink/Entree.Migrations/Scripts/AppData/BranchSupport/Tables/BranchSupports/PostDeployment/Branchsupports.Branchsupport_NewBranchNames_up.sql
@@ -0,0 +1,15 @@
+USE [BEK_Commerce_AppData]
+GO
+
+UPDATE [BranchSupport].[BranchSupports]
+   SET [BranchName] = (case when Id = ‘2’ then ‘West Texas’
+				when Id = ‘3’ then ‘Dallas/Fort Worth’
+				when Id = '4' then 'Gulf Coast'
+        when Id = '5' then 'Mid-South'
+        when Id = '6' then 'Arkansas'
+        when Id = '7' then 'New Mexico'
+        when Id = '8' then 'Oklahoma'
+        when Id = '9' then 'San Antonio'
+        when Id = '10' then 'Southeast'
+        end)
+GO
