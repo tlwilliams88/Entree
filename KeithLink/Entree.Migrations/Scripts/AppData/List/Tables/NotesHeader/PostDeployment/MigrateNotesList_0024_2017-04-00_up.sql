53893f0e64851464731db7b57d1abbfd98b3d35b
Matthew Joiner <mdjoiner@benekeith.com>
2017-07-27 14:06:34 -0500

    [#148095087] - Fixed Notes Header migration tabs vs spaces. Always go spaces.

--- KeithLink/Entree.Migrations/Scripts/AppData/List/Tables/NotesHeader/PostDeployment/MigrateNotesList_0024_2017-04-00_up.sql
+++ KeithLink/Entree.Migrations/Scripts/AppData/List/Tables/NotesHeader/PostDeployment/MigrateNotesList_0024_2017-04-00_up.sql
@@ -4,60 +4,60 @@
        -- Description:		Copies relevant contents from Favorites List Type in Generic List tables to their own tables
        -- =============================================
 
-	INSERT -- create customer favorite lists
-		INTO [List].[NotesHeaders]
+    INSERT -- create customer favorite lists
+        INTO [List].[NotesHeaders]
             ([CustomerNumber]
             ,[BranchId]
-			 ,[ModifiedUtc]
-			 ,[CreatedUtc])
+             ,[ModifiedUtc]
+             ,[CreatedUtc])
         SELECT 
             l.[CustomerId]
             ,l.[BranchId]
-			,GetUtcDate()
-			,GetUtcDate()
+            ,GetUtcDate()
+            ,GetUtcDate()
         FROM 
             [List].[Lists] as l
-		WHERE 
-			l.[Type] = 4 AND
+        WHERE 
+            l.[Type] = 4 AND
             l.[CustomerId] IS NOT NULL AND
             l.[BranchId] IS NOT NULL AND
-			NOT EXISTS (
-				SELECT 
-					'x'
-					FROM [List].[NotesHeaders]
-					WHERE [CustomerNumber] = l.[CustomerId]
-						AND [BranchId] = l.[BranchId]
-						AND l.[Type] = 4)
-		
+            NOT EXISTS (
+                SELECT 
+                    'x'
+                    FROM [List].[NotesHeaders]
+                    WHERE [CustomerNumber] = l.[CustomerId]
+                        AND [BranchId] = l.[BranchId]
+                        AND l.[Type] = 4)
+        
 
     INSERT
-		INTO [List].[NotesDetails]
-			([HeaderId]
-			 ,[ItemNumber]
+        INTO [List].[NotesDetails]
+            ([HeaderId]
+             ,[ItemNumber]
              ,[CatalogId]
              ,[Each]
-			 ,[Note]
-			 ,[ModifiedUtc]
-			 ,[CreatedUtc])
-		SELECT
- 			fh.[Id]
-			,li.[ItemNumber]
-			,li.[CatalogId]
-			,li.Each
-			,li.Note
-			,GetUtcDate()
-			,GetUtcDate()
-		FROM List.[ListItems] li
-		INNER JOIN List.[Lists] l on l.Id = li.ParentList_Id
-		INNER JOIN List.[NotesHeaders] fh on fh.CustomerNumber = l.CustomerId and fh.BranchId = l.BranchId
-		WHERE
-			l.Type = 4 and
-			NOT EXISTS (
-				SELECT
-					'x'
-					FROM [List].[NotesDetails]
-					INNER JOIN List.[Lists] l on l.Id = li.ParentList_Id
-					INNER JOIN List.[NotesHeaders] fh on fh.CustomerNumber = l.CustomerId and fh.BranchId = l.BranchId
-					WHERE [ItemNumber] = li.[ItemNumber]
-						AND [Each] = li.[Each]
-						AND [CatalogId] = li.[CatalogId])
+             ,[Note]
+             ,[ModifiedUtc]
+             ,[CreatedUtc])
+        SELECT
+            fh.[Id]
+            ,li.[ItemNumber]
+            ,li.[CatalogId]
+            ,li.Each
+            ,li.Note
+            ,GetUtcDate()
+            ,GetUtcDate()
+        FROM List.[ListItems] li
+        INNER JOIN List.[Lists] l on l.Id = li.ParentList_Id
+        INNER JOIN List.[NotesHeaders] fh on fh.CustomerNumber = l.CustomerId and fh.BranchId = l.BranchId
+        WHERE
+            l.Type = 4 and
+            NOT EXISTS (
+                SELECT
+                    'x'
+                    FROM [List].[NotesDetails]
+                    INNER JOIN List.[Lists] l on l.Id = li.ParentList_Id
+                    INNER JOIN List.[NotesHeaders] fh on fh.CustomerNumber = l.CustomerId and fh.BranchId = l.BranchId
+                    WHERE [ItemNumber] = li.[ItemNumber]
+                        AND [Each] = li.[Each]
+                        AND [CatalogId] = li.[CatalogId])
