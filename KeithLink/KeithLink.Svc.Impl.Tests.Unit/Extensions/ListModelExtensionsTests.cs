8c271dd8b403243a4c5ca6ac9ab1fd4886d51790
Brett Killins <bakillins@benekeith.com>
2017-07-28 10:18:12 -0500

    [#148095087] - Review changes for implement mandatory as readonly per role

--- KeithLink/KeithLink.Svc.Impl.Tests.Unit/Extensions/ListModelExtensionsTests.cs
+++ KeithLink/KeithLink.Svc.Impl.Tests.Unit/Extensions/ListModelExtensionsTests.cs
@@ -22,7 +22,7 @@ namespace KeithLink.Svc.Impl.Tests.Unit.Extensions
     public class ListModelExtensionsTests
     {
         [Fact]
-        public void GoodCustomerIdAndBranch_CallsDeleteDetail()
+        public void UsingListAsSysAdmin_SetListReadOnlyAsExpected()
         {
             // arrange
             ListModel list = new ListModel() {
@@ -33,6 +33,174 @@ namespace KeithLink.Svc.Impl.Tests.Unit.Extensions
                                                      RoleName = Constants.ROLE_NAME_SYSADMIN
             };
             var expected = false;
+
+            // act
+
+            // assert - Always returns what is setup provided the mock is called
+            list.SetUserSpecificProperties(user).ReadOnly
+                                                .Should()
+                                                .Be(expected);
+
+        }
+
+        [Fact]
+        public void UsingListAsDsr_SetListReadOnlyAsExpected()
+        {
+            // arrange
+            ListModel list = new ListModel()
+            {
+                Type = ListType.Mandatory,
+                ReadOnly = true
+            };
+            UserProfile user = new UserProfile()
+            {
+                RoleName = Constants.ROLE_NAME_DSR
+            };
+            var expected = false;
+
+            // act
+
+            // assert - Always returns what is setup provided the mock is called
+            list.SetUserSpecificProperties(user).ReadOnly
+                                                .Should()
+                                                .Be(expected);
+
+        }
+
+        [Fact]
+        public void UsingListAsDsm_SetListReadOnlyAsExpected()
+        {
+            // arrange
+            ListModel list = new ListModel()
+            {
+                Type = ListType.Mandatory,
+                ReadOnly = true
+            };
+            UserProfile user = new UserProfile()
+            {
+                RoleName = Constants.ROLE_NAME_DSM
+            };
+            var expected = true;
+
+            // act
+
+            // assert - Always returns what is setup provided the mock is called
+            list.SetUserSpecificProperties(user).ReadOnly
+                                                .Should()
+                                                .Be(expected);
+
+        }
+
+        [Fact]
+        public void UsingListAsBranchIs_SetListReadOnlyAsExpected()
+        {
+            // arrange
+            ListModel list = new ListModel()
+            {
+                Type = ListType.Mandatory,
+                ReadOnly = true
+            };
+            UserProfile user = new UserProfile()
+            {
+                RoleName = Constants.ROLE_NAME_BRANCHIS
+            };
+            var expected = true;
+
+            // act
+
+            // assert - Always returns what is setup provided the mock is called
+            list.SetUserSpecificProperties(user).ReadOnly
+                                                .Should()
+                                                .Be(expected);
+
+        }
+
+        [Fact]
+        public void UsingListAsGuest_SetListReadOnlyAsExpected()
+        {
+            // arrange
+            ListModel list = new ListModel()
+            {
+                Type = ListType.Mandatory,
+                ReadOnly = true
+            };
+            UserProfile user = new UserProfile()
+            {
+                RoleName = Constants.ROLE_NAME_GUEST
+            };
+            var expected = true;
+
+            // act
+
+            // assert - Always returns what is setup provided the mock is called
+            list.SetUserSpecificProperties(user).ReadOnly
+                                                .Should()
+                                                .Be(expected);
+
+        }
+
+        [Fact]
+        public void UsingListAsKbitAdmin_SetListReadOnlyAsExpected()
+        {
+            // arrange
+            ListModel list = new ListModel()
+            {
+                Type = ListType.Mandatory,
+                ReadOnly = true
+            };
+            UserProfile user = new UserProfile()
+            {
+                RoleName = Constants.ROLE_NAME_KBITADMIN
+            };
+            var expected = true;
+
+            // act
+
+            // assert - Always returns what is setup provided the mock is called
+            list.SetUserSpecificProperties(user).ReadOnly
+                                                .Should()
+                                                .Be(expected);
+
+        }
+
+        [Fact]
+        public void UsingListAsMarketing_SetListReadOnlyAsExpected()
+        {
+            // arrange
+            ListModel list = new ListModel()
+            {
+                Type = ListType.Mandatory,
+                ReadOnly = true
+            };
+            UserProfile user = new UserProfile()
+            {
+                RoleName = Constants.ROLE_NAME_MARKETING
+            };
+            var expected = true;
+
+            // act
+
+            // assert - Always returns what is setup provided the mock is called
+            list.SetUserSpecificProperties(user).ReadOnly
+                                                .Should()
+                                                .Be(expected);
+
+        }
+
+        [Fact]
+        public void UsingListAsPoweruser_SetListReadOnlyAsExpected()
+        {
+            // arrange
+            ListModel list = new ListModel()
+            {
+                Type = ListType.Mandatory,
+                ReadOnly = true
+            };
+            UserProfile user = new UserProfile()
+            {
+                RoleName = Constants.ROLE_NAME_POWERUSER
+            };
+            var expected = true;
 
             // act
 
