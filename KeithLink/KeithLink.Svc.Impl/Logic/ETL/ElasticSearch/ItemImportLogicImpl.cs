ea2ff3a1736b01930c5d408cf78158c160252a41
Matthew Joiner <mdjoiner@benekeith.com>
2017-08-31 15:00:10 -0500

    [#150174745] - Added keyword POC Code.

--- KeithLink/KeithLink.Svc.Impl/Logic/ETL/ElasticSearch/ItemImportLogicImpl.cs
+++ KeithLink/KeithLink.Svc.Impl/Logic/ETL/ElasticSearch/ItemImportLogicImpl.cs
@@ -85,6 +85,14 @@ namespace KeithLink.Svc.Impl.Logic.ETL {
 
                 DataTable branches = _stagingRepository.ReadAllBranches();
 
+                // POC Code - it ain't gonna be pretty
+                DataTable itemKeywordsTable = _stagingRepository.ReadAllItemKeywords();
+
+                Dictionary<string, string> itemKeywords = new Dictionary<string, string>();
+                foreach (DataRow row in itemKeywordsTable.Rows) {
+                    itemKeywords.Add(row.GetString("ItemNumber"), row.GetString("keywords"));
+                }
+
                 foreach (DataRow row in branches.Rows)
                 {
                     _eventLog.WriteInformationLog(String.Format("ETL: Import Item Process :  Start Import items to ES branch {0}", row.GetString("BranchId").ToLower()));
@@ -128,7 +136,7 @@ namespace KeithLink.Svc.Impl.Logic.ETL {
 
                                 products.Add(PopulateElasticSearchItem(rowItem, itemNutritions, itemDiet,
                                                                        itemAllergens, proprietaryItems, pdmItem, 
-                                                                       ESItems));
+                                                                       ESItems, itemKeywords));
 
                                 ESItems.Remove(rowItem.GetString("ItemId"));
                             }
@@ -396,6 +404,21 @@ namespace KeithLink.Svc.Impl.Logic.ETL {
                         brand_not_analyzed = new { type = "string", index = "not_analyzed" },
                         marketingbrand_not_analyzed = new { type = "string", index = "not_analyzed" },
                         brand_description_not_analyzed = new { type = "string", index = "not_analyzed" },
+                        keywords = new {
+                            type = "string",
+                            analyzer = "keyword",
+                            fields = new {
+                                english = new {
+                                    type = "string",
+                                    analyzer = "english"
+                                },
+                                whitespace = new {
+                                    type = "string",
+                                    analyzer = "whitespace_analyzer",
+                                    search_analyzer = "whitespace_analyzer"
+                                }
+                            }
+                        },
                         description = new {
                             type = "string",
                             fields = new {
@@ -565,7 +588,8 @@ namespace KeithLink.Svc.Impl.Logic.ETL {
                                                      Dictionary<string, List<Diet>> diets, 
                                                      Dictionary<string, Allergen> allergens, 
                                                      Dictionary<string, List<string>> proprietaryItems, 
-                                                     PdmEnrichedItem pdmData, List<string> existingItems) {
+                                                     PdmEnrichedItem pdmData, List<string> existingItems,
+                                                     Dictionary<string, string> itemKeywords) {
             NutritionalInformation nutInfo = new NutritionalInformation();
             nutInfo.BrandOwner = row.GetString("BrandOwner");
             nutInfo.CountryOfOrigin = row.GetString("CountryOfOrigin");
@@ -657,10 +681,15 @@ namespace KeithLink.Svc.Impl.Logic.ETL {
             data.AverageWeight = (row.GetDouble("FPNetWt") > 0 ? row.GetDouble("FPNetWt") / 100 : (row.GetDouble("GrossWt") > 0 ? row.GetDouble("GrossWt") / 100 : 0));
             data.Nutritional = nutInfo;
 
+            if (itemKeywords.ContainsKey(row.GetString("ItemId"))) {
+                data.Keywords = itemKeywords[row.GetString("ItemId")];
+            }
+           
             RootData index = new RootData();
             index._id = row.GetString("ItemId");
             index._index = row.GetString("BranchId").ToLower();
             index.data = data;
+
 
             if (existingItems.Contains(row.GetString("ItemId")))
             {
