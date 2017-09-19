88b111516c5dde3da512faff225bc256e98b099c
Brett Killins <bakillins@benekeith.com>
2017-09-19 08:48:41 -0500

    [#151193213] show/export notes in shopping cart / order details screen / export

--- KeithLink/KeithLink.Svc.Impl/Helpers/FavoritesAndNotesHelper.cs
+++ KeithLink/KeithLink.Svc.Impl/Helpers/FavoritesAndNotesHelper.cs
@@ -1,13 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Linq.Expressions;
-using System.Text;
-using System.Threading.Tasks;
 
 using KeithLink.Svc.Core.Interface.Lists;
-using KeithLink.Svc.Core.Models.Lists;
+using KeithLink.Svc.Core.Models.Orders;
 using KeithLink.Svc.Core.Models.Profile;
+using KeithLink.Svc.Core.Models.ShoppingCart;
 using KeithLink.Svc.Core.Models.SiteCatalog;
 
 namespace KeithLink.Svc.Impl.Helpers
@@ -15,6 +12,44 @@ namespace KeithLink.Svc.Impl.Helpers
     /* For now this helper is in the Impl assembly (for testing), but should only be called from a controller in WebApi */
     public class FavoritesAndNotesHelper
     {
+        public static void GetFavoritesAndNotesFromLists(UserProfile user, UserSelectedContext catalogInfo, List<ShoppingCartItem> prods, IListService listService)
+        {
+            List<Product> products = new List<Product>();
+            foreach (ShoppingCartItem prod in prods)
+            {
+                products.Add(new Product() { ItemNumber = prod.ItemNumber });
+            }
+
+            products = listService.MarkFavoritesAndAddNotes(user, products, catalogInfo);
+
+            if (products != null && products.Count > 0)
+            {
+                foreach (ShoppingCartItem prod in prods)
+                {
+                    SetFavoriteNotesAndInHistory(prod, products);
+                }
+            }
+        }
+
+        public static void GetFavoritesAndNotesFromLists(UserProfile user, UserSelectedContext catalogInfo, List<OrderLine> prods, IListService listService)
+        {
+            List<Product> products = new List<Product>();
+            foreach (OrderLine prod in prods)
+            {
+                products.Add(new Product() { ItemNumber = prod.ItemNumber });
+            }
+
+            products = listService.MarkFavoritesAndAddNotes(user, products, catalogInfo);
+
+            if (products != null && products.Count > 0)
+            {
+                foreach (OrderLine prod in prods)
+                {
+                    SetFavoriteNotesAndInHistory(prod, products);
+                }
+            }
+        }
+
         public static List<Product> GetFavoritesAndNotesFromLists(UserProfile user, UserSelectedContext catalogInfo, List<Product> prods, IListService listService)
         {
             List<Product> products = new List<Product>();
@@ -45,7 +80,52 @@ namespace KeithLink.Svc.Impl.Helpers
             return prod;
         }
 
-        private static void SetFavoriteNotesAndInHistory(Product prod, List<Product> products) {
+        private static void SetFavoriteNotesAndInHistory(ShoppingCartItem prod, List<Product> products)
+        {
+            if (products != null)
+            {
+                var prdList = products.Where(p => p.ItemNumber == prod.ItemNumber)
+                                      .ToList();
+
+                if (prdList != null &&
+                    prdList.Count > 0)
+                {
+                    Product prd = products.Where(p => p.ItemNumber == prod.ItemNumber)
+                                          .First();
+
+                    if (prd != null)
+                    {
+                        prod.Favorite = prd.Favorite;
+                        prod.Notes = prd.Notes;
+                    }
+                }
+            }
+        }
+
+        private static void SetFavoriteNotesAndInHistory(Product prod, List<Product> products)
+        {
+            if (products != null)
+            {
+                var prdList = products.Where(p => p.ItemNumber == prod.ItemNumber)
+                                      .ToList();
+
+                if (prdList != null &&
+                    prdList.Count > 0)
+                {
+                    Product prd = products.Where(p => p.ItemNumber == prod.ItemNumber)
+                                          .First();
+
+                    if (prd != null)
+                    {
+                        prod.Favorite = prd.Favorite;
+                        prod.Notes = prd.Notes;
+                        prod.InHistory = prd.InHistory;
+                    }
+                }
+            }
+        }
+
+        private static void SetFavoriteNotesAndInHistory(OrderLine prod, List<Product> products) {
             if (products != null) {
                 var prdList = products.Where(p => p.ItemNumber == prod.ItemNumber)
                                       .ToList();
@@ -58,7 +138,6 @@ namespace KeithLink.Svc.Impl.Helpers
                     if (prd != null) {
                         prod.Favorite = prd.Favorite;
                         prod.Notes = prd.Notes;
-                        prod.InHistory = prd.InHistory;
                     }
                 }
             }
