1beff39aead8997521c16c76e4adfc7fb7088dbd
Brett Killins <bakillins@benekeith.com>
2017-09-27 11:00:51 -0500

    [#150204173] Review changes

--- KeithLink/Entree.Migrations/Scripts/AppData/Configuration/Tables/AppSettings/PostDeployment/AppSettingsSeed_0044_2017-06-00_down.sql
+++ KeithLink/Entree.Migrations/Scripts/AppData/Configuration/Tables/AppSettings/PostDeployment/AppSettingsSeed_0044_2017-06-00_down.sql
@@ -1,22 +1,3 @@
-﻿declare @AppSettings as TABLE 
-( 
-    [Key]      VARCHAR (50)  NOT NULL,
-    [Value]    VARCHAR (MAX) NOT NULL,
-    [Comment]  VARCHAR (MAX) NOT NULL,
-    [Disabled] BIT           DEFAULT ((0)) NOT NULL,
-    PRIMARY KEY CLUSTERED ([Key] ASC)
-)
-
-INSERT 
-	INTO @AppSettings 
-		([Key], [Value], [Comment], [Disabled]) 
-	VALUES 
-		('ProcessContractChanges', 'false', 'Queue Service Functions', 0)
-
-MERGE INTO [Configuration].[AppSettings] A
-USING @AppSettings B ON (A.[Key] = B.[Key])
-WHEN MATCHED THEN
-    UPDATE SET A.[Comment] = B.[Comment], A.[Value] = B.[Value], A.[Disabled] = B.[Disabled]
-WHEN NOT MATCHED THEN
-    INSERT (Comment, [Key], Value, [Disabled]) 
-	  VALUES(B.[Comment],B.[Key],B.[Value],B.[Disabled]);
+﻿    UPDATE [Configuration].[AppSettings]
+        SET [Value] = 'false' 
+        WHERE [Key] = 'ProcessContractChanges'
