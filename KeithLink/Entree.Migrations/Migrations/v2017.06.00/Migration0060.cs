493dffed7a07bba17019effed1724b4852030a87
Brett Killins <bakillins@benekeith.com>
2017-11-06 08:10:11 -0600

    [#152565741] Update stored procedure for saving a mandatory list

---
+++ KeithLink/Entree.Migrations/Migrations/v2017.06.00/Migration0060.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BEK.FluentMigratorBase;
+using FluentMigrator;
+
+namespace Entree.Migrations {
+    [Migration(60, "Added keywords field to elasticsearch and keywords ETL")]
+    public class Migration0060 : BaseMigration {
+        public Migration0060() {
+            MigrationNumber = "0060";
+        }
+    }
+}
