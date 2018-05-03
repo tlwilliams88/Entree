e1bc7d46322b6c7e8030e01bf453db8df1656cef
Brett Killins <bakillins@benekeith.com>
2018-05-03 09:23:47 -0500

    Update Migration0070.cs

--- KeithLink/Entree.Migrations/Migrations/v2018.05.00/Migration0070.cs
+++ KeithLink/Entree.Migrations/Migrations/v2018.05.00/Migration0070.cs
@@ -2,7 +2,7 @@
 using FluentMigrator;
 
 namespace Entree.Migrations.Migrations.v2018._05._00 {
-    [Migration(70, "Add setting to toggle showing recommended items")]
+    [Migration(70, "Added ETL Staging Growth and Recovery tables")]
     public class Migration0070 : BaseMigration {
         public Migration0070() {
             base.MigrationNumber = "0070";
