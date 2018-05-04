a3d191277c7578d3a42f37a667f8c4b81702b6c6
Matthew Joiner <mdjoiner@benekeith.com>
2018-05-04 16:39:25 -0500

    [#156962934] - IDapperDatabase implemented, integration tests for G&R, G&R repository

---
+++ KeithLink/KeithLink.Svc.Core/Models/Customers/GrowthAndRecoveriesModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeithLink.Svc.Core.Models.Customers
+{
+    public class GrowthAndRecoveriesModel
+    {
+        public enum GrowthAndRecoveryType {
+            Growth = 1,
+            Recovery = 2
+        }
+
+        public int Id { get; set; }
+        public string BranchId { get; set; }
+        public decimal Amount { get; set; }
+        public int GrowthAndRecoveryProductGroup { get; set; }
+        public GrowthAndRecoveryType GrowthAndReccoveryTypeKey { get; set; }
+    }
+}
---
+++ KeithLink/KeithLink.Svc.Impl/Repository/Customers/GrowthAndRecoveriesRepository.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using KeithLink.Svc.Core.Interface.Customers;
+using KeithLink.Svc.Core.Interface.DataConnection;
+using KeithLink.Svc.Core.Models.Customers;
+
+namespace KeithLink.Svc.Impl.Repository.Customers
+{
+    public class GrowthAndRecoveriesRepository : IGrowthAndRecoveriesRepository {
+
+        private IDapperDatabaseConnection connection;
+        public GrowthAndRecoveriesRepository(IDapperDatabaseConnection dbConnection) {
+            connection = dbConnection;
+        }
+
+        public List<GrowthAndRecoveriesModel> GetGrowthAdnGetGrowthAndRecoveryOpportunities(string customerNumber, string branchId) {
+            List<GrowthAndRecoveriesModel> results = connection.Query<GrowthAndRecoveriesModel>(@"
+                SELECT
+	                g.Id,
+	                g.BranchId,
+	                g.CustomerNumber,
+	                g.Amount,
+	                g.GrowthAndRecoveryProductGroup,
+	                g.GrowthAndRecoveryTypeKey,
+	                pg.Code
+                FROM Customers.GrowthAndRecoveries g
+                LEFT JOIN Customers.GrowthAndRecoveryProductGroups pg ON pg.GrowthAndRecoveryProductGroup = g.GrowthAndRecoveryProductGroup
+                WHERE 
+	                g.CustomerNumber = @CustomerNumber
+                AND	g.BranchId = @BranchId
+                ORDER BY g.Amount DESC",
+                new {
+                    CustomerNumber = customerNumber,
+                    BranchId = branchId
+                }).ToList();
+
+            return results;
+        }
+    }
+}
