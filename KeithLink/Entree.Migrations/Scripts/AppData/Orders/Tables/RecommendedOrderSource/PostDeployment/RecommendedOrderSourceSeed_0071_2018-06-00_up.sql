c8a9c863d2bb980ab16df3b5f0a3af6b5f27bc8d
Brett Killins <bakillins@benekeith.com>
2018-05-08 14:22:07 -0500

    [#157359721] change case of CartIQ / SalesIQ

--- KeithLink/Entree.Migrations/Scripts/AppData/Orders/Tables/RecommendedOrderSource/PostDeployment/RecommendedOrderSourceSeed_0071_2018-06-00_up.sql
+++ KeithLink/Entree.Migrations/Scripts/AppData/Orders/Tables/RecommendedOrderSource/PostDeployment/RecommendedOrderSourceSeed_0071_2018-06-00_up.sql
@@ -3,9 +3,9 @@ INSERT
 	INTO [Orders].[RecommendedOrderSource] 
 		([OrderSource], [Description]) 
 	VALUES 
-		('cartIQ', 'Recommended to order because of customer type')
+		('CartIQ', 'Recommended to order because of customer type')
 INSERT 
 	INTO [Orders].[RecommendedOrderSource] 
 		([OrderSource], [Description]) 
 	VALUES 
-		('salesIQ', 'Recommended to order because of past sales')
+		('SalesIQ', 'Recommended to order because of past sales')
