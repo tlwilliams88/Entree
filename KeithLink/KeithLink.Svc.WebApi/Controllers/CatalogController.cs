be17530c09da8c3fb81e3b96cb7a5911c78c876d
Brett Killins <bakillins@benekeith.com>
2018-07-02 16:38:20 -0500

    [#156937085] Review changes 3

--- KeithLink/KeithLink.Svc.WebApi/Controllers/CatalogController.cs
+++ KeithLink/KeithLink.Svc.WebApi/Controllers/CatalogController.cs
@@ -87,9 +87,9 @@ namespace KeithLink.Svc.WebApi.Controllers {
             try
             {
                 IEnumerable<KeyValuePair<string, string>> pairs = Request.GetQueryNameValuePairs();
-                if (catalogType.ToLower().Equals("unfi") && this.SelectedUserContext.BranchId.Equals("FEL"))
+                if (catalogType.Equals(Constants.CATALOG_UNFI) && this.SelectedUserContext.BranchId.Equals(Constants.BRANCH_FEL))
                 {
-                    catalogType = "unfi_e";
+                    catalogType = Constants.CATALOG_UNFIEAST;
                 }
                 ret.SuccessResponse = _catalogLogic.GetCategories(0, 2000, catalogType);
                 ret.IsSuccess = true;
@@ -142,9 +142,9 @@ namespace KeithLink.Svc.WebApi.Controllers {
             OperationReturnModel<ProductsReturn> ret = new OperationReturnModel<ProductsReturn>();
             try
             {
-                if (catalogType.ToLower().Equals("unfi") && this.SelectedUserContext.BranchId.Equals("FEL"))
+                if (catalogType.Equals(Constants.CATALOG_UNFI) && this.SelectedUserContext.BranchId.Equals(Constants.BRANCH_FEL))
                 {
-                    catalogType = "unfi_e";
+                    catalogType = Constants.CATALOG_UNFIEAST;
                 }
                 searchModel.CatalogType = catalogType;
 
@@ -271,9 +271,9 @@ namespace KeithLink.Svc.WebApi.Controllers {
             try
             {
                 IEnumerable<KeyValuePair<string, string>> pairs = Request.GetQueryNameValuePairs();
-                if (catalogType.ToLower().Equals("unfi") && this.SelectedUserContext.BranchId.Equals("FEL"))
+                if (catalogType.Equals(Constants.CATALOG_UNFI) && this.SelectedUserContext.BranchId.Equals(Constants.BRANCH_FEL))
                 {
-                    catalogType = "unfi_e";
+                    catalogType = Constants.CATALOG_UNFIEAST;
                 }
 
                 Product prod = _catalogLogic.GetProductById(this.SelectedUserContext, id, this.AuthenticatedUser, catalogType);
@@ -380,6 +380,10 @@ namespace KeithLink.Svc.WebApi.Controllers {
             OperationReturnModel<ProductsReturn> ret = new OperationReturnModel<ProductsReturn>();
             try
             {
+                if (catalogType.Equals(Constants.CATALOG_UNFI) && this.SelectedUserContext.BranchId.Equals(Constants.BRANCH_FEL))
+                {
+                    catalogType = Constants.CATALOG_UNFIEAST;
+                }
                 searchModel.CatalogType = catalogType;
                 ProductsReturn prods = _catalogService.GetProductsBySearch(this.SelectedUserContext, searchTerms, searchModel, this.AuthenticatedUser);
 
