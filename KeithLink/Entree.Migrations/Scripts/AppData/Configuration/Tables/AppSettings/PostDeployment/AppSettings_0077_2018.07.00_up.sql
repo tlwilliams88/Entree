a18628406ccf3b851966deb8a0538651a9958170
Daniel DeRusha <dmderusha@benekeith.com>
2018-07-05 09:53:07 -0500

    [#158011149] Changing connection string to point at prod azure blob

--- KeithLink/Entree.Migrations/Scripts/AppData/Configuration/Tables/AppSettings/PostDeployment/AppSettings_0077_2018.07.00_up.sql
+++ KeithLink/Entree.Migrations/Scripts/AppData/Configuration/Tables/AppSettings/PostDeployment/AppSettings_0077_2018.07.00_up.sql
@@ -1,4 +1,4 @@
 /****** Object:  Table [Configuration].[AppSettings]    Script Date: 10/27/2016 1:05:25 PM ******/
 INSERT INTO [Configuration].[AppSettings]
 ([KEY], [VALUE], [COMMENT], [DISABLED])
-VALUES ('AzureConnectionString', 'DefaultEndpointsProtocol=https;AccountName=bekblobtest;AccountKey=EYWilrFss+KiSNrsbaPcLmFE4cQgInWkQ0IXAsiAiLvmSGSqfqSAy3MkvGhQri1t/09c9XWvR4Gk4ku84xxtLw==;EndpointSuffix=core.windows.net', 'This is the connection string to Azure blob storage', '0')
+VALUES ('AzureConnectionString', 'DefaultEndpointsProtocol=https;AccountName=bekstorage;AccountKey=40MASQoiTQ9UqGrP6+KruZPjmWFWR8qObX8O86ea6i8/B6HttbJxMQKalij0TBS+SxdvJ13g5+ZvrxebAy0yKw==;EndpointSuffix=core.windows.net', 'This is the connection string to Azure blob storage', '0')
