cf56a6797de5eeb430f4dac137ad1a6e7d4250fc
Justin Mills <jmmills@benekeith.com>
2018-07-10 15:19:14 -0500

    [#156521526] Updated messaging for each specific form.  Added BranchService and  dependencies.

--- KeithLink/KeithLink.Web.Presentation/angular-app/app/scripts/controllers/UserFeedbackController.js
+++ KeithLink/KeithLink.Web.Presentation/angular-app/app/scripts/controllers/UserFeedbackController.js
@@ -1,8 +1,26 @@
 'use strict';
 
 angular.module('bekApp')
-  .controller('UserFeedbackController', ['$scope', '$state', 'UserFeedbackService', 'SessionService', 'BranchService',
-    function ($scope, $state, UserFeedbackService, SessionService, BranchService) {
+  .controller('UserFeedbackController', ['$scope', '$state', 'UserFeedbackService', 'SessionService', 'BranchService', '$stateParams',
+    function ($scope, $state, UserFeedbackService, SessionService, BranchService, $stateParams) {
+
+      $scope.formMessage;
+      $scope.formType;
+
+      switch($stateParams.audience) {
+        case "Support":
+          $scope.formType = 'Support';
+          $scope.formMessage = 'to submit your Entree feedback.';
+        break;
+        case "BranchSupport":
+          $scope.formType = 'Branch Support';
+          $scope.formMessage = 'to contact your Local Support Staff.';
+        break;
+        case "SalesRep":
+          $scope.formType = 'Sales Rep';
+          $scope.formMessage = 'to contact your Sales Representative.'
+        break;
+      }
 
       var init = function () {
         $scope.userProfile = SessionService.userProfile; //Get current users profile from LocalStorage
@@ -13,19 +31,25 @@ angular.module('bekApp')
           BranchService.getBranches().then(
             function (resp) {
               $scope.branches = resp;
+
+              findBranch(branchId);
             }
           )
-        };
+        } else {
+          findBranch(branchId);
+        }
 
+      };
+
+      function findBranch(branchid){
         angular.forEach($scope.branches,
           function (branch) {
-            if (branch.id.toUpperCase() === branchId.toUpperCase()) {
+            if (branch.id.toUpperCase() === branchid.toUpperCase()) {
               $scope.branch = branch;
             }
           }
         );
-
-      };
+      }
 
       $scope.goBack = function () {
         $state.go('menu.home');
