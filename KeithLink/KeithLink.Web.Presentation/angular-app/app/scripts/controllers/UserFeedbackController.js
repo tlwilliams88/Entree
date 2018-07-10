555d19df536a61d7e7fbb94310b35277cc14a0c3
John Devonshire <jjdevonshire@benekeith.com>
2018-07-10 13:21:21 -0500

    Replaced GetCustomerForUser with GetCustomerByCustomerNumber. Added branch support and salesman phone numbers to feedback form.

--- KeithLink/KeithLink.Web.Presentation/angular-app/app/scripts/controllers/UserFeedbackController.js
+++ KeithLink/KeithLink.Web.Presentation/angular-app/app/scripts/controllers/UserFeedbackController.js
@@ -6,6 +6,25 @@ angular.module('bekApp')
 
       var init = function () {
         $scope.userProfile = SessionService.userProfile; //Get current users profile from LocalStorage
+
+        var branchId = $scope.selectedUserContext.customer.customerBranch;
+
+        if (!$scope.branches) {
+          BranchService.getBranches().then(
+            function (resp) {
+              $scope.branches = resp;
+            }
+          )
+        };
+
+        angular.forEach($scope.branches,
+          function (branch) {
+            if (branch.id.toUpperCase() === branchId.toUpperCase()) {
+              $scope.branch = branch;
+            }
+          }
+        );
+
       };
 
       $scope.goBack = function () {
@@ -17,10 +36,10 @@ angular.module('bekApp')
         userFeedback.browserUserAgent = navigator.userAgent;
         userFeedback.browserVendor = navigator.vendor;
 
-       $scope.submitUserFeedbackErrorMessage = null;
+        $scope.submitUserFeedbackErrorMessage = null;
 
-       UserFeedbackService.submitUserFeedback(userFeedback).then(
-         function (profile) {
+        UserFeedbackService.submitUserFeedback(userFeedback).then(
+          function (profile) {
             $scope.userFeedback = {};
             $scope.userFeedbackForm.$setPristine();
             $scope.displayMessage('success', 'Successfully sent feedback.');
