7d25dc64ac83ef9aac739d1e39cd9ff46f2e8316
Justin Mills <jmmills@benekeith.com>
2018-07-11 13:46:18 -0500

    [#158474960] Moving mobile offline changes to current branch

--- KeithLink/KeithLink.Web.Presentation/angular-app/app/scripts/controllers/AddToOrderController.js
+++ KeithLink/KeithLink.Web.Presentation/angular-app/app/scripts/controllers/AddToOrderController.js
@@ -1074,7 +1074,7 @@ angular.module('bekApp')
       var invalidItemFound = false;
 
       updatedCart.items.forEach(function(cartitem){
-        if (!cartitem.extPrice && !(cartitem.extPrice > 0) && !((cartitem.quantity === 0 || cartitem.quantity == '') && cartitem.status && cartitem.status.toUpperCase() === 'OUT OF STOCK')){
+        if (!cartitem.extPrice && !(cartitem.extPrice > 0) && !((cartitem.quantity === 0 || cartitem.quantity == '' && !(isMobileApp || isMobile)) && cartitem.status && cartitem.status.toUpperCase() === 'OUT OF STOCK')){
           invalidItemFound = true;
           $scope.displayMessage('error', 'Cannot create cart. Item ' + cartitem.itemnumber +' is invalid.  Please contact DSR for more information.');
         }
