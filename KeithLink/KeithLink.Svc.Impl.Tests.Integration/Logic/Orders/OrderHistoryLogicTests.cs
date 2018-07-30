0cb0196365d338be86adf3bdeb243c325693715e
John Devonshire <jjdevonshire@benekeith.com>
2018-07-30 09:32:03 -0500

    Initial push.

---
+++ KeithLink/KeithLink.Svc.Impl.Tests.Integration/Logic/Orders/OrderHistoryLogicTests.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.IO;
+using System.Reflection;
+
+using Autofac;
+
+using FluentAssertions;
+
+using KeithLink.Common.Core.Extensions;
+using KeithLink.Common.Core.Interfaces.Logging;
+using KeithLink.Svc.Core.Interface.Common;
+using KeithLink.Svc.Core.Interface.OnlinePayments.Invoice;
+using KeithLink.Svc.Core.Interface.Orders;
+using KeithLink.Svc.Core.Interface.Orders.History;
+using KeithLink.Svc.Core.Interface.Profile;
+using KeithLink.Svc.Core.Interface.SiteCatalog;
+using KeithLink.Svc.Core.Models.Orders.History;
+using EF = KeithLink.Svc.Core.Models.Orders.History.EF;
+using KeithLink.Svc.Core.Models.Profile;
+using KeithLink.Svc.Core.Models.SiteCatalog;
+using KeithLink.Svc.Impl.Logic.Orders;
+using KeithLink.Svc.Impl.Repository.EF.Operational;
+using KeithLink.Svc.Impl.Repository.SmartResolver;
+using KeithLink.Svc.Impl.Tests.Integration.Repository;
+
+using Newtonsoft.Json;
+
+using Xunit;
+
+namespace KeithLink.Svc.Impl.Tests.Integration.Logic.Orders
+{
+    public class OrderHistoryLogicTests
+    {
+        #region ParseMainframeFile
+        public class ParseMainframeFile
+        {
+            [Fact]
+            public void OrderHistory_HasValidHeadersAndOrderDate()
+            {
+                // arrange
+                IOrderHistoryLogic testunit = MakeUnitToBeTested();
+
+                // act
+                var mockDataReader = GetMockDataReader("OT780FDF-2018206-10273884.TXT");
+                OrderHistoryFileReturn orderHistoryFiles = testunit.ParseMainframeFile(mockDataReader);
+                mockDataReader.Close();
+
+                // assert
+                orderHistoryFiles.Files.Count.Should().Be(40);
+                orderHistoryFiles.Files.ForEach(file => CheckFile(file));
+            }
+
+        }
+        #endregion
+
+
+        #region ReadOrderFromQueue
+        public class ReadOrderFromQueue
+        {
+            [Fact]
+            public void OrderHistory_HasValidHeadersAndOrderDate()
+            {
+                // arrange
+                IOrderHistoryLogic testunit = MakeUnitToBeTested();
+
+                // act
+                string jsonOrderHistoryFile = testunit.ReadOrderFromQueue();
+
+                // assert
+                OrderHistoryFile orderHistoryFile = JsonConvert.DeserializeObject<OrderHistoryFile>(jsonOrderHistoryFile);
+                CheckFile(orderHistoryFile);
+            }
+        }
+        #endregion
+
+
+        #region ProcessOrder
+        public class ProcessOrder : MigratedDatabaseTest
+        {
+            [Fact]
+            public void OrderHistory_IsSentToRepositoryWithValidOrderDate()
+            {
+                // arrange
+                IOrderHistoryLogic testunit = MakeUnitToBeTested();
+
+                // act
+                string jsonOrderHistoryFile = GetMockData("OrderHistoryFile.json");
+                testunit.ProcessOrder(jsonOrderHistoryFile);
+
+                // assert
+            }
+        }
+        #endregion
+
+        private static void CheckFile(OrderHistoryFile file)
+        {
+            file.ValidHeader.Should().BeTrue();
+
+            CheckHeader(file.Header);
+        }
+
+        private static void CheckHeader(OrderHistoryHeader header)
+        {
+            if (string.IsNullOrWhiteSpace(header.OrderDateTime) == false)
+            {
+                DateTime orderDate = DateTime.Parse(header.OrderDateTime);
+                DateTime deliveryDate = DateTime.Parse(header.DeliveryDate).AddDays(1).AddSeconds(-1);   // end of day
+                orderDate.Should().BeOnOrBefore(deliveryDate);
+            }
+
+            header.ErrorStatus.Should().BeTrue("because I said so");
+        }
+
+
+        #region Setup
+
+        private static string GetMockData(string mockDataName)
+        {
+            StreamReader reader = GetMockDataReader(mockDataName);
+            string  mockData = reader.ReadToEnd();
+            reader.Close();
+
+            return mockData;
+        }
+
+        private static StreamReader GetMockDataReader(string mockDataName)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var resourceNames = assembly.GetManifestResourceNames();
+            var resourceName = typeof(OrderHistoryLogicTests).Namespace + "." + mockDataName;
+
+            Stream stream = assembly.GetManifestResourceStream(resourceName);
+
+            StreamReader reader = null;
+            if (stream != null)
+                reader = new StreamReader(stream);
+
+            return reader;
+        }
+
+        private static IOrderHistoryLogic MakeUnitToBeTested()
+        {
+            ContainerBuilder builder = DependencyMapFactory.GetOrderServiceContainer();
+            IContainer container = builder.Build();
+
+            IOrderHistoryLogic testunit = container.Resolve<IOrderHistoryLogic>();
+
+            return testunit;
+        }
+
+        #endregion Setup
+    }
+}
---
+++ KeithLink/KeithLink.Svc.Windows.OrderService.Tests.Integration/OrderServiceTests.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.IO;
+using System.Reflection;
+
+using Autofac;
+
+using Castle.Components.DictionaryAdapter;
+
+using FluentAssertions;
+
+using KeithLink.Common.Core.Interfaces.Logging;
+using KeithLink.Svc.Core.Interface.Common;
+using KeithLink.Svc.Core.Interface.Orders.History;
+using KeithLink.Svc.Core.Models.Orders.History;
+using KeithLink.Svc.Impl.Tests.Unit;
+using Moq;
+
+using Xunit;
+
+namespace KeithLink.Svc.Windows.OrderService.Tests.Integration
+{
+    public class OrderServiceTests : BaseDITests
+    {
+        #region ProcessOrderUpdatesTick
+        public class ProcessOrderUpdatesTick
+        {
+            [Fact]
+            public void StreamContainingRawOrderHistory_IsSentToParseMainframeFile()
+            {
+                // arrange
+                MockDependents mockDependents = new MockDependents();
+                OrderService testunit = MakeUnitToBeTested(true, mockDependents);
+
+                // act
+                object state = null;
+                testunit.ProcessOrderUpdatesTick(state);
+
+                // assert
+
+                // The OrderService does not use the mocks verified here.
+                // The container where the mocked instances are registered is replaced by the OrderService
+                // The OrderService builds a container from a builder it obtains from the DependencyMapFactory.
+
+                //mockDependents.MockOrderHistoryLogic
+                //    .Verify(m => m.ParseMainframeFile(It.IsAny<StreamReader>()), Times.Once, "not called.");
+
+                //mockDependents.MockGenericQueueRepository
+                //    .Verify(m => m.BulkPublishToQueue(It.IsAny<List<string>>(), It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>()), Times.Once, "not called.");
+            }
+
+        }
+        #endregion
+
+        #region Setup
+        public class MockDependents
+        {
+            public Mock<IOrderHistoryLogic> MockOrderHistoryLogic { get; set; }
+            public Mock<IGenericQueueRepository> MockGenericQueueRepository { get; set; }
+
+            public MockDependents()
+            {
+                // The OrderService does not use the mocks created here.
+                // The container where the mocked instances are registered is replaced by the OrderService
+                // The OrderService builds a container from a builder it obtains from the DependencyMapFactory.
+
+                //MockOrderHistoryLogic = new Mock<IOrderHistoryLogic>();
+                //MockGenericQueueRepository = new Mock<IGenericQueueRepository>();
+
+                //MockOrderHistoryLogic
+                //    .Setup(m => m.ParseMainframeFile(It.IsAny<StreamReader>()))
+                //    .Returns(It.IsAny<OrderHistoryFileReturn>());
+
+                //MockGenericQueueRepository
+                //    .Setup(m => m.BulkPublishToQueue(It.IsAny<List<string>>(), It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>()));
+            }
+        }
+
+        private static OrderService MakeUnitToBeTested(bool useAutoFac, MockDependents mockDependents)
+        {
+            if (useAutoFac)
+            {
+                ContainerBuilder containerBuilder = GetTestsContainer();
+
+                containerBuilder.RegisterType<OrderService>();
+
+                // Register mocks
+                RegisterInContainer(ref containerBuilder, mockDependents);
+
+                // The OrderService does not use the container built here.
+                // It builds a container from a builder it obtains from the DependencyMapFactory.
+                IContainer testcontainer = containerBuilder.Build();
+
+                return testcontainer.Resolve<OrderService>();
+            }
+
+            OrderService testunit = new OrderService();
+
+            return testunit;
+        }
+
+        public static void RegisterInContainer(ref ContainerBuilder cb, MockDependents mockDependents)
+        {
+            //cb.RegisterInstance(mockDependents.MockOrderHistoryLogic.Object)
+            //  .As<IOrderHistoryLogic>();
+
+            //cb.RegisterInstance(mockDependents.MockGenericQueueRepository.Object)
+            //  .As<IGenericQueueRepository>();
+        }
+
+
+        #endregion Setup
+    }
+}
