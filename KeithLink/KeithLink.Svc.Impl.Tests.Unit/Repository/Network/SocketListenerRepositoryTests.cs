15a26e4505608388c4be31cbcd793650e631fb30
John Devonshire <jjdevonshire@benekeith.com>
2018-08-03 12:59:50 -0500

    [159513068] Initial checkin.

---
+++ KeithLink/KeithLink.Svc.Impl.Tests.Unit/Repository/Network/SocketListenerRepositoryTests.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.IO;
+using System.Reflection;
+using System.Net;
+using System.Net.Sockets;
+
+using Autofac;
+
+using Castle.Components.DictionaryAdapter;
+
+using FluentAssertions;
+
+using KeithLink.Common.Core.Interfaces.Logging;
+
+using KeithLink.Svc.Core.Interface.Common;
+using KeithLink.Svc.Impl.Repository.Network;
+
+using Moq;
+
+using Newtonsoft.Json;
+
+using Xunit;
+
+namespace KeithLink.Svc.Impl.Tests.Unit.Repository.Network
+{
+    public class SocketListenerRepositoryTests : BaseDITests
+    {
+        #region Listen
+        public class Listen
+        {
+            [Fact]
+            public void WhenPortIsInUse_HasNoException()
+            {
+                // arrange
+                int WSAEADDRINUSE = 10048;
+
+                MockDependents mockDependents = new MockDependents();
+                mockDependents.MockSocket
+                    .Setup(m => m.Bind(It.IsAny<IPEndPoint>()))
+                    .Throws(BuildMockException(WSAEADDRINUSE));
+
+                ISocketListenerRepository testunit = MakeUnitToBeTested(true, mockDependents);
+
+                // act
+                Action listen = () => testunit.Listen(Configuration.MainframeConfirmationListeningPort);
+                listen.Invoke();
+
+                // assert
+                mockDependents.MockSocket
+                    .Verify(m => m.Bind(It.IsAny<IPEndPoint>()), Times.Once, "not called.");
+                listen.Should().NotThrow();
+            }
+
+            private Exception BuildMockException(int errorCode)
+            {
+                SocketException socketException = new SocketException(errorCode);
+                return socketException;
+            }
+        }
+        #endregion
+
+
+        #region Setup
+        public class MockDependents
+        {
+            public Mock<IEventLogRepository> MockEventLogRepository { get; set; }
+            public Mock<Socket> MockSocket { get; set; }
+
+            public MockDependents()
+            {
+                MockEventLogRepository = new Mock<IEventLogRepository>();
+                MockSocket = new Mock<Socket>();
+            }
+        }
+
+        private static ISocketListenerRepository MakeUnitToBeTested(bool useAutoFac, MockDependents mockDependents)
+        {
+            if (useAutoFac)
+            {
+                ContainerBuilder cb = GetTestsContainer();
+
+                // Register mocks
+                RegisterInContainer(ref cb, mockDependents);
+
+                IContainer testcontainer = cb.Build();
+
+                return testcontainer.Resolve<ISocketListenerRepository>();
+            }
+
+            SocketListenerRepositoryImpl testunit = new SocketListenerRepositoryImpl(
+                mockDependents.MockEventLogRepository.Object);
+
+            return testunit;
+        }
+
+        public static void RegisterInContainer(ref ContainerBuilder cb, MockDependents mockDependents)
+        {
+            cb.RegisterInstance(mockDependents.MockEventLogRepository.Object)
+              .As<IEventLogRepository>();
+
+            cb.RegisterInstance(mockDependents.MockSocket.Object)
+              .As<Socket>();
+        }
+
+
+        #endregion Setup
+    }
+}
--- KeithLink/KeithLink.Svc.Impl/Repository/Network/SocketListenerRepositoryImpl.cs
+++ KeithLink/KeithLink.Svc.Impl/Repository/Network/SocketListenerRepositoryImpl.cs
@@ -3,17 +3,24 @@ using KeithLink.Svc.Core.Events.EventArgs;
 using KeithLink.Svc.Core.Events.EventHandlers;
 using KeithLink.Svc.Core.Exceptions.Orders;
 using KeithLink.Svc.Core.Interface.Common;
+
 using System;
+using KeithLink.Common.Core.Interfaces.Logging;
+
 using System.IO;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 
-namespace KeithLink.Svc.Impl.Repository.Network {
-    public class SocketListenerRepositoryImpl : ISocketListenerRepository, IDisposable {
+namespace KeithLink.Svc.Impl.Repository.Network
+{
+    public class SocketListenerRepositoryImpl : ISocketListenerRepository, IDisposable
+    {
         #region attributes
         private Socket _handler;
         private bool _disposed;
+        private IEventLogRepository _log;
 
         public event EventHandler BeginningFileReceipt;
         public event EventHandler ClosedPort;
@@ -24,32 +31,73 @@ namespace KeithLink.Svc.Impl.Repository.Network {
         #endregion
 
         #region ctor
-        public SocketListenerRepositoryImpl() {
+        public SocketListenerRepositoryImpl(IEventLogRepository eventLogRepository)
+        {
+            _log = eventLogRepository;
             _disposed = false;
             _handler = null;
         }
         #endregion
 
         #region methods
-        public void Dispose() {
-            if (!_disposed) {
+        public void Dispose()
+        {
+            if (!_disposed)
+            {
                 if (_handler != null) { _handler.Dispose(); }
             }
 
             _disposed = true;
         }
 
-        public void Listen(int listeningPort) {
+        private void BindSocketToPort(Socket socket, IPEndPoint endPoint)
+        {
+            int attempts = 0;
+
+            while (socket.IsBound == false)
+            {
+                attempts++;
+                if (attempts % 60 == 0)
+                {
+                    _log.WriteInformationLog
+                        (string.Format("A socket listener is having trouble obtaining a port: {0}", endPoint.Port));
+                }
+
+                try
+                {
+                    socket.Bind(endPoint);
+                }
+                catch (SocketException ex)
+                {
+                    int WSAEADDRINUSE = 10048;
+
+                    if (ex.ErrorCode == WSAEADDRINUSE)
+                    {
+                        Thread.Sleep(1000);  // allow port to be released
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+            }
+        }
+
+        public void Listen(int listeningPort)
+        {
             IPEndPoint localEndPoint = new IPEndPoint(IPAddress.Any, listeningPort);
             Socket listener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
 
-            try {
+            try
+            {
                 OnOpeningPort(new EventArgs());
 
-                listener.Bind(localEndPoint);
+                BindSocketToPort(listener, localEndPoint);
+
                 listener.Listen(10);
 
-                while (true)  {
+                while (true)
+                {
                     OnWaitingConnection(new EventArgs());
                     _handler = listener.Accept();
 
@@ -57,13 +105,15 @@ namespace KeithLink.Svc.Impl.Repository.Network {
                     StringBuilder data = new StringBuilder();
                     bool receivingFile = true;
 
-                    while (receivingFile) {
+                    while (receivingFile)
+                    {
                         Byte[] bytes = new Byte[1024];
                         int bytesReceived = _handler.Receive(bytes);
 
                         data.Append(Encoding.ASCII.GetString(bytes, 0, bytesReceived));
 
-                        if (data.ToString().IndexOf("END###") > -1) {
+                        if (data.ToString().IndexOf("END###") > -1)
+                        {
                             OnFileReceived(new ReceivedFileEventArgs(data.ToString()));
 
                             _handler.Send(Encoding.ASCII.GetBytes("OK"));
@@ -71,7 +121,8 @@ namespace KeithLink.Svc.Impl.Repository.Network {
                             data = new StringBuilder();
                         }
 
-                        if (data.ToString().IndexOf("BYE###") > -1) {
+                        if (data.ToString().IndexOf("BYE###") > -1)
+                        {
                             receivingFile = false;
                         }
                     }
@@ -79,8 +130,11 @@ namespace KeithLink.Svc.Impl.Repository.Network {
                     _handler.Shutdown(SocketShutdown.Both);
                     _handler.Close();
                 }
-            } catch (Exception e) {
-                if ((_handler != null) && _handler.Connected) {
+            }
+            catch (Exception e)
+            {
+                if (_handler != null && _handler.Connected)
+                {
                     _handler.Shutdown(SocketShutdown.Both);
                     _handler.Close();
                 }
@@ -90,39 +144,51 @@ namespace KeithLink.Svc.Impl.Repository.Network {
 
             OnClosedPort(new EventArgs());
         }
-        
-        protected virtual void OnBeginningFileReceipt(EventArgs e) {
-            if (BeginningFileReceipt != null) {
+
+        protected virtual void OnBeginningFileReceipt(EventArgs e)
+        {
+            if (BeginningFileReceipt != null)
+            {
                 BeginningFileReceipt(this, e);
             }
         }
 
-        protected virtual void OnClosedPort(EventArgs e) {
-            if (ClosedPort != null) {
+        protected virtual void OnClosedPort(EventArgs e)
+        {
+            if (ClosedPort != null)
+            {
                 ClosedPort(this, e);
             }
         }
 
-        protected virtual void OnErrorEncountered(ExceptionEventArgs e) {
-            if (ErrorEncountered != null) {
+        protected virtual void OnErrorEncountered(ExceptionEventArgs e)
+        {
+            if (ErrorEncountered != null)
+            {
                 ErrorEncountered(this, e);
             }
         }
 
-        protected virtual void OnFileReceived(ReceivedFileEventArgs e) {
-            if (FileReceived != null) {
+        protected virtual void OnFileReceived(ReceivedFileEventArgs e)
+        {
+            if (FileReceived != null)
+            {
                 FileReceived(this, e);
             }
         }
 
-        protected virtual void OnOpeningPort(EventArgs e) {
-            if (OpeningPort != null) {
+        protected virtual void OnOpeningPort(EventArgs e)
+        {
+            if (OpeningPort != null)
+            {
                 OpeningPort(this, e);
             }
         }
 
-        protected virtual void OnWaitingConnection(EventArgs e) {
-            if (WaitingConnection != null) {
+        protected virtual void OnWaitingConnection(EventArgs e)
+        {
+            if (WaitingConnection != null)
+            {
                 WaitingConnection(this, e);
             }
         }
