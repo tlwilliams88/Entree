4c962cf20c15a2a109bd9133479724df11e9d40d
John Devonshire <jjdevonshire@benekeith.com>
2018-08-09 14:56:15 -0500

    [#158375841] Injected Constants into NavigationBar controller.

--- KeithLink/KeithLink.Web.Presentation/angular-app/app/scripts/directives/NavigationBar.js
+++ KeithLink/KeithLink.Web.Presentation/angular-app/app/scripts/directives/NavigationBar.js
@@ -11,10 +11,11 @@ angular.module('bekApp')
         userProfile: '=userProfile',      //Two-way data binding
       },
       templateUrl: 'views/directives/navigationBar.html',
-      controller: ['$scope', '$rootScope', '$stateParams', '$state',
+      controller: ['$scope', '$rootScope', '$stateParams', '$state', 'Constants',
         'AccessService', 'NotificationService', 'UserProfileService', 'ENV', 
-        function ($scope, $rootScope, $stateParams, $state,
-          AccessService, NotificationService, UserProfileService, ENV) {
+        function ($scope, $rootScope, $stateParams, $state, Constants,
+          AccessService, NotificationService, UserProfileService, ENV)
+        {
 
           if (typeof $scope.isSidebarOpen === "undefined")
             alert("The attribute, 'is-sidebar-open', was not defined in the element, 'navigation-bar'.");
@@ -24,6 +25,7 @@ angular.module('bekApp')
             alert("The attribute, 'user-profile', was not defined in the element, 'navigation-bar'.");
 
           $scope.$state = $state;
+          $scope.constants = Constants;
 
           $scope.isOrderEntryCustomer = AccessService.isOrderEntryCustomer();
           $scope.canBrowseCatalog = AccessService.canBrowseCatalog();
