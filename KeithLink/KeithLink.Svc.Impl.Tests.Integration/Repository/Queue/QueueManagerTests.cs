b6651e5b68ac83f7b6d3d586bfd954cdb7daef16
John Devonshire <jjdevonshire@benekeith.com>
2018-09-05 14:20:19 -0500

    Created QueueManager.

---
+++ KeithLink/KeithLink.Svc.Impl.Tests.Integration/Repository/Queue/QueueManagerTests.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.IO;
+using System.Reflection;
+
+using Autofac;
+
+using FluentAssertions;
+
+using KeithLink.Svc.Impl.Repository.Queue;
+using KeithLink.Svc.Impl.Repository.SmartResolver;
+using KeithLink.Svc.Impl.Tests.Integration.Repository;
+
+using RabbitMQ.Client;
+using Xunit;
+
+namespace KeithLink.Svc.Impl.Tests.Integration.Repository.Queue
+{
+    public class QueueManagerTests
+    {
+        public class AddTtlToQueue 
+        {
+            [Fact]
+            public void AddTtlToQueue_HasValidHeadersAndOrderDate()
+            {
+                // arrange
+                QueueManager testunit = MakeUnitToBeTested();
+
+                var queues = new List<(string name, int timeToLive)>
+                    {
+                        ("devonshire_confirmations", 36000000),   // time to live is 10 hours
+                        ("devonshire_orders_historyrequest", 3600000),   // time to live is 1 hours
+                    };
+
+                // act
+                List<QueueDeclareOk> responses = testunit.AddTtlToQueue(Configuration.RabbitMQConfirmationServer,
+                                                               "qarqadmin",  // Configuration.RabbitMQUserNameConsumer, 
+                                                               "1jqhqt34",  // Configuration.RabbitMQUserPasswordConsumer,
+                                                               Configuration.RabbitMQVHostConfirmation,
+                                                               queues);
+
+                // assert
+                responses.ForEach(response => response.QueueName.Should().Contain("devonshire"));
+            }
+        }
+
+        private static QueueManager MakeUnitToBeTested()
+        {
+            //ContainerBuilder builder = DependencyMapFactory.GetOrderServiceContainer();
+            //IContainer container = builder.Build();
+
+            //QueueManager testunit = container.Resolve<QueueManager>();
+
+            QueueManager testunit = new QueueManager();
+
+            return testunit;
+        }
+
+    }
+}
---
+++ KeithLink/KeithLink.Svc.Impl/Repository/Queue/QueueManager.cs
@@ -0,0 +1,79 @@
+﻿using KeithLink.Svc.Core.Exceptions.Queue;
+using KeithLink.Svc.Core.Interface.Common;
+using KeithLink.Svc.Core.Models.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using RabbitMQ.Client.Exceptions;
+
+namespace KeithLink.Svc.Impl.Repository.Queue
+{
+    public class QueueManager
+    {
+        #region methods
+        public List<QueueDeclareOk> AddTtlToQueue(string server, string username, string password, string virtualHost, List<(string name, int timeToLive)> queues)
+        {
+            ConnectionFactory connectionFactory = new ConnectionFactory()
+            {
+                HostName = server,
+                UserName = username,
+                Password = password,
+                VirtualHost = virtualHost
+            };
+
+            using (IConnection connection = connectionFactory.CreateConnection())
+            {
+                using (IModel model = connection.CreateModel())
+                {
+                    var responses = new List<QueueDeclareOk>();
+
+                    foreach (var queue in queues)
+                    {
+                        var args = new Dictionary<string, object>();
+                        args.Add("x-message-ttl", queue.timeToLive);    // time to live
+
+                        QueueDeclareOk response = model.QueueDeclare(queue.name, true, false, false, args);
+                        responses.Add(response);
+                    }
+
+                    return responses;
+                }
+            }
+        }
+
+        public QueueDeclareOk PassivelyDeclareQueue
+            (string server, string username, string password, string virtualHost, string queue)
+        {
+            QueueDeclareOk result = null;
+            try
+            {
+                ConnectionFactory connectionFactory = new ConnectionFactory()
+                {
+                    HostName = server,
+                    UserName = username,
+                    Password = password,
+                    VirtualHost = virtualHost
+                };
+
+                using (IConnection connection = connectionFactory.CreateConnection())
+                {
+                    using (IModel model = connection.CreateModel())
+                    {
+                        result = model.QueueDeclarePassive(queue);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new QueueConnectionException(server, virtualHost, string.Empty, queue, ex.Message, ex);
+            }
+            return result;
+        }
+
+        #endregion
+    }
+}
